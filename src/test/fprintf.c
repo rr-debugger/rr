75f60605450d5e51784c03a8b22bacf015d82744
nimrodpar <nimrodpar@gmail.com>
2012-09-20 05:25:31 +1200

    test suite updated, added a test utility script

--- src/test/fprintf.c
+++
@@ -1,7 +0,0 @@
-#include <stdio.h>
-
-int main() {
-	FILE *file = fopen("tmp","w");
-	fprintf(file,"Hello, file.\n");
-	return 0;
-}
---
+++ src/test/util.sh
@@ -0,0 +1,48 @@
+# default rr command
+rr="RecordReplayDebug"
+
+# check if the user supplied a custom rr command, if so use it, otherwise use default.
+# $1 is number of arguments supplied to the test script
+# $2 is the rr command (if exists)
+function get_rr_cmd {
+	if [ "$1" -gt "0" ]; then
+		rr=$2
+	fi
+}
+
+# compile test
+# $1 is test name
+# $2 are compilation flags
+function compile {
+	gcc $1.c $2
+}
+
+# record test. 
+# $1 is test name
+function record {
+	$rr --record a.out 1> $1.out.record
+}
+
+# replay test. 
+# $1 is test name 
+# $2 are rr flags
+function replay {
+	$rr --replay $2 trace_0/ 1> $1.out.replay 2> $1.err.replay
+}
+
+# check test success\failure.
+# $1 is test name
+function check {
+	if [[ $(cat $1.err.replay | grep "replayer sucessfully finished") == "" || $(diff $1.out.record $1.out.replay) != "" ]]; then
+		echo "Test $1 FAILED"
+	else
+		echo "Test $1 PASSED"
+		# test passed, OK to delete temporaries
+		rm -rf $1.out.record $1.out.replay $1.err.replay
+	fi
+}
+
+# cleanup
+function cleanup {
+	rm -rf a.out trace_0
+}
