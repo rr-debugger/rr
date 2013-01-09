2f1f918586495b7be5dbf6560d21bc8deae2e3f5
nimrodpar <nimrodpar@gmail.com>
2013-01-08 17:52:47 -0800

    more socketcall functions are now wrapped

--- src/share/wrap_syscalls.c
+++ src/share/wrap_syscalls.c
@@ -212,8 +212,6 @@ static void init() {
 		install_syscall_filter();
 	}
 	setup_buffer();
-	// print a message
-	debug("Initialized cache buffer for thread %d at %p",tid,buffer);
 }
 
 /**
@@ -231,7 +229,7 @@ init();																				\
 int ret;																			\
 int record_size_in_bytes = WRAP_SYSCALLS_RECORD_BASE_SIZE * sizeof(int);			\
 if (buffer[0] + record_size_in_bytes + extra_space > WRAP_SYSCALLS_CACHE_SIZE) {	\
-  flush_buffer();																	\
+flush_buffer();																		\
 }																					\
 int * const new_record = (void*)buffer + sizeof(int) + buffer[0];					\
 void * ptr = &new_record[WRAP_SYSCALLS_RECORD_BASE_SIZE];							\
@@ -355,13 +353,7 @@ int epoll_wait(int epfd, struct epoll_event *events, int maxevents, int timeout)
 }
 
 #define _copy_socketcall_args(arg0,arg1,arg2,arg3,arg4,arg5) \
-long args[6];			\
-args[0] = (long)arg0;	\
-args[1] = (long)arg1;	\
-args[2] = (long)arg2;	\
-args[3] = (long)arg3;	\
-args[4] = (long)arg4;	\
-args[5] = (long)arg5;
+volatile long args[6] = { (long)arg0, (long)arg1, (long)arg2, (long)arg3, (long)arg4, (long)arg5 };
 
 int accept4(int sockfd, struct sockaddr *addr, socklen_t *addrlen, int flags) {
 	// Stuff gets recorded only if addr is not null
@@ -523,61 +515,46 @@ ssize_t recvfrom(int sockfd, void *buf, size_t len, int flags, struct sockaddr *
 	_syscall_post(socketcall)
 }
 
-/* FIXME: write does not work, this has to do with the fact that it runs before stuff gets initialized in the main thread (??) */
-ssize_t write_(int fd, const void *buf, size_t count) {
-	_syscall_pre(0)
-	_syscall3(write,fd,buf,count,ret)
-	_syscall_post(write)
-}
-
-ssize_t writev_(int fd, const struct iovec *iov, int iovcnt) {
-	_syscall_pre(0)
-	_syscall3(writev,fd,iov,iovcnt,ret)
-	_syscall_post(writev)
-}
-
 #define _socketcall_no_output(call,arg0,arg1,arg2,arg3,arg4,arg5) 	\
 	_syscall_pre(0)													\
-	_copy_socketcall_args(arg0,arg1,arg2,arg3,arg4,arg5)				\
+	_copy_socketcall_args(arg0,arg1,arg2,arg3,arg4,arg5)			\
 	_syscall2(socketcall,call,args,ret) 							\
 	_syscall_post(socketcall)
 
-
-/* TODO: these behave strangly, maybe libc does domething that we dont? */
-int socket_(int domain, int type, int protocol) {
-	_socketcall_no_output(SYS_SOCKET,domain,type,protocol,0, 0, 0)
+int socket(int domain, int type, int protocol) {
+	_socketcall_no_output(SYS_SOCKET,domain,type,protocol,0,0,0)
 }
 
 int bind(int sockfd, const struct sockaddr *addr, socklen_t addrlen) {
-	_socketcall_no_output(SYS_BIND,sockfd,addr,addrlen,0, 0, 0)
+	_socketcall_no_output(SYS_BIND,sockfd,addr,addrlen,0,0,0)
 }
 
-int connect_(int sockfd, const struct sockaddr *addr, socklen_t addrlen) {
-	_socketcall_no_output(SYS_CONNECT,sockfd,addr,addrlen,0, 0, 0)
+int connect(int sockfd, const struct sockaddr *addr, socklen_t addrlen) {
+	_socketcall_no_output(SYS_CONNECT,sockfd,addr,addrlen,0,0,0)
 }
 
-int listen_(int sockfd, int backlog) {
-	_socketcall_no_output(SYS_LISTEN,sockfd,backlog,0,0, 0, 0)
+int listen(int sockfd, int backlog) {
+	_socketcall_no_output(SYS_LISTEN,sockfd,backlog,0,0,0,0)
 }
 
-ssize_t sendmsg_(int sockfd, const struct msghdr *msg, int flags) {
-	_socketcall_no_output(SYS_SENDMSG,sockfd,msg,flags,0, 0, 0)
+ssize_t sendmsg(int sockfd, const struct msghdr *msg, int flags) {
+	_socketcall_no_output(SYS_SENDMSG,sockfd,msg,flags,0,0,0)
 }
 
-ssize_t send_(int sockfd, const void *buf, size_t len, int flags) {
-	_socketcall_no_output(SYS_SEND,sockfd,buf, len, flags, 0, 0)
+ssize_t send(int sockfd, const void *buf, size_t len, int flags) {
+	_socketcall_no_output(SYS_SEND,sockfd,buf,len,flags,0,0)
 }
 
-ssize_t sendto_(int sockfd, const void *buf, size_t len, int flags, const struct sockaddr *dest_addr, socklen_t addrlen) {
-	_socketcall_no_output(SYS_SENDTO,sockfd,buf, len, flags, dest_addr, addrlen)
+ssize_t sendto(int sockfd, const void *buf, size_t len, int flags, const struct sockaddr *dest_addr, socklen_t addrlen) {
+	_socketcall_no_output(SYS_SENDTO,sockfd,buf,len,flags,dest_addr,addrlen)
 }
 
-int setsockopt_(int sockfd, int level, int optname, const void *optval, socklen_t optlen) {
-	_socketcall_no_output(SYS_SETSOCKOPT,sockfd, level, optname, optval, optlen, 0)
+int setsockopt(int sockfd, int level, int optname, const void *optval, socklen_t optlen) {
+	_socketcall_no_output(SYS_SETSOCKOPT,sockfd,level,optname,optval,optlen,0)
 }
 
-int shutdown_(int socket, int how) {
-	_socketcall_no_output(SYS_SHUTDOWN,socket, how, 0, 0, 0, 0)
+int shutdown(int socket, int how) {
+	_socketcall_no_output(SYS_SHUTDOWN,socket,how,0,0,0,0)
 }
 
 /* TODO: fix the complex logic here */
@@ -642,20 +619,58 @@ int socketpair_(int domain, int type, int protocol, int sv[2]) {
 	record_size_in_bytes += sizeof(sv);
 	args[3] = (long)sv2;
 	_syscall2(socketcall,SYS_SOCKETPAIR,args,ret)
-	// now in the replay we can simply push the recorded buffer and allow the wrapper to copy it to the actual parameters
 	memcpy(sv, sv2, sizeof(sv));
 	_syscall_post(socketcall)
 }
 
-/* TODO: fill in the rest of the calls */
-int socketcall_(int call, unsigned long *args){
+/* TODO: recv, recvfrom create a compilation error
+int socketcall(int call, unsigned long *args){
 	switch (call) {
+	case SYS_SOCKET:
+		return socket(args[0],args[1],args[2]);
+	case SYS_BIND:
+		return bind(args[0],(const struct sockaddr *)args[1],args[2]);
+	case SYS_CONNECT:
+		return connect(args[0],(const struct sockaddr *)args[1],args[2]);
+	case SYS_LISTEN:
+		return listen(args[0],args[1]);
 	case SYS_ACCEPT:
 		return accept(args[0],(struct sockaddr *)args[1],(socklen_t *)args[2]);
+	case SYS_GETSOCKNAME:
+		return getsockname(args[0],(struct sockaddr *)args[1],(socklen_t *)args[2]);
+	case SYS_GETPEERNAME:
+		return getpeername(args[0],(struct sockaddr *)args[1],(socklen_t *)args[2]);
+	case SYS_SOCKETPAIR:
+		return socketpair(args[0],args[1],args[2],(int *)args[3]);
+	case SYS_SEND:
+		return send(args[0],(void*)args[1],args[2],args[3]);
+	case SYS_SENDTO:
+		return sendto(args[0], (void *)args[1], args[2], args[3],(struct sockaddr *)args[4], args[5]);
+	case SYS_RECV:
+		return recv(args[0], (void *)args[1], args[2], args[3]);
+	case SYS_RECVFROM:
+		return recvfrom(args[0], (void *)args[1], args[2], args[3], (struct sockaddr *)args[4], (int *)args[5]);
+	case SYS_SHUTDOWN:
+		return shutdown(args[0], args[1]);
+	case SYS_SETSOCKOPT:
+		return setsockopt(args[0], args[1], args[2], (char *)args[3], args[4]);
+	case SYS_GETSOCKOPT:
+		return getsockopt(args[0], args[1], args[2], (char *)args[3], (int *)args[4]);
+	case SYS_SENDMSG:
+		return sendmsg(args[0], (struct msghdr *)args[1], args[2]);
+	case SYS_SENDMMSG:
+		return sendmmsg(args[0], (struct mmsghdr *)args[1], args[2], args[3]);
+	case SYS_RECVMSG:
+		return recvmsg(args[0], (struct msghdr *)args[1], args[2]);
+	case SYS_RECVMMSG:
+		return recvmmsg(args[0], (struct mmsghdr *)args[1], args[2], args[3],(struct timespec *)args[4]);
+	case SYS_ACCEPT4:
+		return accept4(args[0], (struct sockaddr *)args[1], (int *)args[2], args[3]);
 	default:
 		assert(0);
 	}
 }
+*/
 
 #define _stat(call,file,buf) 						\
 _syscall_pre( buf ? sizeof(struct stat) : 0 )		\
@@ -679,7 +694,6 @@ int lstat(const char *path, struct stat *buf) {
 	_stat(lstat64,path,buf);
 }
 
-/* FIXME: this causes SIGSEGV */
 int stat(const char *path, struct stat *buf){
 	_stat(stat64,path,buf);
 }
@@ -705,6 +719,19 @@ ssize_t read_(int fd, void *buf, size_t count) {
 		memcpy(buf,buf2,ret);
 	}
 	_syscall_post(read)
+}
+
+/* FIXME: write does not work, this has to do with the fact that it runs before stuff gets initialized in the main thread (??) */
+ssize_t write_(int fd, const void *buf, size_t count) {
+	_syscall_pre(0)
+	_syscall3(write,fd,buf,count,ret)
+	_syscall_post(write)
+}
+
+ssize_t writev_(int fd, const struct iovec *iov, int iovcnt) {
+	_syscall_pre(0)
+	_syscall3(writev,fd,iov,iovcnt,ret)
+	_syscall_post(writev)
 }
 
 /* TODO: hangs */
