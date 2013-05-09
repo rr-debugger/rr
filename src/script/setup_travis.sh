b6d18d33b307668f85d44bcebd0b51d044e3209d
Chris Jones <cjones.bugs@gmail.com>
2013-05-09 14:05:41 -0700

    Add some diagnostic information to travis setup script

--- src/script/setup_travis.sh
+++ src/script/setup_travis.sh
@@ -2,4 +2,10 @@
 
 # Install the prerequisites needed to build and run tests on travis-ci.
 
-sudo apt-get install gcc-multilib libdisasm-dev
+echo Configuring travis-ci build slave ...
+echo The slave is `uname -a`
+
+sudo apt-get install libc6-dev libdisasm-dev
+sudo apt-get install libc6-dev-i386
+
+echo ... finished configuring slave
