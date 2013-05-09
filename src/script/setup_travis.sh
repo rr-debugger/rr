3ca5a7bf7fd29dcb0e096475467728bc337ac2dc
Chris Jones <cjones.bugs@gmail.com>
2013-05-09 16:49:06 -0700

    It's ok if setup.sh fails on systems without yama ptrace hardening

--- src/script/setup_travis.sh
+++ src/script/setup_travis.sh
@@ -5,8 +5,11 @@
 echo Configuring travis-ci build slave ...
 echo The slave is `uname -a`
 
-sudo ./src/script/setup.sh && \
-    apt-get update && \
+# setup.sh will return an error code on systems that don't use yama
+# ptrace hardening
+sudo ./src/script/setup.sh
+
+sudo apt-get update && \
     sudo apt-get install gcc-multilib libc6-dev:i386 libdisasm-dev:i386 && \
     wget https://s3-us-west-1.amazonaws.com/rr-packages/libpfm_4.3.0-1_amd64.deb && \
     sudo dpkg -i libpfm_4.3.0-1_amd64.deb && \
