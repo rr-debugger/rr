f0b783e4685d3efae128ed76bced252ca7bccd24
Chris Jones <cjones.bugs@gmail.com>
2013-05-09 15:45:35 -0700

    Update build slave before installing prereqs

--- src/script/setup_travis.sh
+++ src/script/setup_travis.sh
@@ -5,7 +5,7 @@
 echo Configuring travis-ci build slave ...
 echo The slave is `uname -a`
 
-sudo apt-get install libc6-dev:i386 libdisasm-dev:i386
-sudo apt-get install gcc-multilib
-
-echo ... finished configuring slave
+sudo apt-get update && \
+    sudo apt-get upgrade && \
+    sudo apt-get install gcc-multilib libc6-dev:i386 libdisasm-dev:i386 && \
+    echo ... finished configuring slave
