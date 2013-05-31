69ff8101f4f20d223eb3d4bc1c3fafe8246f9e5f
Chris Jones <cjones.bugs@gmail.com>
2013-05-30 19:13:38 -0700

    Add a test for asynchronously-delivered SIGSEGV, disabled because it fails currently

--- src/test/segfault.c
+++ src/test/segfault.c
@@ -1,8 +1,6 @@
 /* -*- Mode: C; tab-width: 8; c-basic-offset: 8; indent-tabs-mode: t; -*- */
 
-#include <stdlib.h>
-
 int main(int argc, char *argv[]) {
-	*((int*)rand()) = rand();
+	*((volatile int*)0) = 0;
 	return 0;
 }
