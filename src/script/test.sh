d6d9650733d5b7dc08aa61d2fef59d8f67ff95e7
Chris Jones <cjones.bugs@gmail.com>
2013-07-16 19:10:46 -0700

    Build tests with cmake, and run them in their own tmp dirs.

--- src/script/test.sh
+++ src/script/test.sh
@@ -5,22 +5,16 @@ while [[ $# > 0 ]] ; do
 		-dir=*)     dir=${1#-dir=} ;    shift 1 ;;
 		-h)         usage=1 ;            shift 1 ;;
 		-lib=*)     lib=${1#-lib=} ;    shift 1 ;;
-		-rr=*)      rr=${1#-rr=} ;      shift 1 ;;
 		*)                              shift 1 ;;
 	esac
 done
 
 if [ -n "$usage" ]; then
-	echo "Usage: test.sh [-dir=test_directory] [-rr=rr_command] [-lib=filter_lib]"
+	echo "Usage: test.sh [-dir=test_directory] [-lib=filter_lib]"
 	echo "Runs rr standard tests."
 	echo "Default rr command is 'rr'"
 	echo "Default test directory is '../test/'"
 	exit 0
-fi
-
-# find the rr command, if not given
-if [ -z $rr ]; then
-	rr=rr
 fi
 
 # find the tests dir, if not given
@@ -40,5 +34,5 @@ cd $dir
 
 # record and replay all tests
 for test in *.run; do
-	bash $test "$lib" $rr
+	bash $test "$lib"
 done
