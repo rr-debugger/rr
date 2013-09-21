26f4c7879cd8f1850b0a7144a02b8acfd67934af
Chris Jones <cjones.bugs@gmail.com>
2013-09-20 21:52:50 -0700

    Add test for fix in a46ede.

---
+++ src/test/break_time_slice.py
@@ -0,0 +1,12 @@
+from rrutil import *
+
+send_gdb('b breakpoint\n')
+expect_gdb('Breakpoint 1')
+
+send_gdb('c\n')
+expect_gdb('Breakpoint 1, breakpoint')
+
+send_gdb('c\n')
+expect_gdb('exited normally')
+
+ok()
