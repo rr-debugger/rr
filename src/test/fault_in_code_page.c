d348dd0d5391a6cce41d9aff664748dcbfcf9739
Chris Jones <cjones.bugs@gmail.com>
2014-02-26 20:05:30 -0800

    Fix off-by-one error, and add test of faulting in JIT code.

---
+++ src/test/fault_in_code_page.c
@@ -0,0 +1,60 @@
+/* -*- Mode: C; tab-width: 8; c-basic-offset: 8; indent-tabs-mode: t; -*- */
+
+#include "rrutil.h"
+
+typedef int (*fn_type)(void);
+static fn_type fn = NULL;
+static const byte fn_insns[] = {
+	0xb8, 0x2a, 0x00, 0x00, 0x00, /* movl $42, %eax */
+	0xc3,			      /* ret */
+};
+
+static byte* code_page;
+static size_t page_size;
+
+static int fault_count;
+static void fault_in_code_page(int sig, siginfo_t* si, void* context) {
+	atomic_printf("FAULT: signal %d: code %d for addr %p\n",
+		      sig, si->si_code, si->si_addr);
+	test_assert(SIGSEGV == sig);
+	test_assert(SEGV_ACCERR == si->si_code);
+	test_assert(code_page == si->si_addr);
+	test_assert(1 == ++fault_count);
+
+	atomic_puts("  populating page...");
+	test_assert(0 == mprotect(code_page, page_size,
+				  PROT_READ | PROT_WRITE));
+	test_assert(sizeof(fn_insns) < page_size);
+	memcpy(code_page, fn_insns, sizeof(fn_insns));
+
+	test_assert(0 == mprotect(code_page, page_size,
+				  PROT_READ | PROT_EXEC));
+	atomic_puts("  ... and protected it. sigreturn'ing");
+}
+
+int main(int argc, char *argv[]) {
+	struct sigaction act;
+
+	page_size = sysconf(_SC_PAGESIZE);
+
+	memset(&act, 0, sizeof(act));
+	act.sa_sigaction = fault_in_code_page;
+	act.sa_flags = SA_SIGINFO;
+	sigaction(SIGSEGV, &act, NULL);
+
+	code_page = mmap(NULL, page_size,
+			 PROT_NONE, MAP_PRIVATE | MAP_ANONYMOUS, -1, 0);
+
+	atomic_printf("(%d) mapped code page to %p\n", errno, code_page);
+	test_assert(code_page != (void*)-1);
+	fn = (fn_type)code_page;
+
+	atomic_printf("calling fn(), faulting ...\n");
+	int ret = fn();
+
+	atomic_printf("fn() returned %d\n", ret);
+	test_assert(42 == ret);
+
+	atomic_puts("EXIT-SUCCESS");
+	return 0;
+}
