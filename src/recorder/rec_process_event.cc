0cfdc00fd949897c00e154dc44ff092dfbca1a56
Chris Jones <cjones.bugs@gmail.com>
2014-03-02 13:12:53 -0800

    Fold handle_ioctl into rec_process_event, for consistency.

--- src/recorder/rec_process_event.cc
+++ src/recorder/rec_process_event.cc
@@ -17,6 +17,7 @@
 #include <poll.h>
 #include <sched.h>
 #include <sys/epoll.h>
+#include <sys/ioctl.h>
 #include <sys/mman.h>
 #include <sys/quota.h>
 #include <sys/resource.h>
@@ -27,17 +28,18 @@
 #include <sys/times.h>
 #include <sys/utsname.h>
 #include <sys/vfs.h>
+#include <termios.h>
 
 #include <limits>
 #include <utility>
 
 #include <rr/rr.h>
 
-#include "handle_ioctl.h"
 #include "rec_sched.h"
 
 #include "../preload/syscall_buffer.h"
 #include "../share/dbg.h"
+#include "../share/drm.h"
 #include "../share/sys.h"
 #include "../share/task.h"
 #include "../share/trace.h"
@@ -929,6 +931,125 @@ static void process_execve(Task* t)
 	record_child_data(t, 16, rand_addr);
 
 	init_scratch_memory(t);
+}
+
+static void process_ioctl(Task *t, int request)
+{
+	int syscall = SYS_ioctl;
+	int type = _IOC_TYPE(request);
+	int nr = _IOC_NR(request);
+	int dir = _IOC_DIR(request);
+	int size = _IOC_SIZE(request);
+
+	debug("handling ioctl(0x%x): type:0x%x nr:0x%x dir:0x%x size:%d",
+	      request, type, nr, dir, size);
+
+	assert_exec(t, !t->is_desched_event_syscall(),
+		    "Failed to skip past desched ioctl()");
+
+	/* Some ioctl()s are irregular and don't follow the _IOC()
+	 * conventions.  Special case them here. */
+	switch (request) {
+	case TCGETS:
+		push_syscall(t, syscall);
+		record_child_data(t, sizeof(struct termios),
+				  (byte*)t->regs().edx);
+		pop_syscall(t);
+		return;
+	case TIOCINQ:
+		push_syscall(t, syscall);
+		record_child_data(t, sizeof(int), (byte*)t->regs().edx);
+		pop_syscall(t);
+		return;
+	}
+
+	/* In ioctl language, "_IOC_WRITE" means "outparam".  Both
+	 * READ and WRITE can be set for inout params. */
+	if (!(_IOC_WRITE & dir)) {
+		/* If the kernel isn't going to write any data back to
+		 * us, we hope and pray that the result of the ioctl
+		 * (observable to the tracee) is deterministic. */
+		debug("  (deterministic ioctl, nothing to do)");
+		return;
+	}
+
+	/* The following are thought to be "regular" ioctls, the
+	 * processing of which is only known to (observably) write to
+	 * the bytes in the structure passed to the kernel.  So all we
+	 * need is to record |size| bytes.*/
+	switch (request) {
+	/* TODO: what are the 0x46 ioctls? */
+	case 0xc020462b:
+	case 0xc048464d:
+	case 0xc0204637:
+	case 0xc0304627:
+		fatal("Unknown 0x46-series ioctl nr 0x%x", nr);
+		break;	/* not reached */
+
+	/* The following are ioctls for the linux Direct Rendering
+	 * Manager (DRM).  The ioctl "type" is 0x64 (100, or ASCII 'd'
+	 * as they docs helpfully declare it :/).  The ioctl numbers
+	 * are allocated as follows
+	 *
+	 *  [0x00, 0x40) -- generic commands
+	 *  [0x40, 0xa0) -- device-specific commands
+	 *  [0xa0, 0xff) -- more generic commands
+	 *
+	 * Chasing down unknown ioctls is somewhat annoying in this
+	 * scheme, but here's an example: request "0xc0406481".  "0xc"
+	 * means it's a read/write ioctl, and "0x0040" is the size of
+	 * the payload.  The actual ioctl request is "0x6481".
+	 *
+	 * As we saw above, "0x64" is the DRM type.  So now we need to
+	 * see what command "0x81" is.  It's in the
+	 * device-specific-command space, so we can start by
+	 * subtracting "0x40" to get a command "0x41".  Then
+	 *
+	 *  $ cd 
+	 *  $ grep -rn 0x41 *
+	 *  nouveau_drm.h:200:#define DRM_NOUVEAU_GEM_PUSHBUF        0x41
+	 *
+	 * Well that was lucky!  So the command is
+	 * DRM_NOUVEAU_GEM_PUSHBUF, and the parameters etc can be
+	 * tracked down from that.
+	 */
+
+	/* TODO: At least one of these ioctl()s, most likely
+	 * NOUVEAU_GEM_NEW, opens a file behind rr's back on behalf of
+	 * the callee.  That wreaks havoc later on in execution, so we
+	 * disable the whole lot for now until rr can handle that
+	 * behavior (by recording access to shmem segments). */
+	case DRM_IOCTL_VERSION:
+	case DRM_IOCTL_NOUVEAU_GEM_NEW:
+	case DRM_IOCTL_NOUVEAU_GEM_PUSHBUF:
+		fatal("Intentionally unhandled DRM(0x64) ioctl nr 0x%x", nr);
+		break;
+
+	case DRM_IOCTL_GET_MAGIC:
+	case DRM_IOCTL_RADEON_INFO:
+	case DRM_IOCTL_I915_GEM_PWRITE:
+	case DRM_IOCTL_GEM_OPEN:
+	case DRM_IOCTL_I915_GEM_MMAP:
+	case DRM_IOCTL_RADEON_GEM_CREATE:
+	case DRM_IOCTL_RADEON_GEM_GET_TILING:
+		fatal("Not-understood DRM(0x64) ioctl nr 0x%x", nr);
+		break;	/* not reached */
+
+	case 0x4010644d:
+	case 0xc0186441:
+	case 0x80086447:
+	case 0xc0306449:
+	case 0xc030644b:
+		fatal("Unknown DRM(0x64) ioctl nr 0x%x", nr);
+		break;	/* not reached */
+
+	default:
+		print_register_file_tid(t);
+		assert_exec(t, 0,
+			    "Unknown ioctl(0x%x): type:0x%x nr:0x%x dir:0x%x size:%d addr:%p",
+			    request, type, nr, dir, size,
+			    (void*)t->regs().edx);
+	}
 }
 
 static void process_ipc(Task* t, int call)
@@ -1867,7 +1988,7 @@ void rec_process_syscall(Task *t)
 	 * 			7-0	function #
 	 * */
 	case SYS_ioctl:
-		handle_ioctl_request(t, t->regs().ecx);
+		process_ioctl(t, t->regs().ecx);
 		break;
 
 	/**
