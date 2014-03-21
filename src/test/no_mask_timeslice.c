bfcc86c97554df5cc7fc04b32cef951387508437
Chris Jones <cjones.bugs@gmail.com>
2014-03-21 15:33:04 -0700

    Add files missing from last commit.

---
+++ src/test/no_mask_timeslice.c
@@ -0,0 +1,37 @@
+/* -*- Mode: C; tab-width: 8; c-basic-offset: 8; indent-tabs-mode: t; -*- */
+
+#include "rrutil.h"
+
+static int pseudospinlock;
+static pthread_barrier_t bar;
+
+static void* thread(void* unused) {
+	pthread_barrier_wait(&bar);
+
+	sched_yield();
+	pseudospinlock = 1;
+
+	return NULL;
+}
+
+int main(int argc, char *argv[]) {
+	sigset_t old, mask;
+	pthread_t t;
+
+	pthread_barrier_init(&bar, NULL, 2);
+
+	test_assert(0 == pthread_create(&t, NULL, thread, NULL));
+
+	sigfillset(&mask);
+	pthread_sigmask(SIG_BLOCK, &mask, &old);
+
+	pthread_barrier_wait(&bar);
+	while (!pseudospinlock);
+
+	pthread_sigmask(SIG_SETMASK, &old, NULL);
+
+	pthread_join(t, NULL);
+
+	atomic_puts("EXIT-SUCCESS");
+	return 0;
+}
