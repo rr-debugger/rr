7a1a54212c73b2b39ea0ad5705a08eb7b9664171
Chris Jones <cjones.bugs@gmail.com>
2014-03-26 18:34:24 -0500

    Bump the gdb packet size.

---
+++ src/test/read_big_struct.c
@@ -0,0 +1,25 @@
+/* -*- Mode: C; tab-width: 8; c-basic-offset: 8; indent-tabs-mode: t; -*- */
+
+#include "rrutil.h"
+
+static void breakpoint(void) {
+	int break_here = 1;
+	(void)break_here;
+}
+
+/* FIXME: we should be able to send arbitrarily large structs over the
+ * debugging socket.  This is a temporary hack. */
+struct big {
+	char bytes[8192];
+};
+
+int main(int argc, char *argv[]) {
+	struct big big;
+
+	memset(&big, 0x5a, sizeof(big));
+
+	breakpoint();
+
+	atomic_puts("EXIT-SUCCESS");
+	return 0;
+}
