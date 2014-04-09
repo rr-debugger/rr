rr-debugger/rr

commit a2e62741f7c4721a692dd34fd3e82a7da8f35751

Handle mmaps where some pages of the mapping are beyond the end of the file; these pages generate SIGBUS when accessed.

src/record_syscall.cc
@@ -3206,7 +3206,7 @@ void rec_process_syscall(Task *t)
 		size_t size = ceil_page_size(t->regs().ecx);
 		int prot = t->regs().edx, flags = t->regs().esi,
 		      fd = t->regs().edi;
-		off_t offset_pages = t->regs().ebp;
+		off64_t offset = t->regs().ebp * 4096; // 4096-byte chunks, not pages
 		if (flags & MAP_ANONYMOUS) {
 			// Anonymous mappings are by definition not
 			// backed by any file-like object, and are
@@ -3246,12 +3246,13 @@ void rec_process_syscall(Task *t)
 						      prot, flags,
 						      WARN_DEFAULT);
 		if (file.copied) {
-			record_child_data(t, size, addr);
+			off64_t end = (off64_t)file.stat.st_blocks * 512 -
+				offset;
+			record_child_data(t, (size_t)min(end, (off64_t)size), addr);
 		}
 		record_mmapped_file_stats(&file);
 
-		t->vm()->map(addr, size, prot, flags,
-			     page_size() * offset_pages,
+		t->vm()->map(addr, size, prot, flags, offset,
 			     MappableResource(FileId(file.stat),
 					      file.filename));
 		break;

src/replay_syscall.cc
@@ -826,6 +826,49 @@ static void* finish_anonymous_mmap(Task* t,
 				      fd, offset_pages);
 }
 
+/* Ensure that accesses to the memory region given by start/length
+   cause a SIGBUS, as for accesses beyond the end of an mmaped file. */
+static void create_sigbus_region(Task* t,
+				 struct current_state_buffer* state,
+				 int prot, void* start, size_t length)
+{
+	if (length == 0) {
+		return;
+	}
+
+	/* Open an empty file in the tracee */
+	char filename[] = "/tmp/rr-emptyfile-XXXXXX";
+	int fd = mkstemp(filename);
+	/* Close our side immediately */
+	close(fd);
+
+	int child_fd;
+	{
+		struct restore_mem restore;
+		void* child_str = push_tmp_str(t, state, filename, &restore);
+		child_fd = remote_syscall2(t, state, SYS_open, child_str, O_RDONLY);
+		if (0 > child_fd) {
+			fatal("Couldn't open %s to mmap in tracee", filename);
+		}
+		pop_tmp_mem(t, state, &restore);
+	}
+
+	/* Unlink it now that the child has opened it */
+	unlink(filename);
+
+	/* mmap it in the tracee. We need to set the correct 'prot' flags
+	   so that the correct signal is generated on a memory access
+	   (SEGV if 'prot' doesn't allow the access, BUS if 'prot' does allow
+	   the access). */
+	remote_syscall6(t, state, SYS_mmap2,
+			start, length,
+			prot, MAP_FIXED | MAP_PRIVATE,
+			child_fd, 0);
+	/* Don't leak the tmp fd.  The mmap doesn't need the fd to
+	 * stay open. */
+	remote_syscall1(t, state, SYS_close, fd);
+}
+
 static void* finish_private_mmap(Task* t,
 				 struct current_state_buffer* state,
 				 struct trace_frame* trace,
@@ -844,7 +887,13 @@ static void* finish_private_mmap(Task* t,
 						  flags | MAP_ANONYMOUS,
 						  DONT_NOTE_TASK_MAP);
 	/* Restore the map region we copied. */
-	t->set_data_from_trace();
+	ssize_t data_size = t->set_data_from_trace();
+
+	/* Ensure pages past the end of the file fault on access */
+	size_t data_pages = ceil_page_size(data_size);
+	size_t mapped_pages = ceil_page_size(num_bytes);
+	create_sigbus_region(t, state, prot, (char*)mapped_addr + data_pages,
+			     mapped_pages - data_pages);
 
 	t->vm()->map(mapped_addr, num_bytes, prot, flags,
 		     page_size() * offset_pages,

CMakeLists.txt
@@ -125,6 +125,7 @@ set(BASIC_TESTS
   mmap_ro
   mmap_shared
   mmap_shared_subpage
+  mmap_short_file
   mmap_tmpfs
   mmap_write
   mprotect

src/test/mmap_short_file.c
@@ -1,0 +1,39 @@
+/* -*- Mode: C; tab-width: 8; c-basic-offset: 8; indent-tabs-mode: t; -*- */
+
+#include "rrutil.h"
+
+
+#define DUMMY_FILE "dummy.txt"
+
+static void sighandler(int sig) {
+	atomic_printf("caught signal %d, exiting\n", sig);
+	atomic_puts("EXIT-SUCCESS");
+	_exit(0);
+}
+
+int main(int argc, char *argv[]) {
+	size_t page_size = sysconf(_SC_PAGESIZE);
+	int fd = open(DUMMY_FILE, O_CREAT | O_EXCL | O_RDWR, 0600);
+	int one = 1;
+	int* rpage;
+
+	unlink(DUMMY_FILE);
+
+	test_assert(fd >= 0);
+	test_assert(sizeof(one) == write(fd, &one, sizeof(one)));
+
+	rpage = mmap(NULL, page_size*2, PROT_READ, MAP_PRIVATE, fd, 0);
+	test_assert(rpage != (void*)-1);
+	test_assert(*rpage == 1);
+
+	signal(SIGSEGV, sighandler);
+	signal(SIGBUS, sighandler);
+	/* This should generate a SIGBUS, but the test will pass whether the
+	   kernel generates SIGBUS or SIGSEGV.
+	   rr checks that the same signal is produced during replay
+	   as during recording. */
+	char ch = *((char*)rpage + page_size);
+
+	atomic_printf("FAILED: no segfault, read %d", ch);
+	return 0;
+}

src/test/mmap_short_file.run
@@ -1,0 +1,2 @@
+source `dirname $0`/util.sh mmap_short_file "$@"
+compare_test 'EXIT-SUCCESS'

commit 771132722f433893c3b3ea53c718137a28da3570

Fix 'make check' regressions from a2e62741f7c4721a692dd34fd3e82a7da8f35751

src/record_syscall.cc
@@ -3246,7 +3246,7 @@ void rec_process_syscall(Task *t)
 						      prot, flags,
 						      WARN_DEFAULT);
 		if (file.copied) {
-			off64_t end = (off64_t)file.stat.st_blocks * 512 -
+			off64_t end = (off64_t)file.stat.st_size -
 				offset;
 			record_child_data(t, (size_t)min(end, (off64_t)size), addr);
 		}

src/replay_syscall.cc
@@ -837,7 +837,7 @@ static void create_sigbus_region(Task* t,
 	}
 
 	/* Open an empty file in the tracee */
-	char filename[] = "/tmp/rr-emptyfile-XXXXXX";
+	char filename[] = PREFIX_FOR_EMPTY_MMAPED_REGIONS "XXXXXX";
 	int fd = mkstemp(filename);
 	/* Close our side immediately */
 	close(fd);
@@ -1036,7 +1036,8 @@ static void* finish_shared_mmap(Task* t,
 	void* rec_addr;
 	size_t num_bytes;
 	byte* data = (byte*)read_raw_data(&(t->trace), &num_bytes, &rec_addr);
-	assert(data && rec_addr == mapped_addr && rec_num_bytes == num_bytes);
+	assert(data && rec_addr == mapped_addr &&
+	       rec_num_bytes == ceil_page_size(num_bytes));
 
 	off64_t offset_bytes = page_size() * offset_pages;
 	if (ssize_t(num_bytes) !=

src/task.cc
@@ -408,6 +408,10 @@ static bool is_adjacent_mapping(const MappingResourcePair& left,
 	}
 	const MappableResource& rleft = left.second;
 	const MappableResource& rright = right.second;
+	if (rright.fsname.substr(0, strlen(PREFIX_FOR_EMPTY_MMAPED_REGIONS)) ==
+	    PREFIX_FOR_EMPTY_MMAPED_REGIONS) {
+		return true;
+	}
 	if (rleft != rright) {
 		debug("    (not the same resource)");
 		return false;

src/util.h
@@ -73,6 +73,8 @@ struct trace_frame;
  * checks that include this fd. */
 #define REPLAY_DESCHED_EVENT_FD -123
 
+#define PREFIX_FOR_EMPTY_MMAPED_REGIONS "/tmp/rr-emptyfile-"
+
 /**
  * Collecion of data describing a mapped memory segment, as parsed
  * from /proc/[tid]/maps on linux.