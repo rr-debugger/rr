5bc650958f15f62c115f755cf6d813f1ed7e8058
Robert O'Callahan <robert@ocallahan.org>
2014-05-07 22:34:26 -0700

    Add architecture-neutral register accessors to Registers.
    This assumes every architecture has an instruction pointer, a stack
    pointer, a register for holding the system call number on entry to
    a syscall, a (possibly different) register for the system call
    result, a pseudo-register recording the original system call number
    during a ptrace system call notification, and registers for six
    syscall parameters.

--- src/registers.h
+++ src/registers.h
@@ -3,6 +3,7 @@
 #ifndef RR_REGISTERS_H_
 #define RR_REGISTERS_H_
 
+#include <stdint.h>
 #include <sys/user.h>
 
 /**
@@ -17,6 +18,58 @@
  * get sign-extension right.
  */
 class Registers: public user_regs_struct {
+public:
+	uintptr_t ip() const { return eip; }
+	void set_ip(uintptr_t addr) { eip = addr; }
+	uintptr_t sp() const { return esp; }
+	void set_sp(uintptr_t addr) { esp = addr; }
+
+	// Access the registers holding system-call numbers, results, and
+	// parameters.
+
+	intptr_t syscallno() const { return eax; }
+	void set_syscallno(intptr_t syscallno) { eax = syscallno; }
+
+	uintptr_t syscall_result() const { return eax; }
+	intptr_t syscall_result_signed() const { return eax; }
+	void set_syscall_result(uintptr_t syscall_result) {
+		eax = syscall_result;
+	}
+
+	/**
+	 * This pseudo-register holds the system-call number when we get ptrace
+	 * enter-system-call and exit-system-call events. Setting it changes
+	 * the system-call executed when resuming after an enter-system-call
+	 * event.
+	 */
+	intptr_t original_syscallno() const { return orig_eax; }
+	void set_original_syscallno(intptr_t syscallno) {
+		orig_eax = syscallno;
+	}
+
+	uintptr_t arg1() const { return ebx; }
+	intptr_t arg1_signed() const { return ebx; }
+	void set_arg1(uintptr_t value) { ebx = value; }
+
+	uintptr_t arg2() const { return ecx; }
+	intptr_t arg2_signed() const { return ecx; }
+	void set_arg2(uintptr_t value) { ecx = value; }
+
+	uintptr_t arg3() const { return edx; }
+	intptr_t arg3_signed() const { return edx; }
+	void set_arg3(uintptr_t value) { edx = value; }
+
+	uintptr_t arg4() const { return esi; }
+	intptr_t arg4_signed() const { return esi; }
+	void set_arg4(uintptr_t value) { esi = value; }
+
+	uintptr_t arg5() const { return edi; }
+	intptr_t arg5_signed() const { return edi; }
+	void set_arg5(uintptr_t value) { edi = value; }
+
+	uintptr_t arg6() const { return ebp; }
+	intptr_t arg6_signed() const { return ebp; }
+	void set_arg6(uintptr_t value) { ebp = value; }
 };
 
 #endif /* RR_REGISTERS_H_ */
