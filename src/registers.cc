ca453a04cc5d1f23328bcba8bfd88482169e588e
Nathan Froyd <froydnj@gmail.com>
2014-05-22 20:11:14 -0400

    move guts of compare_register_files into Registers

--- src/registers.cc
+++ src/registers.cc
@@ -7,6 +7,9 @@
 #include <assert.h>
 #include <string.h>
 
+#include "log.h"
+#include "util.h"
+
 void Registers::print_register_file(FILE* f) const
 {
 	fprintf(f, "Printing register file:\n");
@@ -51,6 +54,92 @@ void Registers::print_register_file_for_trace(FILE* f, bool raw_dump) const
 			eax, ebx, ecx, edx, esi, edi, ebp,
 			eip, esp, eflags, orig_eax, xfs, xgs);
 	}
+}
+
+static void maybe_print_reg_mismatch(int mismatch_behavior, const char* regname,
+				     const char* label1, long val1,
+				     const char* label2, long val2)
+{
+	if (mismatch_behavior >= BAIL_ON_MISMATCH) {
+		LOG(error) << regname <<" "<< HEX(val1) <<" != "
+			   << HEX(val2) <<" ("<< label1 <<" vs. "<< label2 <<")";
+	} else if (mismatch_behavior >= LOG_MISMATCHES) {
+		LOG(info) << regname <<" "<< HEX(val1) <<" != "
+			  << HEX(val2) <<" ("<< label1 <<" vs. "<< label2 <<")";
+	}
+}
+
+/*static*/ bool Registers::compare_register_files(const char* name1,
+						  const Registers* reg1,
+						  const char* name2,
+						  const Registers* reg2,
+						  int mismatch_behavior)
+{
+	bool match = true;
+
+#define REGCMP(_reg)							   \
+	do {								   \
+		if (reg1-> _reg != reg2-> _reg) {			   \
+			maybe_print_reg_mismatch(mismatch_behavior, #_reg, \
+						 name1, reg1-> _reg,	   \
+						 name2, reg2-> _reg);	   \
+			match = false;					   \
+		}							   \
+	} while (0)
+
+	REGCMP(eax);
+	REGCMP(ebx);
+	REGCMP(ecx);
+	REGCMP(edx);
+	REGCMP(esi);
+	REGCMP(edi);
+	REGCMP(ebp);
+	REGCMP(eip);
+	REGCMP(xfs);
+	REGCMP(xgs);
+	/* The following are eflags that have been observed to be
+	 * nondeterministic in practice.  We need to mask them off in
+	 * this comparison to prevent replay from diverging. */
+	enum {
+		/* The linux kernel has been observed to report this
+		 * as zero in some states during system calls. It
+		 * always seems to be 1 during user-space execution so
+		 * we should be able to ignore it. */
+		RESERVED_FLAG_1 = 1 << 1,
+		/* According to www.logix.cz/michal/doc/i386/chp04-01.htm
+		 *
+		 *   The RF flag temporarily disables debug exceptions
+		 *   so that an instruction can be restarted after a
+		 *   debug exception without immediately causing
+		 *   another debug exception. Refer to Chapter 12 for
+		 *   details.
+		 *
+		 * Chapter 12 isn't particularly clear on the point,
+		 * but the flag appears to be set by |int3|
+		 * exceptions.
+		 *
+		 * This divergence has been observed when continuing a
+		 * tracee to an execution target by setting an |int3|
+		 * breakpoint, which isn't used during recording.  No
+		 * single-stepping was used during the recording
+		 * either.
+		 */
+		RESUME_FLAG = 1 << 16,
+		/* It's no longer known why this bit is ignored. */
+		CPUID_ENABLED_FLAG = 1 << 21,
+	};
+	/* check the deterministic eflags */
+	const long det_mask =
+		~(RESERVED_FLAG_1 | RESUME_FLAG | CPUID_ENABLED_FLAG);
+	long eflags1 = (reg1->eflags & det_mask);
+	long eflags2 = (reg2->eflags & det_mask);
+	if (eflags1 != eflags2) {
+		maybe_print_reg_mismatch(mismatch_behavior, "deterministic eflags",
+					 name1, eflags1, name2, eflags2);
+		match = false;
+	}
+
+	return match;
 }
 
 template<typename T>
