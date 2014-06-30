c39efc424148b84d01e88b242f94db5605db7dc2
Robert O'Callahan <robert@ocallahan.org>
2014-06-30 16:48:26 +1200

    Add fork_stress test.

---
+++ src/test/fork_stress.c
@@ -0,0 +1,18 @@
+/* -*- Mode: C; tab-width: 8; c-basic-offset: 8; indent-tabs-mode: t; -*- */
+
+#include "rrutil.h"
+
+#define NUM_ITERATIONS 500
+
+int main(int argc, char *argv[]) {
+	int i;
+
+	for (i = 0; i < NUM_ITERATIONS; ++i) {
+		if (0 == fork()) {
+			return 0;
+		}
+	}
+
+	atomic_puts("EXIT-SUCCESS");
+	return 0;
+}
