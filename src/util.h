d3fb989b521806e98baabac1f4e1debf94961a7b
Nathan Froyd <froydnj@gmail.com>
2014-07-01 17:44:16 -0400

    add some protection against heap allocating restore_mem

--- src/util.h
+++ src/util.h
@@ -462,6 +462,11 @@ private:
 	void* saved_sp;
 	/* Length of tmp mem. */
 	size_t len;
+
+	restore_mem& operator=(const restore_mem&) = delete;
+	restore_mem(const restore_mem&) = delete;
+	void* operator new(size_t) = delete;
+	void operator delete(void*) = delete;
 };
 
 /**
