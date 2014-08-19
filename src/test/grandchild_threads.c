7d48427da6a2257e79a681fc42e8aa141a16071c
Robert O'Callahan <robert@ocallahan.org>
2014-08-20 11:44:33 +1200

    SIGKILL tests.

---
+++ src/test/grandchild_threads.c
@@ -0,0 +1,31 @@
+/* -*- Mode: C; tab-width: 8; c-basic-offset: 8; indent-tabs-mode: t; -*- */
+
+#include "rrutil.h"
+
+static void* start_thread(void* p)
+{
+	sleep(1000);
+	return NULL;
+}
+
+int main(int argc, char** argv) {
+	pid_t child;
+	pthread_t thread;
+	int pipe_fds[2];
+	char ch;
+
+	pipe(pipe_fds);
+	child = fork();
+	if (child > 0) {
+		read(pipe_fds[0], &ch, 1);
+		kill(child, 9);
+		/* try to exit before the child's exit */
+		return 0;
+	}
+
+	pthread_create(&thread, NULL, start_thread, NULL);
+	atomic_puts("EXIT-SUCCESS");
+	write(pipe_fds[1], &ch, 1);
+	sleep(1000);
+	return 0;
+}
