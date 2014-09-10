fd105294b320558192cb147e5741b9975d649749
Robert O'Callahan <robert@ocallahan.org>
2014-09-10 23:36:41 +1200

    Insert blank lines for easier reading.

--- src/types.h
+++ src/types.h
@@ -37,66 +37,93 @@ enum {
 struct Flags {
   /* Max counter value before the scheduler interrupts a tracee. */
   int max_rbc;
+
   /* Max number of trace events before the scheduler
    * de-schedules a tracee. */
   int max_events;
+
   /* Whenever |ignore_sig| is pending for a tracee, decline to
    * deliver it. */
   int ignore_sig;
+
   /* When true, echo tracee stdout/stderr writes to console. */
   bool redirect;
+
+  /* When true, use syscall buffering optimization during recording. */
   bool use_syscall_buffer;
+
   /* Path to librrpreload library. */
   std::string syscall_buffer_lib_path;
+
   enum { DUMP_ON_ALL = 10000, DUMP_ON_NONE = -DUMP_ON_ALL };
   /* event(s) to create memory dumps for */
   int dump_on; // event
+
   enum { DUMP_AT_NONE = -1 };
   /* time at which to create memory dump */
   int dump_at; // global time
+
   int checksum;
+
   /* IP port to listen on for debug connections. */
   int dbgport;
+
   /* Number of seconds to wait after startup, before starting
    * "real work". */
   int wait_secs;
+
   /* True when not-absolutely-urgently-critical messages will be
    * logged. */
   bool verbose;
+
   /* True when tracee processes in record and replay are allowed
    * to run on any logical CPU. */
   bool cpu_unbound;
+
   // Force rr to do some things that it otherwise wouldn't, for
   // example launching an emergency debugger when the output
   // doesn't seem to be a tty.
   bool force_things;
+
   /* Mark the trace global time along with tracee writes to
    * stdio. */
   bool mark_stdio;
+
   // Check that cached mmaps match /proc/maps after each event.
   bool check_cached_mmaps;
+
   // Suppress warnings related to environmental features outside rr's
   // control.
   bool suppress_environment_warnings;
+
   // Any warning or error that would be printed is treated as fatal
   bool fatal_errors_and_warnings;
+
   // Start a debug server for the task scheduled at the first
   // event at which reached this event AND target_process has
   // been "created".
   uint32_t goto_event;
+
   pid_t target_process;
+
   int process_created_how;
+
   // Dump trace frames in a more easily machine-parseable
   // format.
   bool raw_dump;
+
   // Dump statistics about the trace
   bool dump_statistics;
+
   // Dump syscallbuf contents
   bool dump_syscallbuf;
+
   // Only open a debug socket, don't launch the debugger too.
   bool dont_launch_debugger;
+
   // Pass this file name to debugger with -x
   std::string gdb_command_file_path;
+
   // User override for architecture detection, e.g. when running
   // under valgrind.
   std::string forced_uarch;
