b9e4b228d36c140940700b90aae9af8d05a25710
Robert O'Callahan <robert@ocallahan.org>
2014-09-27 17:11:16 -0400

    Fixup break_ptrace test to match changed rr behavior on abnormal trace termination.

--- src/test/break_ptrace.py
+++ src/test/break_ptrace.py
@@ -2,9 +2,6 @@ from rrutil import *
 
 send_gdb('c\n')
 expect_rr('doing dummy PTRACE_ATTACH')
-expect_gdb('Program received signal SIGTRAP')
-
-send_gdb('bt\n')
-expect_gdb('ptrace \(')
+expect_gdb('exited')
 
 ok()
