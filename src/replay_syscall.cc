851a9ad63002d27a857ccda53d990d977c989332
Nathan Froyd <froydnj@gmail.com>
2014-10-06 10:36:08 -0400

    add support for record/replay of msgctl

--- src/replay_syscall.cc
+++ src/replay_syscall.cc
@@ -1377,6 +1377,7 @@ static void rep_process_syscall_arch(Task* t, struct rep_trace_step* step) {
     case Arch::sendfile64:
     case Arch::waitid:
     case Arch::waitpid:
+    case Arch::msgctl:
       step->syscall.emu = EMULATE;
       step->syscall.emu_ret = EMULATE_RETURN;
       step->syscall.num_emu_args = 1;
