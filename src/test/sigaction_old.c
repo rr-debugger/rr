7cc75ec1e676bb185a8d6763049c15b60e55d238
Robert O'Callahan <robert@ocallahan.org>
2014-11-13 01:29:38 +1300

    Add test for rt_sigaction's out-parameter.

---
+++ src/test/sigaction_old.c
@@ -0,0 +1,32 @@
+/* -*- Mode: C; tab-width: 8; c-basic-offset: 2; indent-tabs-mode: nil; -*- */
+
+#include "rrutil.h"
+
+static void handler1(int sig, siginfo_t* si, void* p) {
+}
+
+static void handler2(int sig, siginfo_t* si, void* p) {
+}
+
+static void handler3(int sig, siginfo_t* si, void* p) {
+}
+
+int main(int argc, char* argv[]) {
+  struct sigaction sa;
+  struct sigaction old_sa;
+
+  sa.sa_sigaction = handler1;
+  sa.sa_flags = SA_SIGINFO;
+  sigaction(SIGUSR1, &sa, NULL);
+
+  sa.sa_sigaction = handler2;
+  old_sa.sa_sigaction = handler3;
+  sigaction(SIGUSR1, &sa, &old_sa);
+  test_assert(old_sa.sa_sigaction == handler1);
+
+  sigaction(SIGUSR1, NULL, &old_sa);
+  test_assert(old_sa.sa_sigaction == handler2);
+
+  atomic_puts("EXIT-SUCCESS");
+  return 0;
+}
