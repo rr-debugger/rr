f57574d1a5997632a4b976c56ea658cc3bd1b65c
Robert O'Callahan <robert@ocallahan.org>
2014-11-15 12:07:07 +1300

    Improve sched_setaffinity test.

--- src/test/sched_setaffinity.c
+++ src/test/sched_setaffinity.c
@@ -3,11 +3,20 @@
 #include "rrutil.h"
 
 int main(void) {
-  cpu_set_t cpus;
+  cpu_set_t* cpus;
+  cpu_set_t* cpus_out;
 
-  CPU_ZERO(&cpus);
-  CPU_SET(0, &cpus);
-  sched_setaffinity(0, sizeof(cpus), &cpus);
+  ALLOCATE_GUARD(cpus, 'x');
+  CPU_ZERO(cpus);
+  CPU_SET(0, cpus);
+  test_assert(0 == sched_setaffinity(0, sizeof(*cpus), cpus));
+  VERIFY_GUARD(cpus);
+
+  ALLOCATE_GUARD(cpus_out, 'x');
+  test_assert(0 == sched_getaffinity(0, sizeof(*cpus_out), cpus_out));
+  /* We can't assert this because rr assigns us random affinity itself.
+  test_assert(0 == memcmp(cpus, cpus_out, sizeof(*cpus))); */
+  VERIFY_GUARD(cpus_out);
 
   atomic_puts("EXIT-SUCCESS");
   return 0;
