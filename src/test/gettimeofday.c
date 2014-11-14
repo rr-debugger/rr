12bfe5ae58d30c453bc7b825a35449fb8c578550
Robert O'Callahan <robert@ocallahan.org>
2014-11-14 17:12:16 +1300

    Add test for gettimeofday.

---
+++ src/test/gettimeofday.c
@@ -0,0 +1,19 @@
+/* -*- Mode: C; tab-width: 8; c-basic-offset: 2; indent-tabs-mode: nil; -*- */
+
+#include "rrutil.h"
+
+int main(int argc, char* argv[]) {
+  struct timeval* tv;
+  struct timezone* tz;
+
+  ALLOCATE_GUARD(tv, 0);
+  ALLOCATE_GUARD(tz, 'x');
+  test_assert(0 == gettimeofday(tv, tz));
+  test_assert(tv->tv_sec > 0);
+  test_assert(tz->tz_dsttime == 0); /* always zero on Linux */
+  VERIFY_GUARD(tv);
+  VERIFY_GUARD(tz);
+
+  atomic_puts("EXIT-SUCCESS");
+  return 0;
+}  
