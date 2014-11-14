faed911495adc781f449dace92eb696a5ed0d1df
Robert O'Callahan <robert@ocallahan.org>
2014-11-14 17:06:52 +1300

    Add test for getrusage.

---
+++ src/test/rusage.c
@@ -0,0 +1,15 @@
+/* -*- Mode: C; tab-width: 8; c-basic-offset: 2; indent-tabs-mode: nil; -*- */
+
+#include "rrutil.h"
+
+int main(int argc, char* argv[]) {
+  struct rusage* r;
+
+  ALLOCATE_GUARD(r, 0);
+  test_assert(0 == getrusage(RUSAGE_SELF, r));
+  test_assert(r->ru_maxrss > 0);
+  VERIFY_GUARD(r);
+
+  atomic_puts("EXIT-SUCCESS");
+  return 0;
+}
