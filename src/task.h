cb49a60c8b660de181a1ea5084457b97e0052695
Robert O'Callahan <robert@ocallahan.org>
2015-01-05 21:42:16 +1300

    Add Task::getuid

--- src/task.h
+++ src/task.h
@@ -924,6 +924,12 @@ public:
   bool try_wait();
 
   /**
+   * Currently we don't allow recording across uid changes, so we can just
+   * return rr's uid.
+   */
+  uid_t getuid() { return ::getuid(); }
+
+  /**
    * Write |N| bytes from |buf| to |child_addr|, or don't
    * return.
    */
