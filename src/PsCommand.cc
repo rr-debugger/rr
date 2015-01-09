ecad930387618a368b22bdd30372a95cae396c5e
Robert O'Callahan <robert@ocallahan.org>
2015-01-09 16:01:58 +1300

    Print 'forked without exec' when the trace is abnormally terminated

--- src/PsCommand.cc
+++ src/PsCommand.cc
@@ -71,23 +71,27 @@ static int ps(const string& trace_dir, FILE* out) {
 
       // Look ahead for an EXEC in one of this process' threads.
       std::map<pid_t, pid_t> tmp_tid_to_pid = tid_to_pid;
+      bool found_exec = false;
       for (size_t j = i + 1; j < events.size(); ++j) {
         auto& ej = events[j];
 
         if (tmp_tid_to_pid[ej.tid()] == tmp_tid_to_pid[e.tid()] &&
             ej.type() == TraceTaskEvent::EXEC) {
           print_exec_cmd_line(events[j], out);
+          found_exec = true;
           break;
         }
 
         update_tid_to_pid_map(tmp_tid_to_pid, ej);
 
         if (ej.tid() == e.tid() && ej.type() == TraceTaskEvent::EXIT) {
-          // The main thread exited. All other threads must too, so there
-          // is no more opportunity for e's pid to exec.
-          fprintf(out, "(forked without exec)\n");
           break;
         }
+      }
+      if (!found_exec) {
+        // The main thread exited. All other threads must too, so there
+        // is no more opportunity for e's pid to exec.
+        fprintf(out, "(forked without exec)\n");
       }
     }
   }
