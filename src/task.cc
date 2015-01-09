95c7a472d1fbd9ca02059310a36fc72163e84216
Robert O'Callahan <robert@ocallahan.org>
2015-01-09 14:47:00 +1300

    Disable syscallbuf in forked/vforked children until it gets explicitly reinitialized

--- src/task.cc
+++ src/task.cc
@@ -1872,6 +1872,32 @@ Task* Task::clone(int flags, remote_ptr<void> stack, remote_ptr<void> tls,
   }
 
   t->as->insert_task(t);
+
+  if (!(CLONE_SHARE_VM & flags) && !syscallbuf_child.is_null() &&
+      &session() == &t->session()) {
+    AutoRemoteSyscalls remote(t);
+    // Unshare the syscallbuf header so when we lock it below, we don't
+    // also lock it in the task we cloned from!
+    int prot = PROT_READ | PROT_WRITE;
+    int flags = MAP_PRIVATE | MAP_FIXED | MAP_ANONYMOUS;
+    auto p = remote.mmap_syscall(syscallbuf_child, num_syscallbuf_bytes, prot,
+                                 flags, -1, 0);
+    ASSERT(t, p == syscallbuf_child.cast<void>());
+    t->vm()->map(p, num_syscallbuf_bytes, prot, flags, 0,
+        MappableResource::anonymous());
+
+    // Mark the clone's syscallbuf as locked. This will prevent the
+    // clone using syscallbuf until the clone reinitializes the
+    // the buffer via its pthread_atfork handler. Otherwise the clone may
+    // log syscalls to its copy of the syscallbuf and we won't know about
+    // them since we don't have it mapped.
+    // In some cases (e.g. vfork(), or raw SYS_fork syscall) the
+    // pthread_atfork handler will never run. Syscallbuf will be permanently
+    // disabled but that's OK, those cases are rare (and in the case of vfork,
+    // tracees should immediately exit or exec anyway).
+    t->write_mem(REMOTE_PTR_FIELD(syscallbuf_child, locked), uint8_t(1));
+  }
+
   return t;
 }
 
---
+++ src/test/vfork_flush.c
@@ -0,0 +1,30 @@
+/* -*- Mode: C; tab-width: 8; c-basic-offset: 2; indent-tabs-mode: nil; -*- */
+
+#include "rrutil.h"
+
+static void unblock_signals(void) {
+  sigset_t mask;
+  sigemptyset(&mask);
+  sigprocmask(SIG_SETMASK, &mask, NULL);
+}
+
+int main(int argc, char* argv[]) {
+  pid_t child;
+  int status;
+
+  if (0 == (child = vfork())) {
+    /* Unblocking SIGSYS should be OK */
+    unblock_signals();
+    test_assert(0 == close(0));
+    _exit(77);
+  }
+
+  test_assert(child == waitpid(child, &status, 0));
+  test_assert(WIFEXITED(status) && WEXITSTATUS(status) == 77);
+
+  /* Unblocking SIGSYS should be OK */
+  unblock_signals();
+
+  atomic_puts("EXIT-SUCCESS");
+  return 0;
+}
