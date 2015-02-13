27bb8502c1983bbaeb78dd0dfe68c576568f8a04
Robert O'Callahan <robert@ocallahan.org>
2015-02-13 21:55:54 +1100

    Add test that munmap works during replay

---
+++ src/test/munmap_segv.c
@@ -0,0 +1,23 @@
+/* -*- Mode: C; tab-width: 8; c-basic-offset: 2; indent-tabs-mode: nil; -*- */
+
+#include "rrutil.h"
+
+static void sighandler(int sig) {
+  atomic_puts("EXIT-SUCCESS");
+  _exit(0);
+}
+
+int main(int argc, char* argv[]) {
+  char* p = (char*)mmap(NULL, PAGE_SIZE, PROT_READ | PROT_WRITE,
+                        MAP_PRIVATE | MAP_ANONYMOUS, -1, 0);
+  test_assert(p != MAP_FAILED);
+
+  signal(SIGSEGV, sighandler);
+
+  *p = 'a';
+
+  test_assert(0 == munmap(p, PAGE_SIZE));
+  *p = 'b';
+
+  return 0;
+}
