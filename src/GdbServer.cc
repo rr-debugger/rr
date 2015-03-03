7fa5100cfd206418a0b27465846be59d80a6f5e3
Robert O'Callahan <robert@ocallahan.org>
2015-03-04 12:23:35 +1300

    Try 'set target-async' as well as 'maint set target-async'.

--- src/GdbServer.cc
+++ src/GdbServer.cc
@@ -121,6 +121,9 @@ static const char gdb_rr_macros[] =
     "define hookpost-run\n"
     "  set $suppress_run_hook = 0\n"
     "end\n"
+    // Try both "set target-async" and "maint set target-async" since
+    // that changed recently.
+    "set target-async 0\n"
     "maint set target-async 0\n"
     "handle SIGURG stop\n";
 
