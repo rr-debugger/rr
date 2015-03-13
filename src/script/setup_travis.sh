108d1ae11434c8d0dab90bfa1b3c5cef94de5fc5
Ted Mielczarek <ted@mielczarek.org>
2015-03-12 21:26:00 -0400

    Add some configure-style checks. Fixes #1112.
    Check that 32-bit cross-compilation works when on a 64-bit host.
    Check that a 32-bit pkgconfig dir exists when 64->32 cross-compiling.
    Check for required libraries (currently only zlib).
    Check for Python 2.7 and required Python modules (currently only pexpect).
    
    Fix the travis setup to run before cmake and to install all the required packages.

--- src/script/setup_travis.sh
+++ src/script/setup_travis.sh
@@ -12,7 +12,8 @@ sudo ./src/script/setup.sh
 packages=(linux-libc-dev linux-libc-dev:i386
 	  gcc-multilib libc6-dev:i386 rpm
 	  g++ lib32stdc++6
-	  zlib1g:i386 zlib1g-dev:i386)
+	  zlib1g:i386 zlib1g-dev:i386
+	  python-pexpect)
 
 sudo apt-get update && \
     sudo apt-get install "${packages[@]}"
