0e93e918db98a954fe9ddd54b1d062acf8d880c9
Robert O'Callahan <robert@ocallahan.org>
2015-04-29 16:19:24 +1200

    Don't call getsid(getsid(0)); our session leader may have exited.
    Not only may our session leader have exited, but its pid may have
    been reused for a completely unrelated process, so it makes no
    sense to call getsid on it.

--- src/test/getsid.c
+++ src/test/getsid.c
@@ -3,16 +3,9 @@
 #include "rrutil.h"
 
 int main(int argc, char* argv[]) {
-  pid_t sid1;
-  pid_t sid2;
-
-  sid1 = getsid(0);
-  atomic_printf("getsid(0) session ID: %d\n", sid1);
-  test_assert(sid1 > 0);
-  sid2 = getsid(sid1);
-  atomic_printf("getsid(getsid(0)) session ID: %d\n", sid2);
-  test_assert(sid2 > 0);
-  test_assert(sid1 == sid2);
+  pid_t sid = getsid(0);
+  atomic_printf("getsid(0) session ID: %d\n", sid);
+  test_assert(sid > 0);
 
   atomic_puts("EXIT-SUCCESS");
 
