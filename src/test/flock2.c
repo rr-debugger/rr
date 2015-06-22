0cbbfd1ad16f52e1e8b7c549af154a29e3ed22ab
Kavinda Wewegama <kavindaw@optumsoft.com>
2015-06-22 16:37:37 -0700

    Add support for flock syscall.

---
+++ src/test/flock2.c
@@ -0,0 +1,35 @@
+/* -*- Mode: C; tab-width: 8; c-basic-offset: 2; indent-tabs-mode: nil; -*- */
+/* Test for the 'flock' system call. */
+
+#include "rrutil.h"
+
+#define FILENAME "foo.txt"
+
+int main(int argc, char* argv[]) {
+  int fd;
+  int result;
+
+  fd = open(FILENAME, O_CREAT | O_EXCL | O_RDWR, 0600);
+  test_assert(fd >= 0);
+
+  result = flock(fd, LOCK_SH);
+  test_assert(result == 0);
+
+  result = flock(fd, LOCK_EX);
+  test_assert(result == 0);
+
+  result = flock(fd, LOCK_UN);
+  test_assert(result == 0);
+
+  result = close(fd);
+  test_assert(result == 0);
+
+  result = flock(fd, LOCK_EX);
+  test_assert(result < 0);
+  test_assert(errno == EBADF);
+
+  unlink(FILENAME);
+
+  atomic_puts("EXIT-SUCCESS");
+  return 0;
+}
