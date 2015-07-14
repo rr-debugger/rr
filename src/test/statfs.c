2dbfe45b7ac55bfb0f439a93d08b64ff846cc694
Robert O'Callahan <robert@ocallahan.org>
2015-07-15 00:05:27 +1200

    Check statfs flags

--- src/test/statfs.c
+++ src/test/statfs.c
@@ -10,12 +10,13 @@ static void dump_statfs(const char* label, const struct statfs* s) {
                 "    blocks:%ld, bfree:%ld, bavail:%ld,\n"
                 "    files:%lu, ffree:%lu,\n"
                 "    fsid: { %d, %d },\n"
-                "    namelen:%ld, frsize:%ld\n"
+                "    namelen:%ld, frsize:%ld,\n"
+                "    flags:0x%lx\n"
                 "}\n",
                 label, (long)s->f_type, (long)s->f_bsize, s->f_blocks,
                 s->f_bfree, s->f_bavail, s->f_files, s->f_ffree,
                 s->f_fsid.__val[0], s->f_fsid.__val[1], (long)s->f_namelen,
-                (long)s->f_frsize);
+                (long)s->f_frsize, (long)s->f_flags);
 }
 
 static int same_statfs_det(const struct statfs* s1, const struct statfs* s2) {
@@ -25,7 +26,8 @@ static int same_statfs_det(const struct statfs* s1, const struct statfs* s2) {
           s1->f_blocks == s2->f_blocks &&
           s1->f_fsid.__val[0] == s2->f_fsid.__val[0] &&
           s1->f_fsid.__val[1] == s2->f_fsid.__val[1] &&
-          s1->f_namelen == s2->f_namelen && s1->f_frsize == s2->f_frsize);
+          s1->f_namelen == s2->f_namelen && s1->f_frsize == s2->f_frsize &&
+          s1->f_flags == s2->f_flags);
 }
 
 int main(void) {
