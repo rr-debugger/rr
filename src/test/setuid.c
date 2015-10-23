3ffaa29e40fea5551d10d9292bbcaf8e2270ecb3
Petr Spacek <pspacek@redhat.com>
2015-10-23 20:15:15 +0200

    add support for setuid, setuid32 syscalls

---
+++ src/test/setuid.c
@@ -0,0 +1,27 @@
+/* -*- Mode: C; tab-width: 8; c-basic-offset: 2; indent-tabs-mode: nil; -*- */
+
+#include "rrutil.h"
+
+#include <sys/types.h>
+#include <unistd.h>
+
+int main(int argc, char* argv[]) {
+  uid_t orig;
+  uid_t new;
+  int ret;
+
+  orig = getuid();
+  new = orig + 1;
+  ret = setuid(new);
+  if (ret == -1) {
+    test_assert(errno == EPERM);
+    atomic_puts("Test did nothing because process does not have CAP_SETUID?");
+    atomic_puts("EXIT-SUCCESS");
+    return 0;
+  } else {
+    test_assert(getuid() == new);
+  }
+  atomic_puts("EXIT-SUCCESS");
+
+  return 0;
+}
