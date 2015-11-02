134371b2bee043aff825e2a550c5ed913849f731
Robert O'Callahan <robert@ocallahan.org>
2015-11-02 14:50:33 +1300

    Update release script

--- src/script/update-gh-pages.sh
+++ src/script/update-gh-pages.sh
@@ -5,48 +5,24 @@ function fatal { why=$1;
     exit 1
 }
 
-RELDIR="releases"
-
-major=$1
-minor=$2
-patch=$3
-dist=$4
-ver="$major.$minor.$patch"
-echo "Preparing for release '$ver' ..."
-if [[ $major == "" || $minor == "" || $patch == "" ]]; then
-    fatal "Usage: ./update-gh-pages.sh MAJOR MINOR PATCH DIST-FILES-DIR"
+rev=HEAD
+if [[ $1 != "" ]]; then
+  rev=$1
+fi
+ver=`git name-rev --name-only --tags $rev`
+if [[ $ver == undefined ]]; then
+  fatal "No tag found"
 fi
 
 echo "Updating repo ..."
-git fetch origin || fatal "Failed fetch from origin."
 git checkout gh-pages || fatal "Failed to checkout gh-pages branch."
 
-ARCHS="i686 x86_64"
-KERNELS="Linux"
-PACKS="deb rpm tar.gz"
-for arch in $ARCHS; do
-    for kernel in $KERNELS; do
-        for pack in $PACKS; do
-            package="rr-$ver-$kernel-$arch.$pack"
-            if [ -f "$dist/$package" ]; then
-                echo "Adding package to git ..."
-                cp "$dist/$package" $RELDIR/
-                git add $RELDIR/$package $RELDIR/$link
-            else
-                fatal "$dist/$package doesn't exist." 
-            fi
-        done
-    done
-done
-
 verfile=index.html
-echo "Patching $verfile with new version string ..."
-sed -i "s/rr-[0-9]*[.][0-9]*[.][0-9]*/rr-$ver/g" $verfile
+echo "Patching $verfile with new version $ver ..."
+sed -i "s/<span class=ver>[^<]*</<span class=ver>$ver</g" index.html
 
 echo "Showing diff ..."
 git diff -p -U8
-echo "Showing status ..."
-git status
 echo -n "Is this what you expected to see? [Y/n] "
 read ok
 if [[ $ok != "Y" ]]; then
