55ffeace93353a504c3b5e829157428e9a4baae3
Robert O'Callahan <robert@ocallahan.org>
2016-02-03 00:03:07 +1300

    In the desched_ticks test, handle the case where __before_poll_syscall_breakpoint is not hit

--- src/test/desched_ticks.py
+++ src/test/desched_ticks.py
@@ -1,4 +1,7 @@
 from rrutil import *
+import re
+
+send_gdb('handle SIGKILL stop')
 
 send_gdb('b main')
 expect_gdb('Breakpoint 1')
@@ -7,10 +10,11 @@ expect_gdb('Breakpoint 1')
 send_gdb('b __before_poll_syscall_breakpoint')
 expect_gdb('Breakpoint 2')
 send_gdb('c')
-expect_gdb('Breakpoint 2')
-# This is testing that we can reverse-step without crashing rr.
-send_gdb('reverse-stepi')
-send_gdb('c')
-expect_gdb('Breakpoint 2')
+index = expect_list([re.compile(r'Breakpoint 2'), re.compile(r'SIGKILL')])
+if index == 0:
+  # This is testing that we can reverse-step without crashing rr.
+  send_gdb('reverse-stepi')
+  send_gdb('c')
+  expect_gdb('Breakpoint 2')
 
 ok()
