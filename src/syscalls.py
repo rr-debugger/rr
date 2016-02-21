854bcb30b9bb2f033ecfb5831a3f0232510784dc
Robert O'Callahan <robert@ocallahan.org>
2016-02-21 22:45:55 +1300

    Support symlinkat syscall

--- src/syscalls.py
+++ src/syscalls.py
@@ -1418,7 +1418,7 @@ unlinkat = EmulatedSyscall(x86=301, x64=263)
 
 renameat = EmulatedSyscall(x86=302, x64=264)
 linkat = EmulatedSyscall(x86=303, x64=265)
-symlinkat = UnsupportedSyscall(x86=304, x64=266)
+symlinkat = EmulatedSyscall(x86=304, x64=266)
 readlinkat = IrregularEmulatedSyscall(x86=305, x64=267)
 fchmodat = UnsupportedSyscall(x86=306, x64=268)
 
---
+++ src/test/symlink.c
@@ -0,0 +1,16 @@
+/* -*- Mode: C; tab-width: 8; c-basic-offset: 2; indent-tabs-mode: nil; -*- */
+
+#include "rrutil.h"
+
+int main(void) {
+  static const char token_file[] = "rr-link-file.txt";
+  static const char link_name[] = "rr-link-file.link";
+
+  test_assert(0 == symlink(token_file, link_name));
+  test_assert(0 == unlink(link_name));
+  test_assert(0 == symlinkat(token_file, AT_FDCWD, link_name));
+  test_assert(0 == unlink(link_name));
+
+  atomic_puts("EXIT-SUCCESS");
+  return 0;
+}
