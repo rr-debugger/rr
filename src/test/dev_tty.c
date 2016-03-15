7dbbea4670814af3f50e04472d6ab14b7f8bc41b
Robert O'Callahan <robert@ocallahan.org>
2016-03-16 11:29:49 +1300

    Make test handle cases where /dev/tty isn't available

--- src/test/dev_tty.c
+++ src/test/dev_tty.c
@@ -4,6 +4,11 @@
 
 int main(void) {
   int fd = open("/dev/tty", O_RDWR);
+  if (fd == -1 && errno == ENXIO) {
+    atomic_puts("/dev/tty does not exist, skipping test");
+    atomic_puts("EXIT-SUCCESS");
+    return 0;
+  }
   test_assert(fd >= 0);
   test_assert(13 == write(fd, "EXIT-SUCCESS\n", 13));
   return 0;
