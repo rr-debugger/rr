263255225d8bc0d1a9f522b970cf52a384b0f9ae
Robert O'Callahan <robert@ocallahan.org>
2016-03-17 16:11:12 +1300

    Update RDRAND hack for 32-bit gcc 4.8.4

--- src/preload/preload.c
+++ src/preload/preload.c
@@ -2216,13 +2216,25 @@ void* XShmCreateImage(__attribute__((unused)) register void* dpy,
 
 typedef void* (*fopen_ptr)(const char* filename, const char* mode);
 
+static void random_device_init_helper(void* this) {
+  void** file_ptr = (void**)this;
+  void* f_ptr = dlsym(RTLD_DEFAULT, "fopen");
+  fopen_ptr fopen = (fopen_ptr)f_ptr;
+  *file_ptr = fopen("/dev/urandom", "rb");
+}
+
 /**
  * libstdc++3 uses RDRAND. Bypass that with this incredible hack.
  */
 void _ZNSt13random_device7_M_initERKNSt7__cxx1112basic_stringIcSt11char_traitsIcESaIcEEE(
     void* this, __attribute__((unused)) void* token) {
-  void** file_ptr = (void**)this;
-  void* f_ptr = dlsym(RTLD_DEFAULT, "fopen");
-  fopen_ptr fopen = (fopen_ptr)f_ptr;
-  *file_ptr = fopen("/dev/urandom", "rb");
+  random_device_init_helper(this);
+}
+
+/**
+ * gcc 4.8.4 in Ubuntu 14.04-32
+ */
+void _ZNSt13random_device7_M_initERKSs(void* this,
+                                       __attribute__((unused)) void* token) {
+  random_device_init_helper(this);
 }
