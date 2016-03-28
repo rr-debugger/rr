9e3227de9fc09170d002096be62df948b4120751
David Reiss <dreiss@fb.com>
2016-03-28 16:02:07 -0700

    Increase step count in reverse_step_threads_break
    Since fa7fb04, 20 steps wasn't enough to hit the breakpoint on my
    machine.  Maybe because I have libc debugging symbols installed and it
    was stepping into pthread_create?

--- src/test/reverse_step_threads_break.py
+++ src/test/reverse_step_threads_break.py
@@ -9,7 +9,7 @@ send_gdb('b breakpoint_thread')
 expect_gdb('Breakpoint 2')
 
 send_gdb('set scheduler-locking off')
-for i in xrange(20):
+for i in xrange(25):
   send_gdb('reverse-step')
 
 expect_gdb('Breakpoint 2')
