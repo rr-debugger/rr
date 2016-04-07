c043233bf69cbf439f8e90c80e673b9af27aa02a
Robert O'Callahan <robert@ocallahan.org>
2016-04-08 11:28:36 +1200

    Move ElfReader to its own file

---
+++ src/ElfReader.cc
@@ -0,0 +1,116 @@
+/* -*- Mode: C++; tab-width: 8; c-basic-offset: 2; indent-tabs-mode: nil; -*- */
+
+#include "ElfReader.h"
+
+#include <elf.h>
+
+#include "log.h"
+
+using namespace std;
+
+namespace rr {
+
+template <typename Arch>
+SymbolTable ElfReader::read_symbols_arch(const char* symtab,
+                                         const char* strtab) {
+  SymbolTable result;
+  typename Arch::ElfEhdr elfheader;
+  if (!read(0, elfheader) || memcmp(&elfheader, ELFMAG, SELFMAG) != 0 ||
+      elfheader.e_ident[EI_CLASS] != Arch::elfclass ||
+      elfheader.e_ident[EI_DATA] != Arch::elfendian ||
+      elfheader.e_machine != Arch::elfmachine ||
+      elfheader.e_shentsize != sizeof(typename Arch::ElfShdr) ||
+      elfheader.e_shstrndx >= elfheader.e_shnum) {
+    LOG(debug) << "Invalid ELF file: invalid header";
+    return result;
+  }
+
+  auto sections =
+      read<typename Arch::ElfShdr>(elfheader.e_shoff, elfheader.e_shnum);
+  if (sections.empty()) {
+    LOG(debug) << "Invalid ELF file: no sections";
+    return result;
+  }
+
+  auto& section_names_section = sections[elfheader.e_shstrndx];
+  auto section_names = read<char>(section_names_section.sh_offset,
+                                  section_names_section.sh_size);
+  if (section_names.empty()) {
+    LOG(debug) << "Invalid ELF file: can't read section names";
+    return result;
+  }
+  section_names[section_names.size() - 1] = 0;
+
+  typename Arch::ElfShdr* symbols = nullptr;
+  typename Arch::ElfShdr* strings = nullptr;
+  for (size_t i = 0; i < elfheader.e_shnum; ++i) {
+    auto& s = sections[i];
+    if (s.sh_name >= section_names.size()) {
+      LOG(debug) << "Invalid ELF file: invalid name offset for section " << i;
+      return result;
+    }
+    const char* name = section_names.data() + s.sh_name;
+    if (strcmp(name, symtab) == 0) {
+      if (symbols) {
+        LOG(debug) << "Invalid ELF file: duplicate symbol section " << symtab;
+        return result;
+      }
+      symbols = &s;
+    }
+    if (strcmp(name, strtab) == 0) {
+      if (strings) {
+        LOG(debug) << "Invalid ELF file: duplicate string section " << strtab;
+        return result;
+      }
+      strings = &s;
+    }
+  }
+
+  if (!symbols) {
+    LOG(debug) << "Invalid ELF file: missing symbol section " << symtab;
+    return result;
+  }
+  if (!strings) {
+    LOG(debug) << "Invalid ELF file: missing string section " << strtab;
+    return result;
+  }
+  if (symbols->sh_entsize != sizeof(typename Arch::ElfSym)) {
+    LOG(debug) << "Invalid ELF file: incorrect symbol size "
+               << symbols->sh_entsize;
+    return result;
+  }
+  if (symbols->sh_size % symbols->sh_entsize) {
+    LOG(debug) << "Invalid ELF file: incorrect symbol section size "
+               << symbols->sh_size;
+    return result;
+  }
+
+  auto symbol_list = read<typename Arch::ElfSym>(
+      symbols->sh_offset, symbols->sh_size / symbols->sh_entsize);
+  if (symbol_list.empty()) {
+    LOG(debug) << "Invalid ELF file: can't read symbols " << symtab;
+    return result;
+  }
+  result.strtab = read<char>(strings->sh_offset, strings->sh_size);
+  if (result.strtab.empty()) {
+    LOG(debug) << "Invalid ELF file: can't read strings " << strtab;
+  }
+  result.symbols.resize(symbol_list.size());
+  for (size_t i = 0; i < symbol_list.size(); ++i) {
+    auto& s = symbol_list[i];
+    if (s.st_shndx >= sections.size()) {
+      continue;
+    }
+    auto& section = sections[s.st_shndx];
+    result.symbols[i] = SymbolTable::Symbol(
+        s.st_value - section.sh_addr + section.sh_offset, s.st_name);
+  }
+  return result;
+}
+
+SymbolTable ElfReader::read_symbols(SupportedArch arch, const char* symtab,
+                                    const char* strtab) {
+  RR_ARCH_FUNCTION(read_symbols_arch, arch, symtab, strtab);
+}
+
+} // namespace rr
---
+++ src/ElfReader.h
@@ -0,0 +1,58 @@
+/* -*- Mode: C++; tab-width: 8; c-basic-offset: 2; indent-tabs-mode: nil; -*- */
+
+#ifndef RR_ELF_READER_H_
+#define RR_ELF_READER_H_
+
+#include <string.h>
+
+#include <memory>
+#include <vector>
+
+#include "kernel_abi.h"
+
+namespace rr {
+
+class SymbolTable {
+public:
+  bool is_name(size_t i, const char* name) const {
+    size_t offset = symbols[i].name_index;
+    return offset < strtab.size() && strcmp(&strtab[offset], name) == 0;
+  }
+  uintptr_t file_offset(size_t i) const { return symbols[i].file_offset; }
+  size_t size() const { return symbols.size(); }
+
+  struct Symbol {
+    Symbol(uintptr_t file_offset, size_t name_index)
+        : file_offset(file_offset), name_index(name_index) {}
+    Symbol() {}
+    uintptr_t file_offset;
+    size_t name_index;
+  };
+  std::vector<Symbol> symbols;
+  std::vector<char> strtab;
+};
+
+class ElfReader {
+public:
+  virtual ~ElfReader() {}
+  virtual bool read(size_t offset, size_t size, void* buf) = 0;
+  template <typename T> bool read(size_t offset, T& result) {
+    return read(offset, sizeof(result), &result);
+  }
+  template <typename T> std::vector<T> read(size_t offset, size_t count) {
+    std::vector<T> result;
+    result.resize(count);
+    if (!read(offset, sizeof(T) * count, result.data())) {
+      result.clear();
+    }
+    return result;
+  }
+  template <typename Arch>
+  SymbolTable read_symbols_arch(const char* symtab, const char* strtab);
+  SymbolTable read_symbols(SupportedArch arch, const char* symtab,
+                           const char* strtab);
+};
+
+} // namespace rr
+
+#endif /* RR_ELF_READER_H_ */
