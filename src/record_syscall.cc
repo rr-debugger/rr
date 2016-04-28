01c13a2b3f9469f56c8e1828581bd920e55ff51b
Robert O'Callahan <robert@ocallahan.org>
2016-04-28 22:54:09 +1200

    Support USBDEVFS_RELEASEINTERFACE ioctl

--- src/record_syscall.cc
+++ src/record_syscall.cc
@@ -1293,6 +1293,7 @@ static Switchable prepare_ioctl(RecordTask* t,
 
     case IOCTL_MASK_SIZE(USBDEVFS_CLAIMINTERFACE):
     case IOCTL_MASK_SIZE(USBDEVFS_SETINTERFACE):
+    case IOCTL_MASK_SIZE(USBDEVFS_RELEASEINTERFACE):
     case IOCTL_MASK_SIZE(USBDEVFS_SUBMITURB):
     case IOCTL_MASK_SIZE(USBDEVFS_SETCONFIGURATION):
       // Doesn't actually seem to write to userspace
