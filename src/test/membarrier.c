958f2cc5a42997a551f18a2505a7584f3d92bdcd
Robert O'Callahan <robert@ocallahan.org>
2016-06-07 14:14:26 +1200

    Support membarrier syscall.
    Resolves #1724

---
+++ src/test/membarrier.c
@@ -0,0 +1,21 @@
+/* -*- Mode: C; tab-width: 8; c-basic-offset: 2; indent-tabs-mode: nil; -*- */
+
+#include "rrutil.h"
+
+#define RR_MEMBARRIER_CMD_QUERY 0
+#define RR_MEMBARRIER_CMD_SHARED 1
+
+int main(void) {
+  int ret = syscall(RR_membarrier, RR_MEMBARRIER_CMD_QUERY, 0);
+  if (ret < 0 && errno == ENOSYS) {
+    atomic_puts("membarrier not supported, skipping test");
+  } else {
+    test_assert(ret >= 0);
+    if (ret & RR_MEMBARRIER_CMD_SHARED) {
+      test_assert(0 == syscall(RR_membarrier, RR_MEMBARRIER_CMD_SHARED, 0));
+    }
+  }
+
+  atomic_puts("EXIT-SUCCESS");
+  return 0;
+}
