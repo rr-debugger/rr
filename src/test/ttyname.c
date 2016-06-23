ada4dbcc9a54aa23bcf8c29ce0001c844511fa15
Robert O'Callahan <robert@ocallahan.org>
2016-06-23 19:18:32 +1200

    Exit gracefully if we're not attached to a tty at all

--- src/test/ttyname.c
+++ src/test/ttyname.c
@@ -5,6 +5,11 @@
 int main(void) {
   char* tty = ttyname(STDIN_FILENO);
   atomic_printf("ttyname = %s\n", tty);
+  if (!tty) {
+    atomic_puts("No tty attached to stdin");
+    fputs("EXIT-SUCCESS", stderr);
+    return 0;
+  }
   int fd = open(tty, O_RDWR);
   test_assert(13 == write(fd, "EXIT-SUCCESS\n", 13));
   return 0;
