ccd80ac4fcb10b0325735fb3eb976046d1f953b8
Robert O'Callahan <robert@ocallahan.org>
2016-06-24 15:34:26 +1200

    Stop on write syscall since __libc_start_main may not be findable

--- src/test/simple_script_debug.py
+++ src/test/simple_script_debug.py
@@ -1,6 +1,8 @@
 from rrutil import *
 
-send_gdb('b __libc_start_main')
+send_gdb('b write')
+# "Make breakpoint pending on future shared library load?"
+send_gdb('y')
 expect_gdb('Breakpoint 1')
 
 send_gdb('c')
