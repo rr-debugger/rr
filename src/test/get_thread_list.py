8faec1ced0ff557f4a90cf6d91b48b8002aee3ad
Robert O'Callahan <robert@ocallahan.org>
2016-06-27 19:18:40 +1200

    Make get_thread_list accept threads stopped in futex_wait
    New versions of glibc seem to have this happening.

--- src/test/get_thread_list.py
+++ src/test/get_thread_list.py
@@ -14,9 +14,16 @@ arch = get_exe_arch()
 # The locations the threads are stopped at depends on the architecture.
 stopped_locations = {
     # on i386, we sometimes stop in the middle of nowhere
-    'i386': ['(0x[0-9a-f]+ in )?__kernel_vsyscall', '(0x[0-9a-f]+ in )?_traced_raw_syscall', '0x[0-9a-f]+ in \?\?',
-             '(0x[0-9a-f]+ in )?__lll_lock_wait', '(0x[0-9a-f]+ in )?pthread_barrier_wait'],
-    'i386:x86-64': ['(0x[0-9a-f]+ in )?__lll_lock_wait', '(0x[0-9a-f]+ in )?pthread_barrier_wait', '0x0*70000002 in \?\?'],
+    'i386': ['(0x[0-9a-f]+ in )?__kernel_vsyscall',
+             '(0x[0-9a-f]+ in )?_traced_raw_syscall',
+             '0x[0-9a-f]+ in \?\?',
+             '(0x[0-9a-f]+ in )?__lll_lock_wait',
+             '(0x[0-9a-f]+ in )?pthread_barrier_wait',
+             '(0x[0-9a-f]+ in )?futex_wait'],
+    'i386:x86-64': ['(0x[0-9a-f]+ in )?__lll_lock_wait',
+                    '(0x[0-9a-f]+ in )?pthread_barrier_wait',
+                    '(0x[0-9a-f]+ in )?futex_wait',
+                    '0x0*70000002 in \?\?'],
 }
 
 for i in xrange(NUM_THREADS + 1, 1, -1):
@@ -26,7 +33,7 @@ for i in xrange(NUM_THREADS + 1, 1, -1):
     # did not number them in order of creation). This does not seem to be a bug
     # so tolerate it.
     send_gdb('info threads')
-    expect_gdb(r'%d\s+Thread[^(]+\(BP-THREAD-[0-9]+\) (?:%s) \(\)'%
+    expect_gdb(r'%d\s+Thread[^(]+\(BP-THREAD-[0-9]+\) (?:%s)'%
                (i, '|'.join(stopped_locations[arch])))
     expect_gdb(r'\(rr\)')
 
