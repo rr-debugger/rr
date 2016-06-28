bf801e2ef19c6325eb9661ba19b3901839a052a1
Robert O'Callahan <robert@ocallahan.org>
2016-06-28 18:31:33 +1200

    Skip installing pyexpect and gdb on Travis
    I can't get the Travis VM to recognize that pyexpect has been installed :-(

--- src/script/setup_travis.sh
+++ src/script/setup_travis.sh
@@ -5,7 +5,7 @@
 echo Configuring travis-ci build slave ...
 echo The slave is `uname -a`
 
-packages=(rpm ccache cmake make g++-multilib gdb pkg-config zlib1g-dev realpath python-pexpect zlib1g-dev)
+packages=(rpm ccache cmake make g++-multilib pkg-config realpath zlib1g-dev)
 
 sudo apt-get update && \
     sudo apt-get install -y "${packages[@]}" && \
