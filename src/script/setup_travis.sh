f829bf97767cc70cc8c22251f1487d71a20e1ce0
Robert O'Callahan <robert@ocallahan.org>
2016-06-28 15:39:06 +1200

    Update package list

--- src/script/setup_travis.sh
+++ src/script/setup_travis.sh
@@ -5,13 +5,8 @@
 echo Configuring travis-ci build slave ...
 echo The slave is `uname -a`
 
-packages=(linux-libc-dev linux-libc-dev:i386
-	  gcc-multilib libc6-dev:i386 rpm
-	  g++ lib32stdc++6 gdb
-	  zlib1g:i386 zlib1g-dev:i386
-	  python-pexpect)
+packages=(rpm ccache cmake make g++-multilib gdb pkg-config zlib1g-dev realpath python-pexpect zlib1g-dev)
 
 sudo apt-get update && \
-    sudo apt-get install "${packages[@]}"
-    sudo ln -s /usr/lib32/libstdc++.so.6 /usr/lib32/libstdc++.so && \
+    sudo apt-get install "${packages[@]}" && \
     echo ... finished configuring slave
