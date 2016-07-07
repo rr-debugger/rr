rr-debugger/rr

commit 732f60a1937beafe2dc5b677998cf9794dfbb032

Use TraceTaskEvent::CLONE instead of FORK for simplicity

src/PsCommand.cc
@@ -34,12 +34,14 @@ static void print_exec_cmd_line(const TraceTaskEvent& event, FILE* out) {
 
 static void update_tid_to_pid_map(map<pid_t, pid_t>& tid_to_pid,
                                   const TraceTaskEvent& e) {
-  if (e.is_fork()) {
-    // Some kind of fork. This task is its own pid.
-    tid_to_pid[e.tid()] = e.tid();
-  } else if (e.type() == TraceTaskEvent::CLONE) {
-    // thread clone. Record thread's pid.
-    tid_to_pid[e.tid()] = tid_to_pid[e.parent_tid()];
+  if (e.type() == TraceTaskEvent::CLONE) {
+    if (e.clone_flags() & CLONE_THREAD) {
+      // thread clone. Record thread's pid.
+      tid_to_pid[e.tid()] = tid_to_pid[e.parent_tid()];
+    } else {
+      // Some kind of fork. This task is its own pid.
+      tid_to_pid[e.tid()] = e.tid();
+    }
   } else if (e.type() == TraceTaskEvent::EXIT) {
     tid_to_pid.erase(e.tid());
   }
@@ -121,7 +123,8 @@ static int ps(const string& trace_dir, FILE* out) {
     auto& e = events[i];
     update_tid_to_pid_map(tid_to_pid, e);
 
-    if (e.is_fork()) {
+    if (e.type() == TraceTaskEvent::CLONE &&
+        !(e.clone_flags() | CLONE_THREAD)) {
       pid_t pid = tid_to_pid[e.tid()];
       fprintf(out, "%d\t%d\t%d\t", e.tid(), tid_to_pid[e.parent_tid()],
               find_exit_code(pid, events, i, tid_to_pid));

src/TraceStream.cc
@@ -31,7 +31,7 @@ namespace rr {
 // MUST increment this version number.  Otherwise users' old traces
 // will become unreplayable and they won't know why.
 //
-#define TRACE_VERSION 59
+#define TRACE_VERSION 60
 
 struct SubstreamData {
   const char* name;
@@ -289,9 +289,6 @@ void TraceWriter::write_task_event(const TraceTaskEvent& event) {
     case TraceTaskEvent::CLONE:
       tasks << event.parent_tid() << event.clone_flags();
       break;
-    case TraceTaskEvent::FORK:
-      tasks << event.parent_tid();
-      break;
     case TraceTaskEvent::EXEC:
       tasks << event.file_name() << event.cmd_line() << event.fds_to_close();
       break;
@@ -311,9 +308,6 @@ TraceTaskEvent TraceReader::read_task_event() {
   switch (r.type()) {
     case TraceTaskEvent::CLONE:
       tasks >> r.parent_tid_ >> r.clone_flags_;
-      break;
-    case TraceTaskEvent::FORK:
-      tasks >> r.parent_tid_;
       break;
     case TraceTaskEvent::EXEC:
       tasks >> r.file_name_ >> r.cmd_line_ >> r.fds_to_close_;

src/TraceTaskEvent.h
@@ -23,19 +23,13 @@ class TraceTaskEvent {
 public:
   enum Type {
     NONE,
-    CLONE, // created by clone(2) syscall
-    FORK,  // created by fork(2) or vfork(2) syscalls
+    CLONE, // created by clone(2), fork(2), vfork(2) syscalls
     EXEC,
     EXIT
   };
 
   TraceTaskEvent(Type type = NONE, pid_t tid = 0) : type_(type), tid_(tid) {}
 
-  static TraceTaskEvent for_fork(pid_t tid, pid_t parent_tid) {
-    TraceTaskEvent result(FORK, tid);
-    result.parent_tid_ = parent_tid;
-    return result;
-  }
   static TraceTaskEvent for_clone(pid_t tid, pid_t parent_tid,
                                   uint32_t clone_flags) {
     TraceTaskEvent result(CLONE, tid);
@@ -59,7 +53,7 @@ public:
   Type type() const { return type_; }
   pid_t tid() const { return tid_; }
   pid_t parent_tid() const {
-    assert(type() == CLONE || type() == FORK);
+    assert(type() == CLONE);
     return parent_tid_;
   }
   uintptr_t clone_flags() const {
@@ -81,10 +75,6 @@ public:
   WaitStatus exit_status() const {
     assert(type() == EXIT);
     return exit_status_;
-  }
-
-  bool is_fork() const {
-    return type() == FORK || (type() == CLONE && !(clone_flags() & CLONE_VM));
   }
 
   void set_fds_to_close(const std::vector<int> fds) {

src/record_syscall.cc
@@ -2187,7 +2187,7 @@ static int ptrace_option_for_event(int ptrace_event) {
 
 template <typename Arch>
 static void prepare_clone(RecordTask* t, TaskSyscallState& syscall_state) {
-  uintptr_t flags = 0;
+  uintptr_t flags;
   CloneParameters params;
   Registers r = t->regs();
   int original_syscall = r.original_syscallno();
@@ -2209,9 +2209,10 @@ static void prepare_clone(RecordTask* t, TaskSyscallState& syscall_state) {
     }
   } else if (is_vfork_syscall(original_syscall, r.arch())) {
     ptrace_event = PTRACE_EVENT_VFORK;
-    flags = CLONE_VM;
+    flags = CLONE_VM | CLONE_VFORK | SIGCHLD;
   } else {
     ptrace_event = PTRACE_EVENT_FORK;
+    flags = SIGCHLD;
   }
 
   while (true) {
@@ -2285,13 +2286,9 @@ static void prepare_clone(RecordTask* t, TaskSyscallState& syscall_state) {
     }
     new_task->record_remote_even_if_null(child_params.ptid);
     new_task->record_remote_even_if_null(child_params.ctid);
-
-    t->session().trace_writer().write_task_event(
-        TraceTaskEvent::for_clone(new_task->tid, t->tid, flags));
-  } else {
-    t->session().trace_writer().write_task_event(
-        TraceTaskEvent::for_fork(new_task->tid, t->tid));
   }
+  t->session().trace_writer().write_task_event(
+      TraceTaskEvent::for_clone(new_task->tid, t->tid, flags));
 
   init_scratch_memory(new_task);
 

src/replay_syscall.cc
@@ -221,9 +221,7 @@ template <typename Arch> static void prepare_clone(ReplayTask* t) {
   // Dig the recorded tid out out of the trace. The tid value returned in
   // the recorded registers could be in a different pid namespace from rr's,
   // so we can't use it directly.
-  TraceTaskEvent tte = read_task_trace_event(
-      t, Arch::clone == t->regs().original_syscallno() ? TraceTaskEvent::CLONE
-                                                       : TraceTaskEvent::FORK);
+  TraceTaskEvent tte = read_task_trace_event(t, TraceTaskEvent::CLONE);
   ASSERT(t, tte.parent_tid() == t->rec_tid);
   long rec_tid = tte.tid();
   pid_t new_tid = t->get_ptrace_eventmsg<pid_t>();

commit 8ca00d520e94f90762413b75dc9d6a15d78d2353

Fix stupid regression in `rr ps`
Regression introduced by 732f60a1937beafe2dc5b677998cf9794dfbb032

CMakeLists.txt
@@ -737,6 +737,8 @@ set(TESTS_WITH_PROGRAM
   mprotect_syscallbuf_overflow
   mutex_pi_stress
   priority
+  # Not called ps, because that interferes with using real 'ps' in tests
+  rr_ps
   read_big_struct
   restart_abnormal_exit
   reverse_continue_breakpoint

src/PsCommand.cc
@@ -124,7 +124,7 @@ static int ps(const string& trace_dir, FILE* out) {
     update_tid_to_pid_map(tid_to_pid, e);
 
     if (e.type() == TraceTaskEvent::CLONE &&
-        !(e.clone_flags() | CLONE_THREAD)) {
+        !(e.clone_flags() & CLONE_THREAD)) {
       pid_t pid = tid_to_pid[e.tid()];
       fprintf(out, "%d\t%d\t%d\t", e.tid(), tid_to_pid[e.parent_tid()],
               find_exit_code(pid, events, i, tid_to_pid));

src/test/rr_ps.c
@@ -1,0 +1,38 @@
+/* -*- Mode: C; tab-width: 8; c-basic-offset: 2; indent-tabs-mode: nil; -*- */
+
+#include "rrutil.h"
+
+int main(int argc, __attribute__((unused)) char* argv[]) {
+  pid_t child;
+  int status;
+
+  if (argc > 1) {
+    return 88;
+  }
+
+  child = fork();
+  if (!child) {
+    return 77;
+  }
+  test_assert(child == wait(&status));
+  test_assert(WIFEXITED(status) && WEXITSTATUS(status) == 77);
+
+  child = fork();
+  if (!child) {
+    kill(getpid(), SIGSEGV);
+  }
+  test_assert(child == wait(&status));
+  test_assert(WIFSIGNALED(status) && WTERMSIG(status) == SIGSEGV);
+
+  child = fork();
+  if (!child) {
+    char* args[] = { argv[0], "token", NULL };
+    execve(argv[0], args, environ);
+    test_assert(0 && "execve failed");
+  }
+  test_assert(child == wait(&status));
+  test_assert(WIFEXITED(status) && WEXITSTATUS(status) == 88);
+
+  atomic_puts("EXIT-SUCCESS");
+  return 0;
+}

src/test/rr_ps.run
@@ -1,0 +1,14 @@
+source `dirname $0`/util.sh
+compare_test EXIT-SUCCESS
+
+if !(do_ps | grep -q $'\t77'); then
+    failed "Can't find return value 77"
+fi
+
+if !(do_ps | grep -q -- $'\t-11'); then
+    failed "Can't find return value -11"
+fi
+
+if !(do_ps | grep $'\t88' | grep -q token); then
+    failed "Can't find exec parameter 'token'"
+fi

src/test/util.sh
@@ -221,6 +221,10 @@ function replay { replayflags=$1
         rr $GLOBAL_OPTIONS replay -a $replayflags 1> replay.out 2> replay.err
 }
 
+function do_ps { psflags=$1
+    _RR_TRACE_DIR="$workdir" rr $GLOBAL_OPTIONS ps $psflags
+}
+
 #  debug <expect-script-name> [replay-args]
 #
 # Load the "expect" script to drive replay of the recording of |exe|.