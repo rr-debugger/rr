693c5b340190745f683eaa679d6402fd86f0869c
Keno Fischer <keno@juliacomputing.com>
2016-09-21 15:45:16 -0400

    Fix reverse-stepping from a mapped-but-not-exectuable ip
    Fixes #1816

---
+++ src/test/invalid_jump.c
@@ -0,0 +1,17 @@
+/* -*- Mode: C; tab-width: 8; c-basic-offset: 2; indent-tabs-mode: nil; -*- */
+
+#include "rrutil.h"
+
+static void sighandler(int sig) {
+  atomic_printf("caught signal %d, exiting\n", sig);
+  _exit(0);
+}
+
+char invalid_jump_here[] = { 0x00, 0x00, 0x00, 0x00, 0x00 };
+
+int main(void) {
+  // Just for clean exit to not worry people running the test manually ;).
+  signal(SIGSEGV, sighandler);
+  goto *(void*)invalid_jump_here;
+  assert(0 && "Shouldn't reach here");
+}
