694dc1c59314241d0073ef3308d5bc353e27746a
Kyle Huey <khuey@kylehuey.com>
2016-09-22 16:32:18 -0700

    Rework syscallbuf initialization to not depend on using pthreads/glibc.
    Syscall buffer initialization is currently implemented by hooking pthread_create and using pthread_at_fork handlers.  Programs that do not use pthread_create for new threads or glibc's fork to fork will not get their syscall buffers reinitialized in the child.  Chromium forks by calling clone(2) manually, so syscall buffering is currently disabled for child processes.
    
    This commit replaces those hooks with a setup based on handling a synthesized SYSCALLBUF_DESCHED_SIGNAL that is dispatched to the newly created tracee task by rr.

--- src/test/reverse_continue_fork_subprocess.py
+++ src/test/reverse_continue_fork_subprocess.py
@@ -22,8 +22,7 @@ expect_gdb('stopped')
 
 send_gdb('stepi')
 send_gdb('reverse-stepi')
-send_gdb('reverse-stepi')
-expect_gdb('stopped')
+expect_gdb("<signal handler called>"); # SYSCALLBUF_DESCHED_SIGNAL
 send_gdb('reverse-cont')
 expect_gdb('stopped')
 
