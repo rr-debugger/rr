f7bcb9a74a26f2f8ed359f6fe60522431ae8dc6d
Keno Fischer <keno@juliacomputing.com>
2016-09-22 17:21:20 -0400

    Add ptrace remote unmapping test

---
+++ src/test/ptrace_remote_unmap.py
@@ -0,0 +1,14 @@
+from rrutil import *
+
+send_gdb('break breakpoint')
+expect_gdb('Breakpoint 1')
+send_gdb('c')
+expect_gdb('Breakpoint 1')
+send_gdb('checkpoint')
+expect_gdb('Checkpoint 1 at')
+send_gdb('restart 1')
+expect_gdb('stopped')
+send_gdb('rc')
+expect_gdb('stopped')
+
+ok()
