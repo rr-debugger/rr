50ad3fec2b889d40a7300902641b2307b1023f25
Robert O'Callahan <robert@ocallahan.org>
2016-10-21 20:36:20 +1300

    Speed up step_thread
    step_thread takes 95 seconds on my (fast) laptop. This is nearly the timeout,
    and sometimes it does time out in automation.
    
    It's slow because it single-steps all threads between ready() and hit_barrier().
    Speed that up by moving thread creation out of that execution region, to before
    ready().

--- src/test/step_thread.c
+++ src/test/step_thread.c
@@ -7,24 +7,30 @@ pthread_barrier_t bar;
 /* NB: these must *not* be macros so that debugger step-next works as
  * expected per the program source. */
 static void A(void) {
-  pthread_barrier_wait(&bar);
-  pthread_barrier_wait(&bar);
+  pthread_barrier_wait(&bar); // Barrier 3
+  pthread_barrier_wait(&bar); // Barrier 4
 }
 static void B(void) {
-  pthread_barrier_wait(&bar);
-  pthread_barrier_wait(&bar);
+  pthread_barrier_wait(&bar); // Barrier 3
+  pthread_barrier_wait(&bar); // Barrier 4
 }
 
 static void* threadA(__attribute__((unused)) void* unused) {
+  pthread_barrier_wait(&bar); // Barrier 1
+  pthread_barrier_wait(&bar); // Barrier 2
   A();
   return NULL;
 }
 static void* threadB(__attribute__((unused)) void* unused) {
+  pthread_barrier_wait(&bar); // Barrier 1
+  pthread_barrier_wait(&bar); // Barrier 2
   B();
   return NULL;
 }
 
-static void C(void) { pthread_barrier_wait(&bar); }
+static void C(void) {
+  pthread_barrier_wait(&bar); // Barrier 3
+}
 
 static void hit_barrier(void) {
   int break_here = 1;
@@ -38,23 +44,23 @@ static void ready(void) {
 }
 
 int main(void) {
-  void* dummy;
   pthread_t a, b;
 
   pthread_barrier_init(&bar, NULL, 3);
-  dummy = pthread_create;
-  dummy = pthread_barrier_wait;
-  (void)dummy;
-
-  ready();
 
   pthread_create(&a, NULL, threadA, NULL);
   pthread_create(&b, NULL, threadB, NULL);
 
+  pthread_barrier_wait(&bar); // Barrier 1
+
+  ready();
+
+  pthread_barrier_wait(&bar); // Barrier 2
+
   C();
   hit_barrier();
 
-  pthread_barrier_wait(&bar);
+  pthread_barrier_wait(&bar); // Barrier 4
 
   pthread_join(a, NULL);
   pthread_join(b, NULL);
