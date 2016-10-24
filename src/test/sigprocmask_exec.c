97d7a5f6b61dd4717a59a0280119d59b9889b7e7
Keno Fischer <keno@juliacomputing.com>
2016-10-23 20:40:52 -0400

    Don't lose signal mask manipulations that happen right before exec
    We were buffering the signal mask in the syscallbuf_hdr, but neglecting
    to reload it before throwing it away during the exec, causing a
    divergence between rr's and the kernel's understanding of the signal mask.
    This was triggered on Ubuntu 16.10 during the ptrace_remote_unmap test,
    because `raise` now calls sigprocmask (and in that test we call raise
    right before exec). However, I still added a test for this specific problem
    to avoid depending on libc implementation details.

---
+++ src/test/sigprocmask_exec.c
@@ -0,0 +1,21 @@
+#include "rrutil.h"
+
+int main(int argc, char* argv[]) {
+  sigset_t set;
+  test_assert(argc == 1 || (argc == 2 && !strcmp("self", argv[1])));
+
+  if (argc != 2) {
+    char* argv[] = { "/proc/self/mem", "self", 0 };
+    sigemptyset(&set);
+    sigaddset(&set, SIGSEGV);
+    sigprocmask(SIG_SETMASK, &set, NULL);
+    execv("/proc/self/exe", argv);
+    test_assert("Not reached" && 0);
+  }
+
+  sigprocmask(SIG_SETMASK, NULL, &set);
+  test_assert(sigismember(&set, SIGSEGV));
+
+  atomic_puts("EXIT-SUCCESS");
+  return 0;
+}
