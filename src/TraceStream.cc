aa6e281979e939b4576113a8fbc1d9bcc2f4b326
Robert O'Callahan <robert@ocallahan.org>
2016-10-25 20:34:20 +1300

    Update trace version for new EV_SECCOMP_TRAP event among other things

--- src/TraceStream.cc
+++ src/TraceStream.cc
@@ -31,7 +31,7 @@ namespace rr {
 // MUST increment this version number.  Otherwise users' old traces
 // will become unreplayable and they won't know why.
 //
-#define TRACE_VERSION 65
+#define TRACE_VERSION 66
 
 struct SubstreamData {
   const char* name;
