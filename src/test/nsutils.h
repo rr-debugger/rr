c91a5f0e2cb527d2ea13c0090601f03291d3b67c
Keno Fischer <keno@juliacomputing.com>
2016-12-29 15:17:43 -0500

    Support prctls modifying capabilities

--- src/test/nsutils.h
+++ src/test/nsutils.h
@@ -5,6 +5,15 @@
 
 #include "rrutil.h"
 
+static int try_setup_ns_internal(int ns_kind, int expect_to_be_root);
+static inline int try_setup_ns_no_root(int ns_kind) {
+  return try_setup_ns_internal(ns_kind, 0);
+}
+
+static inline int try_setup_ns(int ns_kind) {
+  return try_setup_ns_internal(ns_kind, 1);
+}
+
 static int open_proc_file(pid_t pid, const char* file, int mode) {
   char path[100];
   ssize_t n = snprintf(path, sizeof(path), "/proc/%d/%s", pid, file);
@@ -14,11 +23,15 @@ static int open_proc_file(pid_t pid, const char* file, int mode) {
   return fd;
 }
 
-static int try_setup_ns(int ns_kind) {
-  int err = unshare(ns_kind);
-  if (err == -1) {
+static int try_setup_ns_internal(int ns_kind, int expect_to_be_root) {
+  int err = -1;
+  errno = 0;
+  if (ns_kind != CLONE_NEWUSER) {
+    err = unshare(ns_kind);
+  }
+  if (ns_kind == CLONE_NEWUSER || err == -1) {
     // Try again using user namespace functionality
-    test_assert(errno == EPERM);
+    test_assert(errno == 0 || errno == EPERM);
 
     int child_block[2], parent_block[2];
     pipe(child_block);
@@ -44,7 +57,12 @@ static int try_setup_ns(int ns_kind) {
       int uidmap_fd = open_proc_file(pid, "uid_map", O_WRONLY);
       test_assert(uidmap_fd != -1);
       char uidmap[100];
-      nbytes = snprintf(uidmap, (ssize_t)sizeof(uidmap), "0\t%d\t1", getuid());
+      if (expect_to_be_root)
+        nbytes =
+            snprintf(uidmap, (ssize_t)sizeof(uidmap), "0\t%d\t1", getuid());
+      else
+        nbytes =
+            snprintf(uidmap, (ssize_t)sizeof(uidmap), "1000\t%d\t1", getuid());
       test_assert(nbytes > 0 && nbytes <= (ssize_t)sizeof(uidmap));
       test_assert(write(uidmap_fd, uidmap, nbytes) == nbytes);
       test_assert(uidmap_fd);
@@ -52,7 +70,12 @@ static int try_setup_ns(int ns_kind) {
       int gidmap_fd = open_proc_file(pid, "gid_map", O_WRONLY);
       test_assert(gidmap_fd != -1);
       char gidmap[100];
-      nbytes = snprintf(gidmap, (ssize_t)sizeof(gidmap), "0\t%d\t1", getgid());
+      if (expect_to_be_root)
+        nbytes =
+            snprintf(gidmap, (ssize_t)sizeof(gidmap), "0\t%d\t1", getgid());
+      else
+        nbytes =
+            snprintf(gidmap, (ssize_t)sizeof(gidmap), "1000\t%d\t1", getgid());
       test_assert(nbytes > 0 && nbytes <= (ssize_t)sizeof(gidmap));
       test_assert(write(gidmap_fd, gidmap, nbytes) == nbytes);
 
