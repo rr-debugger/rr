757d46c63724330ab0d61e0105f1de17747f180e
Robert O'Callahan <robert@ocallahan.org>
2017-01-13 17:47:49 +1300

    Bump TRACE_VERSION due to changes to record syscallbuf_fds_disabled in the trace

--- src/TraceStream.cc
+++ src/TraceStream.cc
@@ -32,7 +32,7 @@ namespace rr {
 // MUST increment this version number.  Otherwise users' old traces
 // will become unreplayable and they won't know why.
 //
-#define TRACE_VERSION 69
+#define TRACE_VERSION 70
 
 struct SubstreamData {
   const char* name;
