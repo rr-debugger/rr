a307851c512f7b595a59ad0cb046d63ebcdc3aa5
Robert O'Callahan <robert@ocallahan.org>
2017-05-27 14:17:54 +1200

    Add simple_threads_stress test

---
+++ src/test/simple_threads_stress.c
@@ -0,0 +1,18 @@
+#include "rrutil.h"
+
+static void* do_thread(__attribute__((unused)) void* p) {
+  return 0;
+}
+
+int main(void) {
+  int i;
+  pthread_t thread[100];
+  for (i = 0; i < 100; ++i) {
+    pthread_create(&thread[i], NULL, do_thread, NULL);
+  }
+  for (i = 0; i < 100; ++i) {
+    pthread_join(thread[i], NULL);
+  }
+  atomic_puts("EXIT-SUCCESS");
+  return 0;
+}
