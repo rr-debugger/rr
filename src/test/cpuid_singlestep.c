21957580bc7351b9f0b4eea8abcbe07aa277b23a
Robert O'Callahan <robert@ocallahan.org>
2017-07-12 07:22:38 -0700

    Add test for CPUID singlestepping with faulting during replay but not recording

---
+++ src/test/cpuid_singlestep.c
@@ -0,0 +1,21 @@
+/* -*- Mode: C; tab-width: 8; c-basic-offset: 2; indent-tabs-mode: nil; -*- */
+
+#include "rrutil.h"
+
+static void cpuid(int code, int subrequest, unsigned int* a, unsigned int* c,
+                  unsigned int* d) {
+  asm volatile("cpuid_instruction_label: cpuid"
+               : "=a"(*a), "=c"(*c), "=d"(*d)
+               : "a"(code), "c"(subrequest)
+               : "ebx");
+}
+
+int main(void) {
+  unsigned int a, c, d;
+  /* CX ignored for AX==0 */
+  cpuid(0, 2000, &a, &c, &d);
+  test_assert(a > 0);
+
+  atomic_puts("EXIT-SUCCESS");
+  return 0;
+}
---
+++ src/test/cpuid_singlestep.py
@@ -0,0 +1,26 @@
+from rrutil import *
+import re
+
+send_gdb('break *cpuid_instruction_label')
+expect_gdb('Breakpoint 1')
+send_gdb('c')
+expect_gdb('Breakpoint 1')
+expect_gdb('(rr)')
+
+send_gdb('p $pc')
+expect_gdb(re.compile(r'(0x[a-f0-9]+)'))
+pc = eval(last_match().group(1));
+expect_gdb('(rr)')
+send_gdb('stepi')
+expect_gdb('(rr)')
+
+send_gdb('p $pc')
+expect_gdb(re.compile(r'(0x[a-f0-9]+)'))
+pc2 = eval(last_match().group(1));
+if pc2 - pc != 2:
+    failed('Expected 0x%x after singlestep, got 0x%x'%(pc + 2, pc2))
+
+send_gdb('c')
+expect_gdb('EXIT-SUCCESS')
+
+ok()
