6b81c5d207c76ce4aafa8ee2ab25ed4b48c6c805
Robert O'Callahan <robert@ocallahan.org>
2017-07-31 15:38:18 +1200

    Revert "Switch compression from zlib to brotli"
    This reverts commit efb69952e2cdfd616d0bf39a6acbcb8f928f0882.
    
    Ubuntu doesn't package a brotli library so this would make rr difficult to build on Ubuntu.

--- src/TraceStream.cc
+++ src/TraceStream.cc
@@ -32,7 +32,7 @@ namespace rr {
 // MUST increment this version number.  Otherwise users' old traces
 // will become unreplayable and they won't know why.
 //
-#define TRACE_VERSION 74
+#define TRACE_VERSION 73
 
 struct SubstreamData {
   const char* name;
