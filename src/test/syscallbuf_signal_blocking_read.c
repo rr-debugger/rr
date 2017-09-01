de263cb43628b96667c589580e647d2faeaa9d27
Robert O'Callahan <robert@ocallahan.org>
2017-09-01 14:26:50 +1200

    Make syscallbuf_signal_blocking_read allocate memory properly
    We don't read any data, but if the buffer pointer plus the size wraps around
    the end of a 32-bit address space then we'll get EFAULT even so.

--- src/test/syscallbuf_signal_blocking_read.c
+++ src/test/syscallbuf_signal_blocking_read.c
@@ -4,15 +4,15 @@
 
 int main(void) {
   int fd = open("dummy.txt", O_RDWR | O_CREAT | O_EXCL, 0700);
+  char* buf = malloc(100 * 1024 * 1024);
   int i;
   unlink("dummy.txt");
 
   for (i = 0; i < 1000; ++i) {
-    char buf;
     /* Do a large read that can't be buffered so will cause a traced
        read, though we will try to do an ioctl FIOCLONERANGE on it first
        (which we're testing for interference with many TIME_SLICE_SIGNALs) */
-    ssize_t ret = read(fd, &buf, 100 * 1024 * 1024);
+    ssize_t ret = read(fd, buf, 100 * 1024 * 1024);
     test_assert(ret == 0);
   }
 
