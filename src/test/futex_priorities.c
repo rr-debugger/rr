bb016bb9e38d9c774dd16bab5a40ec0cf8e92c32
Robert O'Callahan <robert@ocallahan.org>
2017-09-02 17:53:00 +1200

    Make futex_priorities test more robust

--- src/test/futex_priorities.c
+++ src/test/futex_priorities.c
@@ -2,16 +2,30 @@
 
 #include "util.h"
 
-static int v;
-static int v2;
+static volatile int v;
+static volatile int v2;
 
 static void* run_thread(__attribute__((unused)) void* p) {
+  long ret;
+  struct timespec ts = { 0, 10000000 };
   setpriority(PRIO_PROCESS, 0, 4);
 
-  syscall(SYS_futex, &v, FUTEX_WAKE_OP, 1, NULL, &v2,
+  /* Sleep briefly to ensure that the main thread actually starts waiting.
+   * Otherwise there is a race condition, manifesting especially in some older
+   * kernels, where the main thread enters its futex syscall and we detect it
+   * is not ready to run, but it hasn't actually started waiting in the kernel.
+   * We schedule this thread, which then does its WAKE_OP operation, which
+   * fails to wake up the main thread. Sleeping first ensures that the main
+   * thread really reaches its futex wait state before we try to wake it.
+   */
+  nanosleep(&ts, NULL);
+
+  ret = syscall(SYS_futex, &v, FUTEX_WAKE_OP, 1, NULL, &v2,
           FUTEX_OP(FUTEX_OP_SET, 1, FUTEX_OP_CMP_EQ, 0));
+  test_assert(ret == 1);
   /* We test that the side effects of this syscall on v2 (setting it to 1)
      are performed before we context-switch to the main thread and run it. */
+  atomic_printf("thread: v2 = %d\n", v2);
 
   return NULL;
 }
@@ -20,6 +34,8 @@ int main(void) {
   pthread_t thread;
 
   pthread_create(&thread, NULL, run_thread, NULL);
+
+  atomic_printf("v2 = %d\n", v);
 
   test_assert(0 == syscall(SYS_futex, &v, FUTEX_WAIT, 0, NULL, NULL, 0));
   test_assert(1 == v2);
