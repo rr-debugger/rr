3bdc26a4cc07c2e027a2b0941791f7c5180e4cbc
Kyle Huey <khuey@kylehuey.com>
2017-10-26 22:19:51 -0700

    Fix replace mappings test to work on 64 bit, and actually check results of mmap calls.

--- src/test/mmap_replace_most_mappings.c
+++ src/test/mmap_replace_most_mappings.c
@@ -2,6 +2,8 @@
 
 #include "util.h"
 
+#define MAX_ERRNO 4095
+
 #define RR_PAGE_ADDR 0x70000000
 
 uintptr_t unmappings[1000];
@@ -30,18 +32,20 @@ static __attribute__((noinline)) void breakpoint(void) {
   (void)break_here;
 }
 
-static void my_syscall(uintptr_t syscallno, uintptr_t arg1, uintptr_t arg2,
-                       uintptr_t arg3, uintptr_t arg4) {
+static int my_syscall(uintptr_t syscallno, uintptr_t arg1, uintptr_t arg2,
+                      uintptr_t arg3, uintptr_t arg4) {
+  int ret;
 #ifdef __i386__
-  __asm__ __volatile__("int $0x80\n\t" ::"a"(syscallno), "b"(arg1), "c"(arg2),
-                       "d"(arg3), "S"(arg4));
+  __asm__ __volatile__("int $0x80\n\t" : "=a"(ret) :"a"(syscallno), "b"(arg1),
+                       "c"(arg2), "d"(arg3), "S"(arg4));
 #elif defined(__x86_64__)
   register long r10 asm("r10") = arg4;
-  __asm__ __volatile__("syscall\n\t" ::"a"(syscallno), "D"(arg1), "S"(arg2),
-                       "d"(arg3), "r"(r10));
+  __asm__ __volatile__("syscall\n\t" : "=a"(ret) :"a"(syscallno), "D"(arg1),
+                       "S"(arg2), "d"(arg3), "r"(r10));
 #else
 #error Fill in syscall here
 #endif
+  return ret;
 }
 
 int main(void) {
@@ -51,8 +55,23 @@ int main(void) {
   // be gone at some point. After iterate_maps, no C library calls are allowed
   iterate_maps(0, callback, maps_file);
   for (i = 0; i < nunmappings; ++i) {
-    my_syscall(RR_mmap2, unmappings[2 * i], unmappings[2 * i + 1], PROT_NONE,
-               MAP_PRIVATE | MAP_FIXED | MAP_ANONYMOUS);
+    const int mmap_syscall =
+#ifdef __i386__
+      RR_mmap2
+#elif defined(__x86_64__)
+      RR_mmap
+#else
+#error Fill in syscall here
+#endif
+      ;
+    int ret = my_syscall(mmap_syscall, unmappings[2 * i], unmappings[2 * i + 1], PROT_NONE,
+                         MAP_PRIVATE | MAP_FIXED | MAP_ANONYMOUS);
+    // We can't even use test_assert here, because it'll call strerror to printf the failure
+    // and we're unmapping libc).
+    // NB: Trying to mess with the highest pages on x86-64 gets us an ENOMEM error.
+    if (ret <=0 && ret >= -MAX_ERRNO && ret != -ENOMEM) {
+      my_syscall(RR_exit, ret, 0, 0, 0);
+    }
   }
   breakpoint();
   my_syscall(RR_exit, 0, 0, 0, 0);
