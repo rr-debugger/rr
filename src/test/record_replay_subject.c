a74ed472c710d7a1294db440ab2d3eba8eb301e3
Kyle Huey <khuey@kylehuey.com>
2017-10-26 20:13:25 -0700

    Test asynchronous events for rr inside rr.

--- src/test/record_replay_subject.c
+++ src/test/record_replay_subject.c
@@ -2,13 +2,37 @@
 
 #include "util.h"
 
+static volatile int caught_sig = 0;
+
+void catcher(__attribute__((unused)) int signum,
+             __attribute__((unused)) siginfo_t* siginfo_ptr,
+             __attribute__((unused)) void* ucontext_ptr) {
+  caught_sig = signum;
+}
+
 int main(void) {
+  struct sigaction sact;
+  int counter;
+
+  sigemptyset(&sact.sa_mask);
+  sact.sa_flags = SA_SIGINFO;
+  sact.sa_sigaction = catcher;
+  sigaction(SIGALRM, &sact, NULL);
+
+  alarm(1); /* timer will pop in 1 second */
+
   size_t page_size = sysconf(_SC_PAGESIZE);
   void* p =
       mmap(NULL, page_size, PROT_READ, MAP_ANONYMOUS | MAP_PRIVATE, -1, 0);
   test_assert(p != MAP_FAILED);
 
   test_assert(0 == mprotect(p, page_size, PROT_NONE));
+
+  for (counter = 0; counter >= 0 && !caught_sig; counter++) {
+    if (counter % 10000000 == 0) {
+      write(STDOUT_FILENO, ".", 1);
+    }
+  }
 
   atomic_puts("EXIT-SUCCESS");
   return 0;
