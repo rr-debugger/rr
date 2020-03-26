b8bc5dda1528620535cad29937706b0abd517de0
Robert O'Callahan <robert@ocallahan.org>
2020-03-26 15:53:34 +1300

    Support some simple time64 syscalls

--- src/record_syscall.cc
+++ src/record_syscall.cc
@@ -3996,6 +3996,10 @@ static Switchable rec_prepare_syscall_arch(RecordTask* t,
       syscall_state.reg_parameter<typename Arch::timespec>(4);
       return ALLOW_SWITCH;
 
+    case Arch::clock_nanosleep_time64:
+      syscall_state.reg_parameter<typename Arch::Arch64::timespec>(4);
+      return ALLOW_SWITCH;
+
     case Arch::sched_yield:
       t->session().scheduler().schedule_one_round_robin(t);
       return ALLOW_SWITCH;
@@ -4317,6 +4321,7 @@ static void rec_prepare_restart_syscall_arch(RecordTask* t,
   switch (syscallno) {
     case Arch::nanosleep:
     case Arch::clock_nanosleep:
+    case Arch::clock_nanosleep_time64:
       /* Hopefully uniquely among syscalls, nanosleep()/clock_nanosleep()
        * requires writing to its remaining-time outparam
        * *only if* the syscall fails with -EINTR.  When a
