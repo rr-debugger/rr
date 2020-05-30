84ae4a7d0395c0ce8672a4ce02e8790d47521fcc
Keno Fischer <keno@juliacomputing.com>
2020-05-30 01:34:33 +0000

    Port post_exec_fpu_regs

--- src/test/post_exec_fpu_regs.py
+++ src/test/post_exec_fpu_regs.py
@@ -1,6 +1,13 @@
 from util import *
 import re
 
-send_gdb('p $xmm0')
-expect_gdb('v4_float = {0, 0, 0, 0}')
+arch = get_exe_arch()
+
+if arch == 'aarch64':
+    send_gdb('p $v0.d.u')
+    expect_gdb('{0, 0}')
+elif arch == 'i386' or arch == 'i386:x86-64':
+    send_gdb('p $xmm0')
+    expect_gdb('v4_float = {0, 0, 0, 0}')
+
 ok()
