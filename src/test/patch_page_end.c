8a8861a25777a444f75864a31e168ffd90799bd9
Keno Fischer <keno@juliacomputing.com>
2020-05-30 00:25:36 +0000

    Port patch_page_end
    This patch isn't complete as it depends on the syscallbuf to actually
    work, but let's at least get it building properly on master.

--- src/test/patch_page_end.c
+++ src/test/patch_page_end.c
@@ -2,23 +2,50 @@
 
 #include "util.h"
 
-#if defined(__i386__) || defined(__x86_64__)
 #ifdef __i386__
-static const uint8_t code[] = { 0xcd, 0x80, 0x90, 0x90, 0x90, 0xc3 };
+static const uint8_t code[] = {
+    0xcd, 0x80, // int $80
+    0x90, // nop
+    0x90, // nop
+    0x90, // nop
+    0xc3  // ret
+};
+#elif defined(__x86_64__)
+static const uint8_t code[] = {
+    0x0f, 0x05, // syscall
+    0x90, // nop
+    0x90, // nop
+    0x90, // nop
+    0xc3  // ret
+};
+#elif defined(__aarch64__)
+static const uint8_t code[] = {
+    0x1, 0x0, 0x0, 0xd4,    // svc #0
+    0x1f, 0x20, 0x03, 0xd5, // nop
+    0x1f, 0x20, 0x03, 0xd5, // nop
+    0x1f, 0x20, 0x03, 0xd5, // nop
+    0xc0, 0x03, 0x5f, 0xd6, // ret
+};
 #else
-static const uint8_t code[] = { 0x0f, 0x05, 0x90, 0x90, 0x90, 0xc3 };
+  #error unsupported arch
 #endif
 
 static long do_call(uint8_t* p) {
   long ret;
+#if defined (__i386__) || defined(__x86_64__)
   __asm__ __volatile__("call *%2\n\t" : "=a"(ret) : "a"(SYS_getpid), "c"(p));
+#elif defined(__aarch64__)
+  register long x8 __asm__("x8") = SYS_getpid;
+  register long x0 __asm__("x0") = 0;
+  __asm__ __volatile__("blr %2" : "=r"(x0) : "r"(x8), "r"(p));
+  ret = x0;
+#else
+  #error unsupported arch
+#endif
   return ret;
 }
 
 static void check_patch(uint8_t* p) { test_assert(p[0] == 0xe9); }
-#else
-#error unsupported arch
-#endif
 
 int main(void) {
   size_t page_size = sysconf(_SC_PAGESIZE);
