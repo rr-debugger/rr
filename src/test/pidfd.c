1543ec5649791b57f275f5837fb358e4d29cc83a
Robert O'Callahan <robert@ocallahan.org>
2020-09-17 02:16:08 +1200

    Handle P_PIDFD not supported

--- src/test/pidfd.c
+++ src/test/pidfd.c
@@ -8,6 +8,7 @@ int main(void) {
   int child;
   int pipe_fds[2];
   int pidfd;
+  int status;
 
   pipe(pipe_fds);
   child = fork();
@@ -20,17 +21,24 @@ int main(void) {
   pidfd = syscall(RR_pidfd_open, child, 0);
   write(pipe_fds[1], "x", 1);
   if (pidfd < 0 && errno == ENOSYS) {
-    int status;
     atomic_puts("pidfd_open not supported, skipping test");
     test_assert(wait(&status) == child);
     test_assert(WIFEXITED(status) && WEXITSTATUS(status) == 77);
   } else {
     siginfo_t info;
+    int ret;
     test_assert(pidfd >= 0);
-    test_assert(waitid(RR_P_PIDFD, pidfd, &info, WEXITED) == 0);
-    test_assert(info.si_pid == child);
-    test_assert(info.si_code == CLD_EXITED);
-    test_assert(info.si_status == 77);
+    ret = waitid(RR_P_PIDFD, pidfd, &info, WEXITED);
+    if (ret < 0 && errno == EINVAL) {
+      atomic_puts("P_PIDFD not supported, skipping that part of the test");
+      test_assert(wait(&status) == child);
+      test_assert(WIFEXITED(status) && WEXITSTATUS(status) == 77);
+    } else {
+      test_assert(ret == 0);
+      test_assert(info.si_pid == child);
+      test_assert(info.si_code == CLD_EXITED);
+      test_assert(info.si_status == 77);
+    }
   }
 
   atomic_puts("EXIT-SUCCESS");
