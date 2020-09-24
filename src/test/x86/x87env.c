3976bd314755e2102a1420209bb13b96037ecf93
Robert O'Callahan <robert@ocallahan.org>
2020-09-25 10:00:56 +1200

    Add x87env test

---
+++ src/test/x86/x87env.c
@@ -0,0 +1,26 @@
+/* -*- Mode: C; tab-width: 8; c-basic-offset: 2; indent-tabs-mode: nil; -*- */
+
+#include "util.h"
+
+extern char _x87_instruction;
+
+int main(void) {
+#ifdef __x86_64__
+  long double x;
+  uint32_t buf[256];
+  __asm__ volatile("_x87_instruction: fld1\n"
+                   "fstenv %1\n"
+                   "fldz\n"
+                   "fldenv %1\n"
+                   : "=t"(x), "=m"(buf)
+                   :
+                   : "memory");
+  test_assert(x == 1.0);
+  /* Check saved FIP */
+  atomic_printf("FIP=0x%x\n", buf[3]);
+  test_assert(buf[3] == (uint32_t)(long)&_x87_instruction);
+#endif
+
+  atomic_puts("EXIT-SUCCESS");
+  return 0;
+}
