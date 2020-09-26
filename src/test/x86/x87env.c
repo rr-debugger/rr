551b00297def38a32a140a6c54517263b960dd61
Robert O'Callahan <robert@ocallahan.org>
2020-09-26 17:51:58 +1200

    Test XSAVE too

--- src/test/x86/x87env.c
+++ src/test/x86/x87env.c
@@ -7,20 +7,26 @@ extern char _x87_instruction;
 int main(void) {
 #ifdef __x86_64__
   long double x;
-  uint32_t buf[256];
+  uint32_t fstenv_buf[256];
+  /* use mmap to get alignment >= 64 bytes */
+  uint64_t* xsave_buf = (uint64_t*)mmap(NULL, 65536, PROT_READ | PROT_WRITE, MAP_ANONYMOUS | MAP_PRIVATE, -1, 0);
   __asm__ volatile("_x87_instruction: fld1\n");
+
   /* Trigger PLT code and syscall */
   sched_yield();
   __asm__ volatile("fstenv %1\n"
+                   "xsave64 %2\n"
                    "fldz\n"
                    "fldenv %1\n"
-                   : "=t"(x), "=m"(buf)
-                   :
+                   : "=t"(x), "=m"(fstenv_buf), "=m"(*xsave_buf)
+                   : "a"(0xffffffff), "d"(0xffffffff)
                    : "memory");
   test_assert(x == 1.0);
-  /* Check saved FIP */
-  atomic_printf("FIP=0x%x\n", buf[3]);
-  test_assert(buf[3] == (uint32_t)(long)&_x87_instruction);
+  /* Check saved FIPs */
+  atomic_printf("FIP=0x%x\n", fstenv_buf[3]);
+  test_assert(fstenv_buf[3] == (uint32_t)(uintptr_t)&_x87_instruction);
+  atomic_printf("XSAVE FIP=0x%llx\n", (long long)xsave_buf[1]);
+  test_assert(xsave_buf[1] == (uintptr_t)&_x87_instruction);
 #endif
 
   atomic_puts("EXIT-SUCCESS");
