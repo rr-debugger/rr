db03c986a9d5772eb92c2686bf9925276570680c
Keno Fischer <keno@juliacomputing.com>
2021-01-02 05:11:50 -0500

    Work around a bad GDB heuristic in unwind
    GDB has a heuristic [1] that overrides unwind info if
    the current instruction is a literal `retq`. Unfortunately
    this interferes with our __morestack hack because it changes
    the CFA, which in response to a `fin` command, causes GDB
    to believe that it did not in fact manage to step out of
    the frame, but rather ended up stepping into the same function
    again. This causes #2682. To work around this, we could set
    the producer to "GCC 4.5" or greater, which disables the
    heuristic. However, creating this information in assembly
    is quite tedious because there is no native support for it,
    so it would have to be entered using raw directives. Instead,
    simply put a `nop` at the return of the `_syscall_hook_trampoline`.
    This prevents the heuristic from kicking in, so GDB doesn't miss
    the frame. GDB will still unwind incorrectly at the `retq` that
    follows, but we generally won't stop there. Long term
    this isn't perfect of course and we're playing a bit of a
    game of whack-a-mole with GDB heuristics here when our unwind
    information is actually 100% valid. I have written to the
    lsb-discuss mailing list to see if we can get an unwind
    augmentation that would signal to the debugger to just trust
    our unwind info and disable all heuristics. For the moment
    though, this fixes #2682.
    
    [1] https://github.com/bminor/binutils-gdb/blob/master/gdb/dwarf2/read.c#L10083-L10084

---
+++ src/test/morestack_unwind.py
@@ -0,0 +1,25 @@
+from util import *
+
+send_gdb('break main')
+expect_gdb('Breakpoint 1')
+send_gdb('c')
+expect_gdb('Breakpoint 1')
+
+send_gdb('break _syscall_hook_trampoline')
+expect_gdb('Breakpoint 2')
+send_gdb('c')
+expect_gdb('Breakpoint 2')
+
+send_gdb('fin')
+send_gdb('fin')
+send_gdb('fin')
+
+# Verify we didn't run too far
+
+send_gdb('b breakpoint')
+expect_gdb('Breakpoint 3')
+
+send_gdb('c')
+expect_gdb('Breakpoint 3')
+
+ok()
