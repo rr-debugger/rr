ea545ce6a3421cb171fbac0dc4f79d219ab12d2b
Robert O'Callahan <robert@ocallahan.org>
2021-03-29 19:16:29 +1300

    Be more clear about what LIBRARY is

--- src/SourcesCommand.cc
+++ src/SourcesCommand.cc
@@ -60,7 +60,9 @@ SourcesCommand SourcesCommand::singleton(
     "  --substitute=LIBRARY=PATH  When searching for the source to LIBRARY,\n"
     "                             substitute PATH in place of the path stored\n"
     "                             in the library's DW_AT_comp_dir property\n"
-    "                             for all compilation units.\n");
+    "                             for all compilation units.
+    "                             LIBRARY is the basename of the original file name,
+    "                             e.g. libc-2.32.so\n");
 
 class ExplicitSourcesCommand : public Command {
 public:
@@ -80,7 +82,9 @@ ExplicitSourcesCommand ExplicitSourcesCommand::singleton(
     "  --substitute=LIBRARY=PATH  When searching for the source to LIBRARY,\n"
     "                             substitute PATH in place of the path stored\n"
     "                             in the library's DW_AT_comp_dir property\n"
-    "                             for all compilation units.\n");
+    "                             for all compilation units.
+    "                             LIBRARY is the basename of the original file name,
+    "                             e.g. libc-2.32.so\n");
 
 static void parent_dir(string& s) {
   size_t p = s.rfind('/');
