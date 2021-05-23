c4c818806f27b91d89719f09f49df1a3b3d11b3e
Bob131 <bob@bob131.so>
2021-05-23 10:26:13 +1000

    GdbServer: fix diversion exit notification
    If a diversion session exits during execution, the resumption request
    from GDB doesn't get a response and doesn't get consumed. This has two
    effects:
    
      - GDB may continue to process an inferior call without ever having
        been notified of a change in target state, and
      - the request can end up being reprocessing on the replay session a
        number of times, stepping it forward arbitrarily.
    
    The call_exit test should have picked this up, however the placement
    of the breakpoint masked the test failure by preventing the replay
    session from stepping.
    
    This commit fixes the issue by making sure that a diversion session
    exit triggers some sort of notification for GDB. This is a bit tricky,
    since a simple stop isn't sufficient to make GDB bail out of an
    inferior call but an inferior exit signals the end of the inferior. It
    just so happens, however, that using an exit notification as done in
    this commit causes the current implementation in GDB to revive the
    inferior object after its destruction in a way that seems to work.

--- src/test/call_exit.py
+++ src/test/call_exit.py
@@ -6,8 +6,18 @@ expect_gdb('Breakpoint 1')
 send_gdb('c')
 expect_gdb('Breakpoint 1, main')
 
+# Step over the breakpoint and into `atomic_puts' to make sure it
+# doesn't influence the test.
+send_gdb('step')
+expect_gdb('atomic_puts \\(str=')
+
 send_gdb('call (int)exit(0)')
 expect_gdb('while in a function called from GDB')
+
+# Check sure we're still in the frame of `atomic_puts' and can still
+# continue the replay.
+send_gdb('finish')
+expect_gdb('EXIT-SUCCESS')
 
 restart_replay()
 expect_gdb('Breakpoint 1, main')
