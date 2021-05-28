08e28732ffb8448455971b41592f0c059fcca7d8
Robert O'Callahan <robert@ocallahan.org>
2021-05-28 14:12:16 +1200

    Silence warning

--- src/util.cc
+++ src/util.cc
@@ -2182,7 +2182,7 @@ void SAFE_FATAL(int err, const char *msg)
     {.iov_base = (char*)msg, .iov_len=strlen(msg)},
     {.iov_base = nl, .iov_len=sizeof(nl)}
   };
-  ::writev(STDERR_FILENO, out, sizeof(out)/sizeof(struct iovec));
+  (void)::writev(STDERR_FILENO, out, sizeof(out)/sizeof(struct iovec));
   abort();
 }
 
