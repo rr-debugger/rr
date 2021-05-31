b0ddf0db069ca53e10d30a3b0742ca990ac2bab9
Bernhard Übelacker <bernhardu@mailbox.org>
2021-06-01 00:10:03 +0200

    Add ioctl FBIOGET_FSCREENINFO and FBIOGET_VSCREENINFO.
    Found to be used in xserver-xorg.

--- src/kernel_abi.h
+++ src/kernel_abi.h
@@ -1735,6 +1735,64 @@ struct BaseArch : public wordsize,
     unsigned short v_state;
   };
   RR_VERIFY_TYPE(vt_stat);
+
+  struct fb_fix_screeninfo {
+    char id[16];
+    unsigned long smem_start;
+    __u32 smem_len;
+    __u32 type;
+    __u32 type_aux;
+    __u32 visual;
+    uint16_t xpanstep;
+    uint16_t ypanstep;
+    uint16_t xwrapstep;
+    __u32 line_length;
+    unsigned long mmio_start;
+    __u32 mmio_len;
+    __u32 accel;
+    uint16_t capabilities;
+    uint16_t reserved[2];
+  };
+  RR_VERIFY_TYPE(fb_fix_screeninfo);
+
+  struct fb_bitfield {
+    __u32 offset;
+    __u32 length;
+    __u32 msb_right;
+  };
+  RR_VERIFY_TYPE(fb_bitfield);
+  struct fb_var_screeninfo {
+    __u32 xres;
+    __u32 yres;
+    __u32 xres_virtual;
+    __u32 yres_virtual;
+    __u32 xoffset;
+    __u32 yoffset;
+    __u32 bits_per_pixel;
+    __u32 grayscale;
+    struct fb_bitfield red;
+    struct fb_bitfield green;
+    struct fb_bitfield blue;
+    struct fb_bitfield transp;
+    __u32 nonstd;
+    __u32 active;
+    __u32 height;
+    __u32 width;
+    __u32 accel_flags;
+    __u32 pixclock;
+    __u32 left_margin;
+    __u32 right_margin;
+    __u32 upper_margin;
+    __u32 lower_margin;
+    __u32 hsync_len;
+    __u32 vsync_len;
+    __u32 sync;
+    __u32 vmode;
+    __u32 rotate;
+    __u32 colorspace;
+    __u32 reserved[4];
+  };
+  RR_VERIFY_TYPE(fb_var_screeninfo);
 };
 
 struct X64Arch : public BaseArch<SupportedArch::x86_64, WordSize64Defs> {
