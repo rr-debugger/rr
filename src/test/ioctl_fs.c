24df90a75d8834c086cd524f63426749f561db6b
Robert O'Callahan <robert@ocallahan.org>
2021-06-17 09:23:33 +1200

    FS_IOC_FIEMAP can return EOPNOTSUPP

--- src/test/ioctl_fs.c
+++ src/test/ioctl_fs.c
@@ -14,13 +14,13 @@ int main(void) {
   test_assert(fd >= 0);
   ret = ioctl(fd, FS_IOC_GETVERSION, &version);
   if (ret < 0) {
-    test_assert(errno == ENOTTY);
+    test_assert(errno == ENOTTY || errno == EOPNOTSUPP);
   } else {
     atomic_printf("version=%ld\n", version);
   }
   ret = ioctl(fd, FS_IOC_GETFLAGS, &flags);
   if (ret < 0) {
-    test_assert(errno == ENOTTY);
+    test_assert(errno == ENOTTY || errno == EOPNOTSUPP);
   } else {
     atomic_printf("flags=%lx\n", flags);
   }
@@ -33,7 +33,7 @@ int main(void) {
   fm->fm_length = FIEMAP_MAX_OFFSET - fm->fm_start;
   ret = ioctl(fd, FS_IOC_FIEMAP, fm);
   if (ret < 0) {
-    test_assert(errno == ENOTTY);
+    test_assert(errno == ENOTTY || errno == EOPNOTSUPP);
   } else {
     atomic_printf("fm->fm_mapped_extents=%d\n", fm->fm_mapped_extents);
     for (unsigned int i=0; i < fm->fm_mapped_extents; i++) {
