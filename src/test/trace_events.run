rr-debugger/rr

commit b04220a6e66c2f9f47c9c953b928ce074bf6c075

Skip wait_exit for any thread in the init process of a pid-namespace, because we don't know which thread could be the reaper.
To avoid races when wait_exit is skipped, clear child_tid early if we're skipping wait_exit.

CMakeLists.txt
@@ -962,6 +962,7 @@ set(BASIC_TESTS
   pid_ns_kill_child_threads
   pid_ns_kill_child_zombie
   pid_ns_kill_threads
+  pid_ns_kill_threads_exit_wait
   pid_ns_reap
   pid_ns_segv
   pidfd

src/RecordSession.cc
@@ -233,11 +233,12 @@ static bool handle_ptrace_exit_event(RecordTask* t) {
   // letting this task complete its exit.
   bool may_wait_exit = !is_coredumping_signal(exit_status.fatal_sig()) &&
     !t->waiting_for_pid_namespace_tasks_to_exit();
+  t->record_exit_event(exit_status.fatal_sig(),
+    (!t->already_reaped() && !may_wait_exit) ? RecordTask::WRITE_CHILD_TID : RecordTask::KERNEL_WRITES_CHILD_TID);
   if (!t->already_reaped()) {
     t->proceed_to_exit(may_wait_exit);
   }
   record_exit_trace_event(t, exit_status);
-  t->record_exit_event(exit_status.fatal_sig());
   t->do_ptrace_exit_stop(exit_status);
   if (may_wait_exit) {
     t->did_reach_zombie();
@@ -1643,7 +1644,8 @@ bool RecordSession::signal_state_changed(RecordTask* t, StepState* step_state) {
         t->destroy_buffers(nullptr, nullptr);
         WaitStatus exit_status = WaitStatus::for_fatal_sig(sig);
         record_exit_trace_event(t, exit_status);
-        t->record_exit_event(sig);
+        // Allow writing child_tid now because otherwise the write will race
+        t->record_exit_event(sig, RecordTask::WRITE_CHILD_TID);
         // On a real affected kernel, we probably would have never gotten here,
         // since the signal we would be seeing was not deterministic, but let's
         // be conservative and still try to emulate the ptrace stop.

src/RecordTask.cc
@@ -252,9 +252,7 @@ RecordTask::~RecordTask() {
   }
 }
 
-void RecordTask::record_exit_event(int fatal_signo) {
-  // Task::destroy has already done PTRACE_DETACH so the task can complete
-  // exiting.
+void RecordTask::record_exit_event(int fatal_signo, WriteChildTid write_child_tid) {
   // The kernel explicitly only clears the futex if the address space is shared.
   // If the address space has no other users then the futex will not be cleared
   // even if it lives in shared memory which other tasks can read.
@@ -266,6 +264,17 @@ void RecordTask::record_exit_event(int fatal_signo) {
      as->has_mapping(tid_futex)) {
     int val = 0;
     record_local(tid_futex, &val);
+    if (write_child_tid == WRITE_CHILD_TID) {
+      // Write the memory now, otherwise the kernel will write it later and that can
+      // race with the execution of other threads if we don't wait for this
+      // thread to fully exit.
+      // This could fail since the address space might have gone away/been switched
+      // by execve.
+      bool ok = true;
+      write_mem(tid_futex, 0, &ok);
+      // The kernel will do an unconditional futex wake on that location so we don't
+      // need to do it.
+    }
   }
 
   // Write the exit event here so that the value recorded above is captured.
@@ -2051,15 +2060,53 @@ bool RecordTask::try_wait() {
   return true;
 }
 
+static uint64_t read_pid_ns(const RecordTask* t) {
+  char buf[PATH_MAX];
+  sprintf(buf, "/proc/%d/ns/pid", t->tid);
+  char link[PATH_MAX];
+  int ret = readlink(buf, link, sizeof(link));
+  ASSERT(t, ret >= 0);
+  ASSERT(t, ret < (int)sizeof(link));
+  link[ret] = 0;
+  ASSERT(t, strncmp(link, "pid:[", 5) == 0);
+  char* end;
+  uint64_t result = strtoul(link + 5, &end, 10);
+  ASSERT(t, strcmp(end, "]") == 0);
+  return result;
+}
+
 bool RecordTask::waiting_for_pid_namespace_tasks_to_exit() const {
-  if (tg->tgid_own_namespace != 1 || thread_group()->task_set().size() > 1) {
+  if (tg->tgid_own_namespace != 1) {
     return false;
   }
-  // This is the last thread of pid-1 for its namespace. See if there
-  // are any other tasks in the pid namespace. We can just check if there
-  // are any threadgroup children of our threadgroup.
+  // This might be the last live thread for pid-1 in the pid namespace.
+  // Checking that it *is* the last live thread is tricky because other
+  // threads could unexpectedly die asynchronously :-(.
+  // See if there are any other tasks in the pid namespace.
+  // Note that due to setns there can be tasks in the pid namespace
+  // with parents outside the pid namespace other than our thread-group.
+
+  // If there are multiple threads in our threadgroup, they're in our
+  // pid namespace.
+  if (thread_group()->task_set().size() > 1) {
+    return true;
+  }
+  // If we have any child processes then those belong to our pid namespace
+  // (or a descendant).
   for (auto p : session().thread_group_map()) {
     if (p.second->parent() == tg.get()) {
+      return true;
+    }
+  }
+  // If there are any other tasks in the pid namespace at least one must be
+  // directly in the namespace.
+  uint64_t pid_ns = read_pid_ns(this);
+  for (auto it : session().tasks()) {
+    auto rt = static_cast<RecordTask*>(it.second);
+    if (rt == this) {
+      continue;
+    }
+    if (read_pid_ns(rt) == pid_ns) {
       return true;
     }
   }

src/RecordTask.h
@@ -118,8 +118,13 @@ public:
   void do_ptrace_exit_stop(WaitStatus exit_status);
   /**
    * Return the exit event.
+   * If write_child_tid is set, zero out child_tid now if applicable.
    */
-  void record_exit_event(int exitsig = 0);
+  enum WriteChildTid {
+    KERNEL_WRITES_CHILD_TID,
+    WRITE_CHILD_TID,
+  };
+  void record_exit_event(int exitsig = 0, WriteChildTid write_child_tid = KERNEL_WRITES_CHILD_TID);
   /**
    * Called when we're about to deliver a signal to this task. If it's a
    * synthetic SIGCHLD and there's a ptraced task that needs to SIGCHLD,

src/test/pid_ns_kill_threads_exit_wait.c
@@ -1,0 +1,78 @@
+/* -*- Mode: C; tab-width: 8; c-basic-offset: 2; indent-tabs-mode: nil; -*- */
+
+#include "nsutils.h"
+#include "util.h"
+
+#define NUM_THREADS 2
+
+static int trigger_last_thread_pipe[2];
+static int trigger_pid_ns_init_exit_pipe[2];
+
+static void* do_thread(void* p) {
+  int index = (intptr_t)p;
+  /* Reduce thread priority so rr doesn't schedule these threads unless we want it to */
+  setpriority(PRIO_PROCESS, 0, index + 1);
+  if (index == NUM_THREADS - 1) {
+    char ch;
+    read(trigger_last_thread_pipe[0], &ch, 1);
+    /* Kick off the init process exit ...
+       after our exit_group has started. */
+    write(trigger_pid_ns_init_exit_pipe[1], "x", 1);
+    exit(1);
+  }
+  pause();
+  return NULL;
+}
+
+static int do_pid_ns_init(void) {
+  pid_t child;
+  char ch;
+  pipe(trigger_last_thread_pipe);
+  pipe(trigger_pid_ns_init_exit_pipe);
+
+  test_assert(0 == unshare(CLONE_NEWPID));
+  child = fork();
+  if (!child) {
+    pid_t inner_child;
+    for (int i = 0; i < NUM_THREADS; ++i) {
+      pthread_t thread;
+      pthread_create(&thread, NULL, do_thread, (void*)(intptr_t)i);
+    }
+
+    inner_child = fork();
+    if (!inner_child) {
+      pause();
+      return 0;
+    }
+
+    write(trigger_last_thread_pipe[1], "y", 1);
+    pause();
+    return 0;
+  }
+  read(trigger_pid_ns_init_exit_pipe[0], &ch, 1);
+  /* Trigger exit that will zap_pid_ns_processes for our child tasks */
+  return 77;
+}
+
+int main(void) {
+  pid_t pid;
+  int status;
+
+  if (-1 == try_setup_ns(CLONE_NEWPID)) {
+    /* We may not have permission to set up namespaces, so bail. */
+    atomic_puts("Insufficient permissions, skipping test");
+    atomic_puts("EXIT-SUCCESS");
+    return 0;
+  }
+
+  pid = fork();
+  test_assert(pid >= 0);
+  if (!pid) {
+    return do_pid_ns_init();
+  }
+
+  test_assert(pid == waitpid(pid, &status, 0));
+  test_assert(WIFEXITED(status) && WEXITSTATUS(status) == 77);
+  atomic_puts("EXIT-SUCCESS");
+  return 0;
+}

commit 66489e68e4be7ead2aeca6665a269ffb70b61071

Emit exit trace-event before the actual EXIT event, otherwise it will be associated with the *next* event which may not exist
Fixes a regression from b04220a6e66c2f9f47c9c953b928ce074bf6c075

CMakeLists.txt
@@ -1445,6 +1445,7 @@ set(TESTS_WITHOUT_PROGRAM
   syscallbuf_timeslice_250
   trace_version
   term_trace_cpu
+  trace_events
   tty
   unmap_vdso
   unwind_on_signal

src/RecordSession.cc
@@ -233,12 +233,12 @@ static bool handle_ptrace_exit_event(RecordTask* t) {
   // letting this task complete its exit.
   bool may_wait_exit = !is_coredumping_signal(exit_status.fatal_sig()) &&
     !t->waiting_for_pid_namespace_tasks_to_exit();
+  record_exit_trace_event(t, exit_status);
   t->record_exit_event(exit_status.fatal_sig(),
     (!t->already_reaped() && !may_wait_exit) ? RecordTask::WRITE_CHILD_TID : RecordTask::KERNEL_WRITES_CHILD_TID);
   if (!t->already_reaped()) {
     t->proceed_to_exit(may_wait_exit);
   }
-  record_exit_trace_event(t, exit_status);
   t->do_ptrace_exit_stop(exit_status);
   if (may_wait_exit) {
     t->did_reach_zombie();

src/test/trace_events.run
@@ -1,0 +1,11 @@
+source `dirname $0`/util.sh
+
+exe=simple$bitness
+cp ${OBJDIR}/bin/$exe $exe-$nonce
+just_record $exe-$nonce
+if [[ `rr dump -e latest-trace|grep --count TraceTaskEvent::EXEC` != 1 ]]; then
+    failed "Wrong number of TraceTaskEvent::EXECs"
+fi
+if [[ `rr dump -e latest-trace|grep --count TraceTaskEvent::EXIT` != 1 ]]; then
+    failed "Wrong number of TraceTaskEvent::EXITs"
+fi