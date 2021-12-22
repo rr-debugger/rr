24889515ac5e0b0e07b0edd0a72f554f3fbbd300
Keno Fischer <keno@juliacomputing.com>
2021-12-22 01:53:41 -0500

    Fix comm test for 32bit

--- src/test/comm.py
+++ src/test/comm.py
@@ -2,6 +2,6 @@ from util import *
 import re
 
 send_gdb('info threads')
-expect_gdb(r'1\s+Thread \d+\.\d+ \(simple-[A-Za-z0-9]+\)')
+expect_gdb(r'1\s+Thread \d+\.\d+ \(simple(_32)?-[A-Za-z0-9]+\)')
 
 ok()
