f514a36d4afbd82c53b3337c53c32a46b4d05a51
Yichao Yu <yyc1992@gmail.com>
2022-04-12 20:20:31 -0400

    Allow page size up to 64k on aarch64 in sysconf test

--- src/test/sysconf.c
+++ src/test/sysconf.c
@@ -7,7 +7,11 @@ int main(void) {
   long ncpus = sysconf(_SC_NPROCESSORS_ONLN);
 
   atomic_printf("sysconf says page size is %ld bytes\n", pagesize);
+#ifdef __aarch64__
+  test_assert((64 * 1024) % pagesize == 0);
+#else
   test_assert(4096 == pagesize);
+#endif
 
   atomic_printf("sysconf says %ld processors are online\n", ncpus);
   /* TODO: change this when rr supports parallel recording. */
