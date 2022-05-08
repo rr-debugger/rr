1bb80487796987bd8ecbcdfba7846249ee7cc723
Robert O'Callahan <robert@ocallahan.org>
2022-05-09 11:10:28 +1200

    Gracefully handle 'permission denied' when trying to open tty

--- src/test/ttyname.c
+++ src/test/ttyname.c
@@ -11,6 +11,11 @@ int main(void) {
     return 0;
   }
   int fd = open(tty, O_RDWR);
+  if (fd < 0 && errno == EACCES) {
+    atomic_puts("tty access denied (su in progress?), skipping test");
+    fputs("EXIT-SUCCESS", stderr);
+    return 0;
+  }
   test_assert(fd >= 0);
   test_assert(13 == write(fd, "EXIT-SUCCESS\n", 13));
   return 0;
