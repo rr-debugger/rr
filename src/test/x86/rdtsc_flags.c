65ed30b5a257f54bb1c67842c8531df2f710218f
Robert O'Callahan <robert@ocallahan.org>
2022-06-10 18:20:14 +1200

    Ensure that R11 is preserved correctly by syscall buffering

--- src/test/x86/rdtsc_flags.c
+++ src/test/x86/rdtsc_flags.c
@@ -6,23 +6,32 @@ int main(void) {
 #ifdef __x86_64__
   char zero_ok;
   char nonzero_ok;
+  uint64_t r11;
 
-  /* Check that ZF=0 holds across buffered RDTSC */
+  /* Check that ZF=0 holds across buffered RDTSC
+     and that R11 is preserved */
   asm volatile ("xor %%eax,%%eax\n\t"
+                "xor %%r11,%%r11\n\t"
                 "rdtsc\n\t"
                 "mov %%rax,%%rcx\n\t" /* make it bufferable */
                 "sete %0\n\t"
-                : "=m"(zero_ok) :: "eax", "edx", "rcx");
+                "mov %%r11,%1\n\t"
+                : "=m"(zero_ok), "=m"(r11) :: "eax", "edx", "rcx", "r11");
   test_assert(zero_ok);
+  test_assert(r11 == 0);
 
-  /* Check that ZF=1 holds across buffered RDTSC */
+  /* Check that ZF=1 holds across buffered RDTSC
+     and that R11 is preserved */
   asm volatile ("xor %%eax,%%eax\n\t"
+                "mov $1,%%r11d\n\t"
                 "cmp $1,%%eax\n\t"
                 "rdtsc\n\t"
                 "mov %%rax,%%rcx\n\t" /* make it bufferable */
                 "setne %0\n\t"
-                : "=m"(nonzero_ok) :: "eax", "edx", "rcx");
+                "mov %%r11,%1\n\t"
+                : "=m"(nonzero_ok), "=m"(r11) :: "eax", "edx", "rcx", "r11");
   test_assert(nonzero_ok);
+  test_assert(r11 == 1);
 #endif
 
   atomic_puts("EXIT-SUCCESS");
