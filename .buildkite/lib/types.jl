c0de244b1602bda87a5c1305dd987c74d7d44bd0
Dilum Aluthge <dilum@aluthge.com>
2022-06-19 16:58:32 -0400

    CI: update to the latest rootfs images

--- .buildkite/lib/types.jl
+++ .buildkite/lib/types.jl
@@ -18,15 +18,15 @@ const platforms = Platforms(
     [
         Platform(;
             arch            = "x86_64",
-            rootfs_tag      = "v5.14",
-            rootfs_treehash = "72dab0734a234d2277e3a204263ec7d038b4087e",
+            rootfs_tag      = "v5.22",
+            rootfs_treehash = "0",
             allow_fail      = false,
             commit_status   = true,
         ),
         Platform(;
             arch            = "aarch64",
-            rootfs_tag      = "v5.14",
-            rootfs_treehash = "20db175a1cc643f6d05f172eae921171e95326d3",
+            rootfs_tag      = "v5.22",
+            rootfs_treehash = "0",
             allow_fail      = true,
             commit_status   = false,
         ),
