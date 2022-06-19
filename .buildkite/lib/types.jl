e7efb9f7f1b292ab31bbbea907eaae27f5245a8b
Dilum Aluthge <dilum@aluthge.com>
2022-06-19 17:24:32 -0400

    CI: update to the latest rootfs images (#3277)
    * CI: update to the latest rootfs images
    
    * Put the correct number of zeros in the hash
    
    * Update the hashes

--- .buildkite/lib/types.jl
+++ .buildkite/lib/types.jl
@@ -18,15 +18,15 @@ const platforms = Platforms(
     [
         Platform(;
             arch            = "x86_64",
-            rootfs_tag      = "v5.14",
-            rootfs_treehash = "72dab0734a234d2277e3a204263ec7d038b4087e",
+            rootfs_tag      = "v5.22",
+            rootfs_treehash = "1cd67e278881dcfeed695282256b26fad603e15d",
             allow_fail      = false,
             commit_status   = true,
         ),
         Platform(;
             arch            = "aarch64",
-            rootfs_tag      = "v5.14",
-            rootfs_treehash = "20db175a1cc643f6d05f172eae921171e95326d3",
+            rootfs_tag      = "v5.22",
+            rootfs_treehash = "7a63218e46996b36aa108b55746a3d94a3e312c1",
             allow_fail      = true,
             commit_status   = false,
         ),
