0b1a7742d64d8a563b686c05ff0bd8d9090ba4ff
Bernhard Übelacker <bernhardu@mailbox.org>
2023-04-09 22:46:42 +0200

    buildkite: Add asan and force32bit variants to CI tests.

--- .buildkite/lib/types.jl
+++ .buildkite/lib/types.jl
@@ -4,6 +4,8 @@ Base.@kwdef struct Platform
     rootfs_treehash::String
     allow_fail::Bool
     commit_status::Bool
+    cmake_extra_arg::String
+    variant::String
 end
 
 struct Platforms
@@ -22,6 +24,17 @@ const platforms = Platforms(
             rootfs_treehash = "1cd67e278881dcfeed695282256b26fad603e15d",
             allow_fail      = false,
             commit_status   = true,
+            cmake_extra_arg = "",
+            variant         = "",
+        ),
+        Platform(;
+            arch            = "x86_64",
+            rootfs_tag      = "v5.22",
+            rootfs_treehash = "1cd67e278881dcfeed695282256b26fad603e15d",
+            allow_fail      = false,
+            commit_status   = true,
+            cmake_extra_arg = "-Dasan=true",
+            variant         = "asan",
         ),
         Platform(;
             arch            = "aarch64",
@@ -29,6 +42,35 @@ const platforms = Platforms(
             rootfs_treehash = "7a63218e46996b36aa108b55746a3d94a3e312c1",
             allow_fail      = false,
             commit_status   = true,
+            cmake_extra_arg = "",
+            variant         = "",
+        ),
+        Platform(;
+            arch            = "aarch64",
+            rootfs_tag      = "v5.22",
+            rootfs_treehash = "7a63218e46996b36aa108b55746a3d94a3e312c1",
+            allow_fail      = false,
+            commit_status   = true,
+            cmake_extra_arg = "-Dasan=true",
+            variant         = "asan",
+        ),
+        Platform(;
+            arch            = "x86_64",
+            rootfs_tag      = "v5.22",
+            rootfs_treehash = "1cd67e278881dcfeed695282256b26fad603e15d",
+            allow_fail      = false,
+            commit_status   = true,
+            cmake_extra_arg = "-Dforce32bit=true",
+            variant         = "force32bit",
+        ),
+        Platform(;
+            arch            = "x86_64",
+            rootfs_tag      = "v5.22",
+            rootfs_treehash = "1cd67e278881dcfeed695282256b26fad603e15d",
+            allow_fail      = false,
+            commit_status   = true,
+            cmake_extra_arg = "-Dforce32bit=true -Dasan=true",
+            variant         = "force32bit-asan",
         ),
     ]
 )
