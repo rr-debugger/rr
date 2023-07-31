ea26db5de354bc6305aa327326d42711e01d717b
Robert O'Callahan <robert@ocallahan.org>
2023-07-31 22:30:54 +1200

    Fix `unicode` test on distro/gdb combinations where breaking on a function doesn't display the function parameters correctly.

--- src/test/unicode.c
+++ src/test/unicode.c
@@ -2,10 +2,19 @@
 
 #include "util.h"
 
-static char* breakpoint(char* arg) { return arg; }
+static void breakpoint(void) {}
+
+static void outer(__attribute__((unused)) char* arg) {
+  /* On some gdb/distro combinations, function parameters don't have
+     correct debuginfo when stopped at a breakpoint on the function
+     itself. So stop in `breakpoint()` and then use `finish` to return
+     to this stack frame at a point where `arg` is sure to print
+     correctly. */
+  breakpoint();
+}
 
 int main(void) {
-  breakpoint("\xf0\x9d\x95\xa8\xc4\x81\xe2\x89\xa5\x33");
+  outer("\xf0\x9d\x95\xa8\xc4\x81\xe2\x89\xa5\x33");
   atomic_puts("EXIT-SUCCESS");
   return 0;
 }
--- src/test/unicode.py
+++ src/test/unicode.py
@@ -5,6 +5,7 @@ send_gdb('break breakpoint')
 expect_gdb('Breakpoint 1')
 send_gdb('c')
 expect_gdb('Breakpoint 1')
+send_gdb('finish')
 
 send_gdb('checkpoint')
 expect_gdb(re.compile(r'Checkpoint 1 at.*"𝕨ā≥3"'))
