030ffa018bead6b5db0dfb243de957f577d61254
Simon Sobisch <simonsobisch@web.de>
2023-09-13 12:15:03 +0000

    fix compilation without PTRACE_EVENT_STOP
    for test vfork_done
    copied from 34a9f5d ptrace_kill_grandtracee.c

--- src/test/vfork_done.c
+++ src/test/vfork_done.c
@@ -1,6 +1,10 @@
 /* -*- Mode: C; tab-width: 8; c-basic-offset: 2; indent-tabs-mode: nil; -*- */
 #include "util.h"
 
+#ifndef PTRACE_EVENT_STOP
+#define PTRACE_EVENT_STOP 128
+#endif
+
 #include <math.h>
 #include <signal.h>
 
