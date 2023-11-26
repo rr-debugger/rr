55c4f9964d4f39eab4479fbb4c10c7cb1c41e897
Frederick Zhang <frederick888@tsundere.moe>
2023-11-27 02:52:50 +1100

    Add Intel Tremont (#3405)
    The cpu_type is from an Intel N6005 [1].
    
    [1] https://www.intel.com/content/www/us/en/products/sku/212327/intel-pentium-silver-n6005-processor-4m-cache-up-to-3-30-ghz/specifications.html

--- src/PerfCounters_x86.h
+++ src/PerfCounters_x86.h
@@ -65,8 +65,9 @@ static CpuMicroarch compute_cpu_microarch() {
     case 0x506f0:
     case 0x706a0:
     case 0x506c0:
-    case 0x906c0:
       return IntelGoldmont;
+    case 0x906c0:
+      return IntelTremont;
     case 0x706e0:
     case 0x606a0:
       return IntelIcelake;
