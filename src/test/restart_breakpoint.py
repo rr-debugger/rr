23071dad7e60fe6378c623928b1bfc8176906777
Robert O'Callahan <robert@ocallahan.org>
2024-01-13 11:40:00 +1300

    Fix target_process test that I just regressed

--- src/test/restart_breakpoint.py
+++ src/test/restart_breakpoint.py
@@ -6,7 +6,7 @@ expect_gdb('Breakpoint 1')
 def check_breakpoint():
     expect_rr('calling C')
 
-    expect_gdb('Breakpoint 1(.*)) C')
+    expect_gdb('Breakpoint 1(.*) C')
 
     send_gdb('bt')
     expect_gdb('#0[^C]+C[^#]+#1[^B]+B[^#]+#2[^A]+A[^#]+#3[^m]+main')
