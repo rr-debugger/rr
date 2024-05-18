33c5ce716fa83b41f88f5c90f69b3a4d7065cb7f
Robert O'Callahan <robert@ocallahan.org>
2024-05-19 00:41:54 +1200

    Work around gdb bug in Ubuntu 24 LTS Aarch64 that uses the wrong address for `buf` (and `i`)

--- src/test/stack_growth.py
+++ src/test/stack_growth.py
@@ -1,3 +1,5 @@
+import re
+
 from util import *
 
 send_gdb('break breakpoint')
@@ -11,6 +13,12 @@ expect_gdb('Hardware[()/a-z ]+watchpoint 2')
 
 send_gdb('c')
 expect_gdb('Old value = 0')
-expect_gdb('New value = 100')
+expect_gdb(re.compile('New value = ([0-9]+)'))
+value = eval(last_match().group(1))
+# gcc/gdb on Ubuntu 24 LTS Aarch64 produce 84 here.
+# I think it's a gdb bug, using the wrong frame base.
+# The value of i is also incorrect in gdb (always zero).
+if value != 100 and value != 84:
+    failed('Unexpected value')
 
 ok()
