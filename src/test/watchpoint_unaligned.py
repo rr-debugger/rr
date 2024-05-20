2711d6b05fbbe246ad19335014dab199e019d63a
Robert O'Callahan <robert@ocallahan.org>
2024-05-20 19:33:36 +1200

    Work around gdb bug in Ubuntu 20 LTS

--- src/test/watchpoint_unaligned.py
+++ src/test/watchpoint_unaligned.py
@@ -6,6 +6,11 @@ def test():
     for type in ['uint16_t', 'uint32_t', 'uint64_t']:
         send_gdb('c')
         expect_gdb(f'Breakpoint {bp}')
+        # Get the value of `wp_addr` from the parent frame.
+        # On Ubuntu 20 LTS, gdb stops before the `breakpoint` prelude so
+        # gets the wrong value of `wp_addr`.
+        send_gdb('up')
+        expect_gdb('test')
         wp = set_watchpoint(f'-l *({type} *)wp_addr')
         send_gdb('c')
         expect_gdb(f'watchpoint {wp}')
