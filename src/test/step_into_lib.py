613b0c3794573c2b4c2012c4931bfad70ddfa92f
Robert O'Callahan <robert@ocallahan.org>
2024-05-20 19:23:06 +1200

    Fix step_into_lib test to handle the situation on Ubuntu 20 LTS where after breaking at `main` we have to step twice to enter the library

--- src/test/step_into_lib.py
+++ src/test/step_into_lib.py
@@ -5,6 +5,11 @@ send_gdb('b main')
 expect_gdb('Breakpoint 1')
 send_gdb('continue')
 expect_gdb('Breakpoint 1')
+expect_gdb(re.compile(r'\s([a-z_]+)\('))
+if last_match().group(1) == 'main':
+    send_gdb('s')
+    expect_gdb('lib_exit_success')
+
 send_gdb('s')
 # Should have stepped into lib_exit_success where there's an atomic_puts
 expect_gdb('atomic_puts')
