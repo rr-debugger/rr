7895bff01e10bdad65d1703d0f72b500cf98833c
Bernhard Übelacker <bernhardu@mailbox.org>
2024-06-05 00:33:40 +0200

    Include CTestCostData.txt in github actions.

--- scripts/github-actions-test.sh
+++ scripts/github-actions-test.sh
@@ -10,11 +10,14 @@ echo 0 | sudo tee /proc/sys/kernel/yama/ptrace_scope > /dev/null
 (echo 0 | sudo tee /proc/sys/kernel/apparmor_restrict_unprivileged_userns) > /dev/null || true
 let halfproc=`nproc`/2
 cd obj
+mkdir -p Testing/Temporary
+mv ../scripts/github-actions-CTestCostData.txt Testing/Temporary/CTestCostData.txt
 ctest -j$halfproc --verbose
 
 STATUS=$?
 if [[ $STATUS != 0 ]]; then
   mkdir $GITHUB_WORKSPACE/failed-tests
+  #cp -a Testing/Temporary/CTestCostData.txt $GITHUB_WORKSPACE/failed-tests/rr-test-ctestcostdata.txt
   cd /tmp
   rm rr-test-cpu-lock || true
   for dir in rr-test-*; do
