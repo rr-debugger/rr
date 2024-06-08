ca36c98ab63f54f777cac4f962c5c9e059022d41
Robert O'Callahan <robert@ocallahan.org>
2024-06-08 15:38:30 +1200

    Make `when_threads` test work with LLDB

--- src/test/when_threads.py
+++ src/test/when_threads.py
@@ -1,46 +1,42 @@
 from util import *
 import re
 
-send_gdb('break do_thread')
-expect_gdb('Breakpoint 1')
-send_gdb('continue')
-expect_gdb('Breakpoint 1')
+bp = breakpoint_at_function('do_thread')
+cont()
+expect_breakpoint_stop(bp)
 
-send_gdb('when-ticks')
-expect_gdb(re.compile(r'Current tick: (\d+)'))
+send_custom_command('when-ticks')
+expect_debugger(re.compile(r'Current tick: (\d+)'))
 thread_ticks = int(last_match().group(1))
-send_gdb('when-tid')
-expect_gdb(re.compile(r'Current tid: (\d+)'))
+send_custom_command('when-tid')
+expect_debugger(re.compile(r'Current tid: (\d+)'))
 thread_tid = int(last_match().group(1))
 
-send_gdb('info threads')
-expect_gdb('do_thread')
-send_gdb('when-ticks')
-expect_gdb(re.compile(r'Current tick: (\d+)'))
+expect_threads(2, 2)
+send_custom_command('when-ticks')
+expect_debugger(re.compile(r'Current tick: (\d+)'))
 thread_ticks2 = int(last_match().group(1))
 assert thread_ticks == thread_ticks2
-send_gdb('when-tid')
-expect_gdb(re.compile(r'Current tid: (\d+)'))
+send_custom_command('when-tid')
+expect_debugger(re.compile(r'Current tid: (\d+)'))
 thread_tid2 = int(last_match().group(1))
 assert thread_tid == thread_tid2
 
-send_gdb('thread 1')
-expect_gdb('Switching')
-send_gdb('when-ticks')
-expect_gdb(re.compile(r'Current tick: (\d+)'))
+select_thread(1)
+send_custom_command('when-ticks')
+expect_debugger(re.compile(r'Current tick: (\d+)'))
 main_ticks = int(last_match().group(1))
-send_gdb('when-tid')
-expect_gdb(re.compile(r'Current tid: (\d+)'))
+send_custom_command('when-tid')
+expect_debugger(re.compile(r'Current tid: (\d+)'))
 main_tid = int(last_match().group(1))
 
-send_gdb('info threads')
-expect_gdb('do_thread')
-send_gdb('when-ticks')
-expect_gdb(re.compile(r'Current tick: (\d+)'))
+expect_threads(2, 1)
+send_custom_command('when-ticks')
+expect_debugger(re.compile(r'Current tick: (\d+)'))
 main_ticks2 = int(last_match().group(1))
 assert main_ticks == main_ticks2
-send_gdb('when-tid')
-expect_gdb(re.compile(r'Current tid: (\d+)'))
+send_custom_command('when-tid')
+expect_debugger(re.compile(r'Current tid: (\d+)'))
 main_tid2 = int(last_match().group(1))
 assert main_tid == main_tid2
 
