f7f4c298f8acac3c467444471a2b359495c83348
Simon Farre <simon.farre.cx@gmail.com>
2024-07-11 12:08:17 +0200

    Added dynamic `TargetDescription`
    TargetDescription dynamically generates the "master" target description
    contents (target.xml), so that we only have to manage the individual
    concrete CPU features (like foo-sse.xml, for instance). TargetDescription
    will generate the xml contents that includes the used features.
    
    Removed combinatorial target.xml files
    
    These are now handled by `TargetDescription` instead.
    
    This unblocks the #3776 PR (AVX512 support) from getting pulled in,
    though that PR will need to take this PR into account.

---
+++ src/TargetDescription.h
@@ -0,0 +1,35 @@
+/* -*- Mode: C++; tab-width: 8; c-basic-offset: 2; indent-tabs-mode: nil; -*- */
+
+#ifndef RR_TARGET_DESCRIPTION_H_
+#define RR_TARGET_DESCRIPTION_H_
+
+#include <cstdint>
+
+#include "kernel_abi.h"
+
+using namespace std;
+
+namespace rr {
+
+enum class TargetFeature : uint32_t {
+  Core = 0,
+  SSE,
+  Linux,
+  Segment,
+  AVX,
+  PKeys,
+  FPU,
+};
+
+class TargetDescription {
+public:
+  explicit TargetDescription(rr::SupportedArch arch, uint32_t cpu_features);
+  string to_xml() const;
+
+private:
+  SupportedArch arch;
+  vector<TargetFeature> target_features;
+};
+} // namespace rr
+
+#endif /* RR_TARGET_DESCRIPTION_H_ */
