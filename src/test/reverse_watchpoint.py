b618555ac972cde84dafb683b5b37e1522246345
Robert O'Callahan <robert@ocallahan.org>
2024-07-24 00:27:42 +1200

    Make test more robust by using 'watch -l' and starting the watch in 'main' when the variable is in scope

--- src/test/reverse_watchpoint.py
+++ src/test/reverse_watchpoint.py
@@ -1,9 +1,14 @@
 from util import *
 
-send_gdb('break atomic_puts')
+send_gdb('break main')
 expect_gdb('Breakpoint 1')
-send_gdb('watch var')
+send_gdb('continue')
+expect_gdb('Breakpoint 1')
+send_gdb('delete 1')
+send_gdb('watch -l var')
 expect_gdb('Hardware watchpoint 2')
+send_gdb('break atomic_puts')
+expect_gdb('Breakpoint 3')
 
 send_gdb('c')
 expect_gdb('Hardware watchpoint 2')
@@ -16,7 +21,7 @@ expect_gdb('Old value = 42')
 expect_gdb('New value = 1337')
 
 send_gdb('c')
-expect_gdb('Breakpoint 1')
+expect_gdb('Breakpoint 3')
 
 send_gdb('reverse-cont')
 expect_gdb('Hardware watchpoint 2')
