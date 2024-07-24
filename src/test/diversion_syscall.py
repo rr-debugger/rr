f8a9370c4c885c9090bcd8555d2602abd062add4
Robert O'Callahan <robert@ocallahan.org>
2024-07-24 23:58:41 +1200

    Avoid Python warnings by using raw strings for regexes

--- src/test/diversion_syscall.py
+++ src/test/diversion_syscall.py
@@ -8,11 +8,11 @@ send_gdb('c')
 expect_gdb('Breakpoint 1')
 
 send_gdb('p (int)open("diversion_print.out",0102,0600)')
-expect_gdb('\$1 = 3')
+expect_gdb(r'\$1 = 3')
 # This FD is confused with that during record. That's ok, just make sure
 # operations on this during the diversion actually go to our file.
 
 send_gdb('p (int)write(3, "DIVERSION-SUCCESS", 17)')
-expect_gdb('\$2 = 17')
+expect_gdb(r'\$2 = 17')
 
 ok()
