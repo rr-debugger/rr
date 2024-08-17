rr-debugger/rr

commit 696fc97c0671003339b520834327353146eeb4c6

Restart "rr replay -a" if we encounter a transient error (e.g. skid exceeded) during tests

src/ReplaySession.cc
@@ -169,7 +169,9 @@ ReplaySession::ReplaySession(const std::string& dir, const Flags& flags)
       flags_(flags),
       skip_next_execution_event(false),
       replay_stops_at_first_execve_(flags.replay_stops_at_first_execve),
-      trace_start_time(0) {
+      detected_transient_error_(false),
+      trace_start_time(0),
+      suppress_stdio_before_event_(0) {
   if (trace_in.required_forward_compatibility_version() > FORWARD_COMPATIBILITY_VERSION) {
     CLEAN_FATAL()
       << "This rr build is too old to replay the trace (we support forward compatibility version "
@@ -233,7 +235,9 @@ ReplaySession::ReplaySession(const ReplaySession& other)
       fast_forward_status(other.fast_forward_status),
       skip_next_execution_event(other.skip_next_execution_event),
       replay_stops_at_first_execve_(other.replay_stops_at_first_execve_),
-      trace_start_time(other.trace_start_time) {}
+      detected_transient_error_(other.detected_transient_error_),
+      trace_start_time(other.trace_start_time),
+      suppress_stdio_before_event_(other.suppress_stdio_before_event_) {}
 
 ReplaySession::~ReplaySession() {
   // We won't permanently leak any OS resources by not ensuring
@@ -1916,8 +1920,10 @@ ReplayResult ReplaySession::replay_step(const StepConstraints& constraints) {
   finish_initializing();
 
   ReplayResult result(REPLAY_CONTINUE);
-
-  ReplayTask* t = current_task();
+  if (detected_transient_error_) {
+    result.status = REPLAY_TRANSIENT_ERROR;
+    return result;
+  }
 
   if (EV_TRACE_TERMINATION == trace_frame.event().type()) {
     result.status = REPLAY_EXITED;
@@ -1927,6 +1933,7 @@ ReplayResult ReplaySession::replay_step(const StepConstraints& constraints) {
   /* If we restored from a checkpoint, the steps might have been
    * computed already in which case step.action will not be TSTEP_NONE.
    */
+  ReplayTask* t = current_task();
   if (current_step.action == TSTEP_NONE) {
     t = setup_replay_one_trace_frame(t);
     if (current_step.action == TSTEP_NONE) {
@@ -1946,7 +1953,12 @@ ReplayResult ReplaySession::replay_step(const StepConstraints& constraints) {
   result.break_status.task_context = TaskContext(t);
 
   /* Advance towards fulfilling |current_step|. */
-  if (try_one_trace_step(t, constraints) == INCOMPLETE) {
+  Completion complete = try_one_trace_step(t, constraints);
+  if (detected_transient_error_) {
+    result.status = REPLAY_TRANSIENT_ERROR;
+    return result;
+  }
+  if (complete == INCOMPLETE) {
     if (EV_TRACE_TERMINATION == trace_frame.event().type()) {
       // An irregular trace step had to read the
       // next trace frame, and that frame was an
@@ -2146,6 +2158,11 @@ void ReplaySession::reattach_tasks(ScopedFd new_tracee_socket, ScopedFd new_trac
     t->clear_wait_status();
     t->open_mem_fd();
   }
+}
+
+bool ReplaySession::echo_stdio() const {
+  return flags().redirect_stdio &&
+    current_frame_time() >= suppress_stdio_before_event_;
 }
 
 } // namespace rr

src/PerfCounters.cc
@@ -136,6 +136,12 @@ enum CpuMicroarch {
  */
 #define PMU_TICKS_TAKEN_BRANCHES (1<<3)
 
+/*
+ * Set if this CPU is known to have essentially unbounded skid,
+ * i.e. the provided skid value is exceeded in rare cases.
+ */
+#define PMU_SKID_UNBOUNDED (1<<4)
+
 struct PmuConfig {
   CpuMicroarch uarch;
   const char* name;
@@ -186,7 +192,7 @@ static const PmuConfig pmu_configs[] = {
   // 0xd1 == RETIRED_CONDITIONAL_BRANCH_INSTRUCTIONS - Number of retired conditional branch instructions
   // 0x2c == INTERRUPT_TAKEN - Counts the number of interrupts taken
   // Both counters are available on Zen, Zen+ and Zen2.
-  { AMDZen, "AMD Zen", 0x5100d1, 0, 0, 10000, PMU_TICKS_RCB },
+  { AMDZen, "AMD Zen", 0x5100d1, 0, 0, 10000, PMU_TICKS_RCB | PMU_SKID_UNBOUNDED },
   // Performance cores from ARM from cortex-a76 on (including neoverse-n1 and later)
   // have the following counters that are reliable enough for us.
   // 0x21 == BR_RETIRED - Architecturally retired taken branches
@@ -948,12 +954,12 @@ void PerfCounters::close() {
   fd_ticks_in_transaction.close();
 }
 
-Ticks PerfCounters::stop(Task* t) {
+Ticks PerfCounters::stop(Task* t, Error* error) {
   if (!counting) {
     return 0;
   }
 
-  Ticks ticks = read_ticks(t);
+  Ticks ticks = read_ticks(t, error);
   counting = false;
   if (pt_state) {
     pt_thread_state->stop_copying(pt_state.get());
@@ -989,7 +995,7 @@ Ticks PerfCounters::ticks_for_direct_call(Task*) {
   return (pmu_semantics_flags & PMU_TICKS_TAKEN_BRANCHES) ? 1 : 0;
 }
 
-Ticks PerfCounters::read_ticks(Task* t) {
+Ticks PerfCounters::read_ticks(Task* t, Error* error) {
   ASSERT(t, opened);
   ASSERT(t, counting);
   ASSERT(t, counting_period > 0);
@@ -1030,6 +1036,7 @@ Ticks PerfCounters::read_ticks(Task* t) {
       counting_period +
       (t->session().is_recording() ? recording_skid_size() : skid_size());
   uint64_t interrupt_val = read_counter(fd_ticks_interrupt);
+  uint64_t ret;
   if (!fd_ticks_measure.is_open()) {
     if (fd_minus_ticks_measure.is_open()) {
       uint64_t minus_measure_val = read_counter(fd_minus_ticks_measure);
@@ -1045,33 +1052,37 @@ Ticks PerfCounters::read_ticks(Task* t) {
            "received after this warning, any conditions that reliably reproduce it,\n"
            "or sightings of this warning on non-AMD systems.";
       }
-    } else {
-      ASSERT(t, interrupt_val <= adjusted_counting_period)
-          << "Detected " << interrupt_val << " ticks, expected no more than "
-          << adjusted_counting_period;
     }
-    return interrupt_val;
+    ret = interrupt_val;
+  } else {
+    uint64_t measure_val = read_counter(fd_ticks_measure);
+    if (measure_val > interrupt_val) {
+      // There is some kind of kernel or hardware bug that means we sometimes
+      // see more events with IN_TXCP set than without. These are clearly
+      // spurious events :-(. For now, work around it by returning the
+      // interrupt_val. That will work if HLE hasn't been used in this interval.
+      // Note that interrupt_val > measure_val is valid behavior (when HLE is
+      // being used).
+      LOG(debug) << "Measured too many ticks; measure=" << measure_val
+                 << ", interrupt=" << interrupt_val;
+      ret = interrupt_val;
+    } else {
+      ret = measure_val;
+    }
   }
-
-  uint64_t measure_val = read_counter(fd_ticks_measure);
-  if (measure_val > interrupt_val) {
-    // There is some kind of kernel or hardware bug that means we sometimes
-    // see more events with IN_TXCP set than without. These are clearly
-    // spurious events :-(. For now, work around it by returning the
-    // interrupt_val. That will work if HLE hasn't been used in this interval.
-    // Note that interrupt_val > measure_val is valid behavior (when HLE is
-    // being used).
-    LOG(debug) << "Measured too many ticks; measure=" << measure_val
-               << ", interrupt=" << interrupt_val;
-    ASSERT(t, interrupt_val <= adjusted_counting_period)
-        << "Detected " << interrupt_val << " ticks, expected no more than "
-        << adjusted_counting_period;
-    return interrupt_val;
+  if (ret > adjusted_counting_period) {
+    if (error && (perf_attrs[pmu_index].pmu_flags & PMU_SKID_UNBOUNDED)) {
+      *error = Error::Transient;
+    } else {
+      ASSERT(t, false) << "Detected " << ret
+          << " ticks, expected no more than " << adjusted_counting_period;
+    }
+  } else {
+    if (error) {
+      *error = Error::None;
+    }
   }
-  ASSERT(t, measure_val <= adjusted_counting_period)
-      << "Detected " << measure_val << " ticks, expected no more than "
-      << adjusted_counting_period;
-  return measure_val;
+  return ret;
 }
 
 } // namespace rr

src/PerfCounters.h
@@ -94,12 +94,22 @@ public:
    */
   void start(Task* t, Ticks ticks_period);
 
+  enum class Error {
+    // Everything ok
+    None,
+    // A transient error was detected. Retrying might succeed.
+    Transient,
+  };
+
   /**
    * Suspend counting until the next start.
    * Returns the current value of the ticks counter.
    * `t` is used for debugging purposes.
+   * If `error` is non-null and a transient error is detected,
+   * `*error` will be set to `Error::Transient`. If `error` is null
+   * and a transient error is detected, it will be treated as fatal.
    */
-  Ticks stop(Task* t);
+  Ticks stop(Task* t, Error* error = nullptr);
 
   /**
    * Close the perfcounter fds (if open). They will be automatically reopened if/when
@@ -174,7 +184,7 @@ private:
    */
   uint32_t recording_skid_size() { return skid_size() * 5; }
 
-  Ticks read_ticks(Task* t);
+  Ticks read_ticks(Task* t, Error* error);
 
   // Only valid while 'counting' is true
   Ticks counting_period;

src/ReplayCommand.cc
@@ -73,6 +73,8 @@ ReplayCommand ReplayCommand::singleton(
     "  --intel-pt-start-checking-event   verify control flow using Intel PT\n"
     "                             (used for debugging rr)\n"
     "  -x, --gdb-x=<FILE>         execute gdb commands from <FILE>\n"
+    "  --retry-transient-errors   If we detect a transient error that might resolve\n"
+    "                             by retrying, retry it\n"
     "  --stats=<N>                display brief stats every N steps (eg 10000).\n"
     "  --serve-files              Serve all files from the trace rather than\n"
     "                             assuming they exist on disk. Debugging will\n"
@@ -129,6 +131,8 @@ struct ReplayFlags {
   // to test the corresponding code.
   bool share_private_mappings;
 
+  bool retry_transient_errors;
+
   // When nonzero, display statistics every N steps.
   uint32_t dump_interval;
 
@@ -153,6 +157,7 @@ struct ReplayFlags {
         redirect(true),
         cpu_unbound(false),
         share_private_mappings(false),
+        retry_transient_errors(false),
         dump_interval(0),
         serve_files(false),
         intel_pt_start_checking_event(-1) {}
@@ -170,12 +175,14 @@ static bool parse_replay_arg(vector<string>& args, ReplayFlags& flags) {
     { 'k', "keep-listening", NO_PARAMETER },
     { 'f', "onfork", HAS_PARAMETER },
     { 'g', "goto", HAS_PARAMETER },
+    { 'i', "interpreter", HAS_PARAMETER },
     { 'o', "debugger-option", HAS_PARAMETER },
     { 'p', "onprocess", HAS_PARAMETER },
     { 'q', "no-redirect-output", NO_PARAMETER },
     { 'h', "dbghost", HAS_PARAMETER },
     { 's', "dbgport", HAS_PARAMETER },
     { 't', "trace", HAS_PARAMETER },
+    { 'u', "cpu-unbound", NO_PARAMETER },
     { 'x', "gdb-x", HAS_PARAMETER },
     { 0, "share-private-mappings", NO_PARAMETER },
     { 1, "fullname", NO_PARAMETER },
@@ -183,8 +190,7 @@ static bool parse_replay_arg(vector<string>& args, ReplayFlags& flags) {
     { 3, "serve-files", NO_PARAMETER },
     { 4, "tty", HAS_PARAMETER },
     { 5, "intel-pt-start-checking-event", HAS_PARAMETER },
-    { 'u', "cpu-unbound", NO_PARAMETER },
-    { 'i', "interpreter", HAS_PARAMETER }
+    { 6, "retry-transient-errors", NO_PARAMETER }
   };
   ParsedOption opt;
   if (!Command::parse_option(args, options, &opt)) {
@@ -214,6 +220,10 @@ static bool parse_replay_arg(vector<string>& args, ReplayFlags& flags) {
         return false;
       }
       flags.goto_event = opt.int_value;
+      break;
+    case 'i':
+      flags.gdb_options.push_back("-i");
+      flags.gdb_options.push_back(opt.value);
       break;
     case 'k':
       flags.keep_listening = true;
@@ -252,6 +262,9 @@ static bool parse_replay_arg(vector<string>& args, ReplayFlags& flags) {
       }
       flags.singlestep_to_event = opt.int_value;
       break;
+    case 'u':
+      flags.cpu_unbound = true;
+      break;
     case 'x':
       flags.gdb_options.push_back("-x");
       flags.gdb_options.push_back(opt.value);
@@ -280,12 +293,8 @@ static bool parse_replay_arg(vector<string>& args, ReplayFlags& flags) {
       }
       flags.intel_pt_start_checking_event = opt.int_value;
       break;
-    case 'u':
-      flags.cpu_unbound = true;
-      break;
-    case 'i':
-      flags.gdb_options.push_back("-i");
-      flags.gdb_options.push_back(opt.value);
+    case 6:
+      flags.retry_transient_errors = true;
       break;
     default:
       DEBUG_ASSERT(0 && "Unknown option");
@@ -351,13 +360,14 @@ static bool pid_execs(const string& trace_dir, pid_t pid) {
 // This needs to be global because it's used by a signal handler.
 static pid_t waiting_for_child;
 
-static ReplaySession::Flags session_flags(const ReplayFlags& flags) {
+static ReplaySession::Flags session_flags(const ReplayFlags& flags, bool can_retry_transient_errors) {
   ReplaySession::Flags result;
   result.redirect_stdio = flags.redirect;
   result.redirect_stdio_file = flags.tty;
   result.share_private_mappings = flags.share_private_mappings;
   result.cpu_unbound = flags.cpu_unbound;
   result.intel_pt_start_checking_event = flags.intel_pt_start_checking_event;
+  result.transient_errors_fatal = !can_retry_transient_errors || !flags.retry_transient_errors;
   return result;
 }
 
@@ -368,12 +378,13 @@ static uint64_t to_microseconds(const struct timeval& tv) {
 static void serve_replay_no_debugger(const string& trace_dir,
                                      const ReplayFlags& flags) {
   ReplaySession::shr_ptr replay_session =
-    ReplaySession::create(trace_dir, session_flags(flags));
+    ReplaySession::create(trace_dir, session_flags(flags, true));
   uint32_t step_count = 0;
   struct timeval last_dump_time;
   double last_dump_rectime = 0;
   Session::Statistics last_stats;
   gettimeofday(&last_dump_time, NULL);
+  FrameTime printed_stdio_up_to_event = 0;
 
   while (true) {
     RunCommand cmd = RUN_CONTINUE;
@@ -392,8 +403,23 @@ static void serve_replay_no_debugger(const string& trace_dir,
     auto result = replay_session->replay_step(cmd);
     FrameTime after_time = replay_session->trace_reader().time();
     DEBUG_ASSERT(after_time >= before_time && after_time <= before_time + 1);
-    if (!last_dump_rectime)
+
+    if (result.status == REPLAY_TRANSIENT_ERROR) {
+      // Restart the replay from the beginning
+      LOG(warn) << "Transient error while replaying event "
+        << after_time << ", re-replaying execution from the beginning";
+      replay_session =
+        ReplaySession::create(trace_dir, session_flags(flags, true));
+      replay_session->set_suppress_stdio_before_event(printed_stdio_up_to_event + 1);
+      continue;
+    }
+
+    if (!last_dump_rectime) {
       last_dump_rectime = replay_session->trace_reader().recording_time();
+    }
+    if (after_time > before_time && before_time > printed_stdio_up_to_event) {
+      printed_stdio_up_to_event = before_time;
+    }
 
     ++step_count;
     if (flags.dump_interval > 0 && step_count % flags.dump_interval == 0) {
@@ -479,7 +505,7 @@ static int replay(const string& trace_dir, const ReplayFlags& flags) {
     if (target.event == numeric_limits<decltype(target.event)>::max()) {
       serve_replay_no_debugger(trace_dir, flags);
     } else {
-      auto session = ReplaySession::create(trace_dir, session_flags(flags));
+      auto session = ReplaySession::create(trace_dir, session_flags(flags, false));
       GdbServer::ConnectionFlags conn_flags;
       conn_flags.dbg_port = flags.dbg_port;
       conn_flags.dbg_host = flags.dbg_host;
@@ -507,7 +533,7 @@ static int replay(const string& trace_dir, const ReplayFlags& flags) {
       prctl(PR_SET_PDEATHSIG, SIGTERM, 0, 0, 0);
 
       ScopedFd debugger_params_write_pipe(debugger_params_pipe[1]);
-      auto session = ReplaySession::create(trace_dir, session_flags(flags));
+      auto session = ReplaySession::create(trace_dir, session_flags(flags, false));
       GdbServer::ConnectionFlags conn_flags;
       conn_flags.dbg_port = flags.dbg_port;
       conn_flags.dbg_host = flags.dbg_host;
@@ -618,6 +644,21 @@ int ReplayCommand::run(vector<string>& args) {
   }
   if (flags.dump_interval > 0 && !flags.dont_launch_debugger) {
     fprintf(stderr, "--stats requires -a\n");
+    print_help(stderr);
+    return 2;
+  }
+  if (flags.retry_transient_errors && flags.dump_interval) {
+    fprintf(stderr, "--retry-transient-errors is not compatible with --dump-interval");
+    print_help(stderr);
+    return 2;
+  }
+  if (flags.retry_transient_errors && flags.singlestep_to_event) {
+    fprintf(stderr, "--retry-transient-errors is not compatible with --singlestep-to-event");
+    print_help(stderr);
+    return 2;
+  }
+  if (flags.retry_transient_errors && !flags.dont_launch_debugger) {
+    fprintf(stderr, "--retry-transient-errors requires -a (for now)");
     print_help(stderr);
     return 2;
   }

src/ReplaySession.h
@@ -107,7 +107,10 @@ enum ReplayStatus {
   // Some execution was replayed. replay_step() can be called again.
   REPLAY_CONTINUE,
   // All tracees are dead. replay_step() should not be called again.
-  REPLAY_EXITED
+  REPLAY_EXITED,
+  // Replay failed and this session is dead, but trying again with a
+  // new session might work.
+  REPLAY_TRANSIENT_ERROR,
 };
 
 struct ReplayResult {
@@ -243,6 +246,7 @@ public:
       , share_private_mappings(false)
       , replay_stops_at_first_execve(false)
       , cpu_unbound(false)
+      , transient_errors_fatal(false)
       , intel_pt_start_checking_event(-1) {}
     Flags(const Flags&) = default;
     bool redirect_stdio;
@@ -250,6 +254,7 @@ public:
     bool share_private_mappings;
     bool replay_stops_at_first_execve;
     bool cpu_unbound;
+    bool transient_errors_fatal;
     FrameTime intel_pt_start_checking_event;
   };
 
@@ -353,6 +358,11 @@ public:
    */
   void reattach_tasks(ScopedFd new_tracee_socket, ScopedFd new_tracee_socket_receiver);
 
+  void notify_detected_transient_error() { detected_transient_error_ = true; }
+
+  void set_suppress_stdio_before_event(FrameTime event) { suppress_stdio_before_event_ = event; }
+  bool echo_stdio() const;
+
 private:
   ReplaySession(const std::string& dir, const Flags& flags);
   ReplaySession(const ReplaySession& other);
@@ -409,11 +419,14 @@ private:
   FastForwardStatus fast_forward_status;
   bool skip_next_execution_event;
   bool replay_stops_at_first_execve_;
+  bool detected_transient_error_;
 
   // The clock_gettime(CLOCK_MONOTONIC) timestamp of the first trace event, used
   // during 'replay' to calculate the elapsed time between the first event and
   // all other recorded events in the timeline during the 'record' phase.
   double trace_start_time;
+
+  FrameTime suppress_stdio_before_event_;
 
   std::shared_ptr<AddressSpace> syscall_bp_vm;
   remote_code_ptr syscall_bp_addr;

src/Session.cc
@@ -798,4 +798,8 @@ void Session::do_bind_cpu() {
   }
 }
 
+bool Session::mark_stdio() const {
+  return visible_execution_ && Flags::get().mark_stdio;
+}
+
 } // namespace rr

src/Session.h
@@ -289,9 +289,8 @@ public:
   bool is_replaying() { return as_replay() != nullptr; }
   bool is_diversion() { return as_diversion() != nullptr; }
 
-  // Returns true if execution should be "visible", i.e. it's the main
+  // Indicate if execution should be "visible", i.e. it's the main
   // session of a recording or a replay whose output could be echoed.
-  bool visible_execution() const { return visible_execution_; }
   void set_visible_execution(bool visible) { visible_execution_ = visible; }
 
   virtual bool need_performance_counters() const { return true; }
@@ -425,6 +424,8 @@ public:
   /* When this is true, we collect Intel PT traces during recording
      or replay. */
   bool intel_pt_enabled() const { return intel_pt_; }
+
+  bool mark_stdio() const;
 
 protected:
   Session();

src/StdioMonitor.cc
@@ -11,7 +11,7 @@
 namespace rr {
 
 Switchable StdioMonitor::will_write(Task* t) {
-  if (Flags::get().mark_stdio && t->session().visible_execution()) {
+  if (t->session().mark_stdio()) {
     char buf[256];
     snprintf(buf, sizeof(buf) - 1, "[rr %d %d]", t->tgid(), t->trace_time());
     ssize_t len = strlen(buf);
@@ -25,17 +25,15 @@ Switchable StdioMonitor::will_write(Task* t) {
 
 void StdioMonitor::did_write(Task* t, const std::vector<Range>& ranges,
                              LazyOffset&) {
-  if (!t->session().is_replaying()) {
+  ReplaySession* replay_session = t->session().as_replay();
+  if (!replay_session || !replay_session->echo_stdio()) {
     return;
   }
-  auto rt = static_cast<ReplayTask*>(t);
-  if (rt->session().flags().redirect_stdio && rt->session().visible_execution()) {
-    for (auto& r : ranges) {
-      auto bytes = t->read_mem(r.data.cast<uint8_t>(), r.length);
-      if (bytes.size() !=
-          (size_t)write(original_fd, bytes.data(), bytes.size())) {
-        ASSERT(t, false) << "Couldn't write to " << original_fd;
-      }
+  for (auto& r : ranges) {
+    auto bytes = t->read_mem(r.data.cast<uint8_t>(), r.length);
+    if (bytes.size() !=
+        (size_t)write(original_fd, bytes.data(), bytes.size())) {
+      ASSERT(t, false) << "Couldn't write to " << original_fd;
     }
   }
 }

commit cc977129f61cae5e546cc0600beba50122a9161f

Don't echo output to stdio during reverse-execution.
I think I regressed this in 696fc97c0671003339b520834327353146eeb4c6.

src/ReplaySession.cc
@@ -2234,7 +2234,7 @@ bool ReplaySession::mark_stdio() const {
 }
 
 bool ReplaySession::echo_stdio() const {
-  return flags().redirect_stdio &&
+  return flags().redirect_stdio && visible_execution_ &&
     current_frame_time() >= suppress_stdio_before_event_;
 }
 

src/test/vfork_read_clone_stress.py
@@ -7,7 +7,9 @@ send_gdb('c')
 expect_gdb('Program received signal SIGKILL')
 
 send_gdb('rc')
-expect_gdb('No more reverse-execution history')
+expect_gdb(['(No more reverse-execution history)', '(child)'])
+
+assert 'child' not in last_match().group(1)
 
 send_gdb('c')
 ok()