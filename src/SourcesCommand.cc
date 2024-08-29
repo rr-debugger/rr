rr-debugger/rr

commit 06d0b22778c43bace677d2407128479e96a5371a

Support more than one debug dir from the gdb script.

src/SourcesCommand.cc
@@ -815,32 +815,32 @@ static bool try_debuglink_file(ElfFileReader& trace_file_reader,
     dd = debug_dirs->process_one_binary(original_file_name);
   }
 
-  const string* chosen_debug_dir = nullptr;
-  if (!dd.debug_file_directories.empty()) {
-    // XXXkhuey what do we do if there's more than one directory?
-    chosen_debug_dir = &dd.debug_file_directories.back();
-  }
-  const string* chosen_src_dir = nullptr;
-  if (!dd.source_directories.empty()) {
-    chosen_src_dir = &dd.source_directories.back();
-  }
+  for (auto chosen_debug_dir : dd.debug_file_directories) {
+    const string* chosen_src_dir = nullptr;
+    if (!dd.source_directories.empty()) {
+      chosen_src_dir = &dd.source_directories.back();
+    }
 
-  bool has_source_files = process_auxiliary_file(trace_file_reader, *reader, altlink_reader.get(),
+    bool has_source_files = process_auxiliary_file(trace_file_reader, *reader, altlink_reader.get(),
+                                                   trace_relative_name, original_file_name,
+                                                   file_names, full_file_name, DEBUGLINK,
+                                                   comp_dir_substitutions, output_comp_dir_substitutions,
+                                                   &chosen_debug_dir, chosen_src_dir,
+                                                   dwos, external_debug_info, false, dir_exists_cache);
+
+    if (altlink_reader) {
+      has_source_files |= process_auxiliary_file(trace_file_reader, *altlink_reader, nullptr,
                                                  trace_relative_name, original_file_name,
-                                                 file_names, full_file_name, DEBUGLINK,
+                                                 file_names, full_altfile_name, DEBUGALTLINK,
                                                  comp_dir_substitutions, output_comp_dir_substitutions,
-                                                 chosen_debug_dir, chosen_src_dir,
-                                                 dwos, external_debug_info, false, dir_exists_cache);
-
-  if (altlink_reader) {
-    has_source_files |= process_auxiliary_file(trace_file_reader, *altlink_reader, nullptr,
-                                               trace_relative_name, original_file_name,
-                                               file_names, full_altfile_name, DEBUGALTLINK,
-                                               comp_dir_substitutions, output_comp_dir_substitutions,
-                                               chosen_debug_dir, chosen_src_dir,
-                                               dwos, external_debug_info, has_source_files, dir_exists_cache);
+                                                 &chosen_debug_dir, chosen_src_dir,
+                                                 dwos, external_debug_info, has_source_files, dir_exists_cache);
+    }
+    if (has_source_files) {
+      return true;
+    }
   }
-  return has_source_files;
+  return false;
 }
 
 struct Symlink {

commit 6fafc98dabbb2c5b11abd84f62dfea18f0083a62

Fix a regression from 06d0b22778c43bace677d2407128479e96a5371a.
We need to run this code at least once even if we have no debug dir.

src/SourcesCommand.cc
@@ -815,7 +815,14 @@ static bool try_debuglink_file(ElfFileReader& trace_file_reader,
     dd = debug_dirs->process_one_binary(original_file_name);
   }
 
-  for (auto chosen_debug_dir : dd.debug_file_directories) {
+  vector<string*> debug_file_directories;
+  debug_file_directories.reserve(dd.debug_file_directories.size() + 1);
+  for (auto& dfd : dd.debug_file_directories) {
+    debug_file_directories.push_back(&dfd);
+  }
+  debug_file_directories.push_back(nullptr);
+
+  for (auto chosen_debug_dir : debug_file_directories) {
     const string* chosen_src_dir = nullptr;
     if (!dd.source_directories.empty()) {
       chosen_src_dir = &dd.source_directories.back();
@@ -825,7 +832,7 @@ static bool try_debuglink_file(ElfFileReader& trace_file_reader,
                                                    trace_relative_name, original_file_name,
                                                    file_names, full_file_name, DEBUGLINK,
                                                    comp_dir_substitutions, output_comp_dir_substitutions,
-                                                   &chosen_debug_dir, chosen_src_dir,
+                                                   chosen_debug_dir, chosen_src_dir,
                                                    dwos, external_debug_info, false, dir_exists_cache);
 
     if (altlink_reader) {
@@ -833,7 +840,7 @@ static bool try_debuglink_file(ElfFileReader& trace_file_reader,
                                                  trace_relative_name, original_file_name,
                                                  file_names, full_altfile_name, DEBUGALTLINK,
                                                  comp_dir_substitutions, output_comp_dir_substitutions,
-                                                 &chosen_debug_dir, chosen_src_dir,
+                                                 chosen_debug_dir, chosen_src_dir,
                                                  dwos, external_debug_info, has_source_files, dir_exists_cache);
     }
     if (has_source_files) {