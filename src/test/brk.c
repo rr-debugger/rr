21f051bce6ece2bc676ed7c12970ba37817fed15
Robert O'Callahan <robert@ocallahan.org>
2024-09-08 09:05:18 +0100

    Handle intermittent failure of `brk` with `ENOMEM`
    This seems ridiculous but it has happened on CI and it doesn't look like our fault.
    https://productionresultssa12.blob.core.windows.net/actions-results/45e0fad4-3a0f-4a69-b11c-5663a1815bc8/workflow-job-run-567a1046-b6a4-5b6c-0182-4dc5e6161b5b/logs/job/job-logs.txt?rsct=text%2Fplain&se=2024-09-08T08%3A07%3A04Z&sig=oOdt6lxVIcfUQrU9s4GUfJLgwDuNVcPP5YBa9siAH94%3D&ske=2024-09-08T18%3A18%3A12Z&skoid=ca7593d4-ee42-46cd-af88-8b886a2f84eb&sks=b&skt=2024-09-08T06%3A18%3A12Z&sktid=398a6654-997b-47e9-b12b-9515b896b4de&skv=2024-05-04&sp=r&spr=https&sr=b&st=2024-09-08T07%3A56%3A59Z&sv=2024-05-04

--- src/test/brk.c
+++ src/test/brk.c
@@ -6,12 +6,25 @@ int main(void) {
   void* prev;
   void* start = sbrk(0);
   test_assert((intptr_t)start != -1);
-  test_assert(start == sbrk(111));
+  void* start_after_allocation = sbrk(111);
+  if (start_after_allocation == (void*)-1) {
+    // We have seen an intermittent failure here on 32-bit :-(
+    test_assert(errno == ENOMEM);
+    test_assert(sizeof(void*) == 4);
+    atomic_puts("EXIT-SUCCESS");
+    return 0;
+  }
 
+  test_assert(start == start_after_allocation);
   memset(start, 0xaa, 111);
 
   prev = sbrk(1000000);
-  test_assert(prev != (void*)-1);
+  if (prev == (void*)-1) {
+    test_assert(errno == ENOMEM);
+    test_assert(sizeof(void*) == 4);
+    atomic_puts("EXIT-SUCCESS");
+    return 0;
+  }
   test_assert(0 == brk(prev));
 
   atomic_puts("EXIT-SUCCESS");
