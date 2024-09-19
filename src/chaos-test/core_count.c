daee39c6e10b501187ceda0a46b3abc88ce016b5
Robert O'Callahan <robert@ocallahan.org>
2024-09-20 00:03:24 +0200

    Fix core_count chaos mode test to reflect that rr with or without chaos mode always returns the true number of processors configured

--- src/chaos-test/core_count.c
+++ src/chaos-test/core_count.c
@@ -7,12 +7,13 @@
 
 int main(__attribute__((unused)) int argc, char** argv) {
   int cores = atoi(argv[1]);
+  int sysconf_onln = sysconf(_SC_NPROCESSORS_ONLN);
 
-  if (sysconf(_SC_NPROCESSORS_ONLN) == cores &&
-      sysconf(_SC_NPROCESSORS_CONF) == cores) {
+  if (sysconf_onln == cores) {
     caught_test_failure("got core count: %d", cores);
   }
 
+  atomic_printf("Found %d cores online\n", sysconf_onln);
   atomic_puts("EXIT-SUCCESS");
   return 0;
 }
