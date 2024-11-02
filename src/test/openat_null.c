01546318c93c7753f745d977e8d7c4d40ea6162c
Kyle Huey <khuey@kylehuey.com>
2024-11-02 08:20:26 -0700

    Test openat(2) instead of open(2) since it exists on AArch64.

--- src/test/openat_null.c
+++ src/test/openat_null.c
@@ -3,9 +3,9 @@
 #include <unistd.h>
 
 int main(void) {
-  // Use syscall(2) because the glibc prototype for open(2) might enforce that
+  // Use syscall(2) because the glibc prototype for openat(2) might enforce that
   // it's nonnull.
-  int fd = syscall(SYS_open, NULL, O_RDONLY);
+  int fd = syscall(SYS_openat, AT_FDCWD, NULL, O_RDONLY);
   test_assert(fd == -1);
   atomic_puts("EXIT-SUCCESS");
   return 0;
