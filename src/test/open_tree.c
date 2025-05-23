24938de7b765a8150eea4ea1a0f627e7881ed772
Robert O'Callahan <robert@ocallahan.org>
2025-05-24 09:19:50 +1200

    Fail gracefully if `/mnt` doesn't exist.
    Resolves #3960

--- src/test/open_tree.c
+++ src/test/open_tree.c
@@ -4,10 +4,17 @@
 
 int main(void) {
   int fd = syscall(RR_open_tree, AT_FDCWD, "/mnt", 0);
-  if (fd < 0 && errno == ENOSYS) {
-    atomic_puts("open_tree not supported, skipping test");
-    atomic_puts("EXIT-SUCCESS");
-    return 0;
+  if (fd < 0) {
+    if (errno == ENOSYS) {
+      atomic_puts("open_tree not supported, skipping test");
+      atomic_puts("EXIT-SUCCESS");
+      return 0;
+    }
+    if (errno == ENOENT) {
+      atomic_puts("/mnt not found, skipping test");
+      atomic_puts("EXIT-SUCCESS");
+      return 0;
+    }
   }
   test_assert(fd >= 0);
   atomic_puts("EXIT-SUCCESS");
