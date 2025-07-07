ea18630e798caab4576c27a563645f81add795e3
Bernhard Übelacker <bernhardu@mailbox.org>
2025-07-07 13:27:12 +0200

    Add TIOCGICOUNT ioctl.

--- src/test/ioctl_ttyS.c
+++ src/test/ioctl_ttyS.c
@@ -1,10 +1,12 @@
 /* -*- Mode: C; tab-width: 8; c-basic-offset: 2; indent-tabs-mode: nil; -*- */
 
 #include "util.h"
+#include <linux/serial.h>
 
 int main(void) {
   int fd;
   int* mbits;
+  struct serial_icounter_struct* sicnt;
 
   fd = open("/dev/ttyS0", O_RDWR);
   if (fd < 0) {
@@ -19,6 +21,13 @@ int main(void) {
   atomic_printf("TIOCMGET returned mbits=0x%x\n", *mbits);
   test_assert(0 == ioctl(fd, TIOCMSET, mbits));
   atomic_printf("TIOCMSET mbits=0x%x afterwards\n", *mbits);
+
+  ALLOCATE_GUARD(sicnt, 'b');
+  test_assert(0 == ioctl(fd, TIOCGICOUNT, sicnt));
+  VERIFY_GUARD(sicnt);
+  atomic_printf("TIOCGICOUNT returned %d, %d, %d, %d, %d, %d, %d, %d, %d, %d, %d\n",
+                sicnt->cts, sicnt->dsr, sicnt->rng, sicnt->dcd, sicnt->rx, sicnt->tx,
+                sicnt->frame, sicnt->overrun, sicnt->parity, sicnt->brk, sicnt->buf_overrun);
 
   atomic_puts("EXIT-SUCCESS");
   return 0;
