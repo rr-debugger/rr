c270cc0198fc4cbebd743354cc1f190f41d71172
Robert O'Callahan <robert@ocallahan.org>
2026-02-14 11:40:14 +1300

    Rename `crc32` to `update_crc32`.
    `crc32` is a macro in some environments.

--- src/util.cc
+++ src/util.cc
@@ -2159,7 +2159,7 @@ int choose_cpu(BindCPU bind_cpu, ScopedFd &cpu_lock_fd_out) {
   return cpus[random() % cpus.size()];
 }
 
-uint32_t crc32(uint32_t crc, unsigned char* buf, size_t len) {
+uint32_t update_crc32(uint32_t crc, unsigned char* buf, size_t len) {
   static const uint32_t crc32_table[256] = {
     0x00000000, 0x77073096, 0xee0e612c, 0x990951ba, 0x076dc419, 0x706af48f,
     0xe963a535, 0x9e6495a3, 0x0edb8832, 0x79dcb8a4, 0xe0d5e91e, 0x97d2d988,
