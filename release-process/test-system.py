94196e851c4da030f56aac3c85ae8dd5e76a71c4
Robert O'Callahan <robert@ocallahan.org>
2026-08-03 23:53:15 +1200

    Bash doesn't support empty functions

--- release-process/test-system.py
+++ release-process/test-system.py
@@ -151,6 +151,8 @@ def get_config_lines_arch(config_key):
 
 def config_script_function(config_key):
     lines = get_config_lines_arch(config_key)
+    if not lines:
+        lines = "true"
     return ('function %s {\n%s\n}' % (config_key, '\n'.join(lines)))
 
 machine_type = args.machine_type
